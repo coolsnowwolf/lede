bc29f3f53a86c9ba886fb4731118bac4b40894bb
coolsnowwolf <coolsnowwolf@gmail.com>
2017-09-07 12:04:38 +0800

    enable wireless by default

--- package/kernel/mac80211/files/lib/wifi/mac80211.sh
+++ package/kernel/mac80211/files/lib/wifi/mac80211.sh
@@ -116,7 +116,6 @@ detect_mac80211() {
 			set wireless.radio${devidx}.hwmode=11${mode_band}
 			${dev_id}
 			${ht_capab}
-			set wireless.radio${devidx}.disabled=1
 
 			set wireless.default_radio${devidx}=wifi-iface
 			set wireless.default_radio${devidx}.device=radio${devidx}
