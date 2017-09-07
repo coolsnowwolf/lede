a69866aaac630702c12402892baaf15d5a8cd997
coolsnowwolf <coolsnowwolf@gmail.com>
2017-09-07 22:57:44 +0800

    set default noscan wifi

--- package/kernel/mac80211/files/lib/wifi/mac80211.sh
+++ package/kernel/mac80211/files/lib/wifi/mac80211.sh
@@ -116,6 +116,7 @@ detect_mac80211() {
 			set wireless.radio${devidx}.hwmode=11${mode_band}
 			${dev_id}
 			${ht_capab}
+			set wireless.radio${devidx}.noscan=1
 
 			set wireless.default_radio${devidx}=wifi-iface
 			set wireless.default_radio${devidx}.device=radio${devidx}
