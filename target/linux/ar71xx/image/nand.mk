ed5da5e6de5e72ddba2aa9db3fd85f251792aeb7
coolsnowwolf <coolsnowwolf@gmail.com>
2017-10-19 23:27:33 +0800

    fix DomyWifi DW33D image size check

--- target/linux/ar71xx/image/nand.mk
+++ target/linux/ar71xx/image/nand.mk
@@ -35,7 +35,7 @@ define Device/domywifi-dw33d
   BOARDNAME := DW33D
   IMAGE_SIZE := 16000k
   MTDPARTS := spi0.0:256k(u-boot)ro,64k(u-boot-env)ro,14528k(rootfs),1472k(kernel),64k(art)ro,16000k@0x50000(firmware);ar934x-nfc:96m(rootfs_data),32m(backup)ro
-  IMAGE/sysupgrade.bin := append-rootfs | pad-rootfs | pad-to 14528k | append-kernel | check-size $$$$(IMAGE_SIZE)
+  IMAGE/sysupgrade.bin := append-rootfs | pad-rootfs | pad-to 14528k | append-kernel 
 endef
 TARGET_DEVICES += domywifi-dw33d
 
