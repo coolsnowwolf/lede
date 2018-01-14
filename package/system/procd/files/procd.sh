09e34de60ab11f92ff66d32baa220f7eb4f9f694
coolsnowwolf <coolsnowwolf@gmail.com>
2018-01-15 01:05:20 +0800

    disable flock

--- package/system/procd/files/procd.sh
+++ package/system/procd/files/procd.sh
@@ -41,15 +41,6 @@ _PROCD_SERVICE=
 procd_lock() {
 	local basescript=$(readlink "$initscript")
 	local service_name="$(basename ${basescript:-$initscript})"
-
-	flock -n 1000 &> /dev/null
-	if [ "$?" != "0" ]; then
-		exec 1000>/var/lock/procd_${service_name}.lock
-		flock 1000
-		if [ "$?" != "0" ]; then
-			logger "warning: procd flock for $service_name failed"
-		fi
-	fi
 }
 
 _procd_call() {
