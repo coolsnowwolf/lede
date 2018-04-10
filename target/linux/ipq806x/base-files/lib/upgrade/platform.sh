7e464f4ef5afc06b7c5d72f5aa2c600e53bb64c7
coolsnowwolf <coolsnowwolf@gmail.com>
2018-04-10 16:31:13 +0800

    switch ipq806x to kernel 4.14

--- target/linux/ipq806x/base-files/lib/upgrade/platform.sh
+++ target/linux/ipq806x/base-files/lib/upgrade/platform.sh
@@ -10,33 +10,17 @@ platform_check_image() {
 
 platform_do_upgrade() {
 	case "$(board_name)" in
-	asus,rt-ac58u|\
-	asus,rt-acrh17)
-		CI_UBIPART="UBI_DEV"
-		CI_KERNPART="linux"
-
-		local ubidev=$(nand_find_ubi $CI_UBIPART)
-		local jffs2=$(nand_find_volume $ubidev jffs2)
-		local linux2=$(nand_find_volume $ubidev linux2)
-		[ -n "$jffs2" ] && ubirmvol /dev/$ubidev --name=jffs2
-		[ -n "$linux2" ] && ubirmvol /dev/$ubidev --name=linux2
-		nand_do_upgrade "$1"
-		;;
-	linksys,ea8500)
-		platform_do_upgrade_linksys "$ARGV"
-		;;
+	compex,wpq864|\
 	netgear,d7800 |\
 	netgear,r7500 |\
 	netgear,r7500v2 |\
 	netgear,r7800 |\
-	qcom,ap-dk04.1-c1 |\
 	qcom,ipq8064-ap148 |\
 	zyxel,nbg6817)
 		nand_do_upgrade "$ARGV"
 		;;
-	openmesh,a42)
-		PART_NAME="inactive"
-		platform_do_upgrade_openmesh "$ARGV"
+	linksys,ea8500)
+		platform_do_upgrade_linksys "$ARGV"
 		;;
 	tplink,c2600)
 		PART_NAME="os-image:rootfs"
@@ -56,10 +40,6 @@ platform_do_upgrade() {
 
 platform_nand_pre_upgrade() {
 	case "$(board_name)" in
-	asus,rt-ac58u)
-		CI_UBIPART="UBI_DEV"
-		CI_KERNPART="linux"
-		;;
 	zyxel,nbg6817)
 		zyxel_do_upgrade "$1"
 		;;
