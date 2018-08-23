2c600cad70819eec5967b4e4a0841af5727c649d
coolsnowwolf <coolsnowwolf@gmail.com>
2018-08-23 22:42:54 +0800

    add GNU kernel download mirror

--- scripts/download.pl
+++ scripts/download.pl
@@ -233,6 +233,7 @@ foreach my $mirror (@ARGV) {
 			push @extra, "$extra[0]/longterm/v$1";
 		}		
 		foreach my $dir (@extra) {
+			push @mirrors, "http://mirrors.ustc.edu.cn/kernel.org/$dir";
 			push @mirrors, "https://cdn.kernel.org/pub/$dir";
 			push @mirrors, "https://mirror.rackspace.com/kernel.org/$dir";
 			push @mirrors, "http://download.xs4all.nl/ftp.kernel.org/pub/$dir";
