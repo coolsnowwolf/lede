69c4a4c9e0fd5633ec8125562febb74cccdf1664
coolsnowwolf <coolsnowwolf@gmail.com>
2018-10-05 20:44:50 +0800

    Add DNS cache to 4.14 kernel target

--- package/lean/luci-app-flowoffload/luasrc/controller/flowoffload.lua
+++ package/lean/luci-app-flowoffload/luasrc/controller/flowoffload.lua
@@ -1,7 +1,7 @@
 module("luci.controller.flowoffload", package.seeall)
 
 function index()
-	if not nixio.fs.access("/sys/module/xt_FLOWOFFLOAD/refcnt") then
+	if not nixio.fs.access("/etc/config/flowoffload") then
 		return
 	end
 	local page
@@ -24,12 +24,17 @@ local function is_fullcone()
 	return luci.sys.call("iptables -t nat -L -n --line-numbers | grep FULLCONENAT >/dev/null") == 0
 end
 
+local function is_dns()
+	return luci.sys.call("pgrep dnscache >/dev/null") == 0
+end
+
 function action_status()
 	luci.http.prepare_content("application/json")
 	luci.http.write_json({
 		run_state = is_running(),
 		down_state = is_bbr(),
-		up_state = is_fullcone()
+		up_state = is_fullcone(),
+		dns_state = is_dns()
 	})
 end
 
