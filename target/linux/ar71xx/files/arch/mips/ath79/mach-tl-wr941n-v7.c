4f82e9c40f7561f059bcd386f81e85c482950ef1
imbrolla <33008627+imbrolla@users.noreply.github.com>
2018-10-06 14:36:04 +0800

    Update mach-tl-wr941n-v7.c

--- target/linux/ar71xx/files/arch/mips/ath79/mach-tl-wr941n-v7.c
+++ target/linux/ar71xx/files/arch/mips/ath79/mach-tl-wr941n-v7.c
@@ -3,7 +3,6 @@
  *
  *  Copyright (C) 2012 Gabor Juhos <juhosg@openwrt.org>
  *  Copyright (C) 2014 Weijie Gao <hackpacsal@gmail.com>
- *  Copyright (c) 2018 Bin Chen <biosdiy@163.com>
  *
  *  This program is free software; you can redistribute it and/or modify it
  *  under the terms of the GNU General Public License version 2 as published
@@ -27,7 +26,7 @@
 #include "dev-leds-gpio.h"
 #include "dev-m25p80.h"
 #include "dev-spi.h"
-/* #include "dev-usb.h" */
+#include "dev-usb.h"
 #include "dev-wmac.h"
 #include "machtypes.h"
 
@@ -250,20 +249,19 @@ static void __init wr941nv7_setup(void)
 	ath79_register_gpio_keys_polled(-1, WR941NV7_KEYS_POLL_INTERVAL,
 					ARRAY_SIZE(wr941nv7_gpio_keys),
 					wr941nv7_gpio_keys);
-	/* WiFi Port */
+
 	ath79_register_wmac(art, mac);
 
 	ar8236_reset();
 
-	ath79_setup_qca955x_eth_cfg(QCA955X_ETH_CFG_MII_GE0 |
-		QCA955X_ETH_CFG_MII_GE0_SLAVE);
+	ath79_setup_qca955x_eth_cfg(QCA955X_ETH_CFG_GE0_MII_EN |
+		QCA955X_ETH_CFG_GE0_MII_SLAVE);
 
 	mdiobus_register_board_info(wr941nv7_mdio0_info,
 				    ARRAY_SIZE(wr941nv7_mdio0_info));
 	ath79_register_mdio(0, 0x0);
 
-	/* WAN Port */
-	ath79_init_mac(ath79_eth0_data.mac_addr, mac, 1);
+	ath79_init_mac(ath79_eth0_data.mac_addr, mac, 0);
 
 	/* GMAC0 is connected to an AR8236 switch */
 	ath79_eth0_data.phy_if_mode = PHY_INTERFACE_MODE_MII;
@@ -272,8 +270,8 @@ static void __init wr941nv7_setup(void)
 	ath79_eth0_data.phy_mask = BIT(0);
 	ath79_eth0_data.mii_bus_dev = &ath79_mdio0_device.dev;
 	ath79_register_eth(0);
-	
-	/* ath79_register_usb(); */
+
+	ath79_register_usb();
 }
 
 MIPS_MACHINE(ATH79_MACH_TL_WR941N_V7, "TL-WR941N-v7",
