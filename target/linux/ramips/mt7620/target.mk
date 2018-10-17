4c45063e3368846b2beebb0018ab728885ab6437
coolsnowwolf <coolsnowwolf@gmail.com>
2018-10-17 22:06:35 +0800

    default enable 802.11r and 802.11w in ar71xx ath79 mvebu mtk7620 mtk7621 target

--- target/linux/ramips/mt7620/target.mk
+++ target/linux/ramips/mt7620/target.mk
@@ -7,7 +7,7 @@ BOARDNAME:=MT7620 based boards
 FEATURES+=usb
 CPU_TYPE:=24kc
 
-DEFAULT_PACKAGES += kmod-rt2800-pci kmod-rt2800-soc kmod-mt76 wpad-mini
+DEFAULT_PACKAGES += kmod-rt2800-pci kmod-rt2800-soc kmod-mt76 wpad-basic
 
 define Target/Description
 	Build firmware images for Ralink MT7620 based boards.
