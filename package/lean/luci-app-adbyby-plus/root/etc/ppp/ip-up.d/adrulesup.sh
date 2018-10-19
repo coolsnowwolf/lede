510d2c3e466e953eba52c1a7c4e1d423ee10e21f
coolsnowwolf <coolsnowwolf@gmail.com>
2018-10-19 12:40:22 +0800

    move lean packages to my packages feed

--- package/lean/luci-app-adbyby-plus/root/etc/ppp/ip-up.d/adrulesup.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-sleep 30 && sh /usr/share/adbyby/adupdate.sh
--- package/lean/luci-app-ssr-plus/root/usr/share/shadowsocksr/gfw2ipset.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-awk '!/^$/&&!/^#/{printf("ipset=/.%s/'"gfwlist"'\n",$0)}' /etc/config/gfw.list > /etc/dnsmasq.ssr/custom_forward.conf
-awk '!/^$/&&!/^#/{printf("server=/.%s/'"127.0.0.1#5353"'\n",$0)}' /etc/config/gfw.list >> /etc/dnsmasq.ssr/custom_forward.conf
-
