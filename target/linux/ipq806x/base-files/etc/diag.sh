96bab7c8f96657b2df9e354eead11b797dfa276d
coolsnowwolf <coolsnowwolf@gmail.com>
2018-12-31 11:33:21 +0800

    Revert "ipq806x: add support for Buffalo WXR-2533DHP"
    This reverts commit a3057c7d652b660f1e248f55c0c0301592e5f643.

---
+++ target/linux/ipq806x/base-files/etc/diag.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+# Copyright (C) 2016 Henryk Heisig hyniu@o2.pl
+
+. /lib/functions/leds.sh
+
+boot="$(get_dt_led boot)"
+failsafe="$(get_dt_led failsafe)"
+running="$(get_dt_led running)"
+upgrade="$(get_dt_led upgrade)"
+
+set_state() {
+	status_led="$boot"
+
+	case "$1" in
+	preinit)
+		status_led_blink_preinit
+		;;
+	failsafe)
+		status_led_off
+		[ -n "$running" ] && {
+			status_led="$running"
+			status_led_off
+		}
+		status_led="$failsafe"
+		status_led_blink_failsafe
+		;;
+	preinit_regular)
+		status_led_blink_preinit_regular
+		;;
+	upgrade)
+		[ -n "$running" ] && {
+			status_led="$running"
+			status_led_off
+		}
+		status_led="$upgrade"
+		status_led_blink_preinit_regular
+		;;
+	done)
+		status_led_off
+		[ -n "$running" ] && {
+			status_led="$running"
+			status_led_on
+		}
+		;;
+	esac
+}
