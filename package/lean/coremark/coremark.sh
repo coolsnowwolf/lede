8ba6d1b75beaf3cd9df836eb94924add2e74629e
coolsnowwolf <coolsnowwolf@gmail.com>
2019-04-18 19:37:45 +0800

    coremakr: store benchmark scores

--- package/lean/coremark/coremark.sh
+++ package/lean/coremark/coremark.sh
@@ -2,11 +2,11 @@
 
 /bin/coremark > /tmp/coremark.log
 
-cat /tmp/coremark.log | grep "CoreMark 1.0" | cut -d  "/"  -f 1 > /tmp/bench.log
-sed -i 's/CoreMark 1.0/(CpuMark/g'  /tmp/bench.log
-echo " Scores)" >> /tmp/bench.log
+cat /tmp/coremark.log | grep "CoreMark 1.0" | cut -d  "/"  -f 1 > /etc/bench.log
+sed -i 's/CoreMark 1.0/(CpuMark/g'  /etc/bench.log
+echo " Scores)" >> /etc/bench.log
 
-if [ -f "/tmp/bench.log" ]; then
+if [ -f "/etc/bench.log" ]; then
  sed -i '/coremark/d' /etc/crontabs/root
  crontab /etc/crontabs/root
 fi
