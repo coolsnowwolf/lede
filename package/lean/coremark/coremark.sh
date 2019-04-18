7a7051a9e8de7773ee3cc21c9b18d7441b86e245
coolsnowwolf <coolsnowwolf@gmail.com>
2019-04-18 19:22:53 +0800

    coremark: auto-run benchmark just onece

--- package/lean/coremark/coremark.sh
+++ package/lean/coremark/coremark.sh
@@ -5,3 +5,8 @@
 cat /tmp/coremark.log | grep "CoreMark 1.0" | cut -d  "/"  -f 1 > /tmp/bench.log
 sed -i 's/CoreMark 1.0/(CpuMark/g'  /tmp/bench.log
 echo " Scores)" >> /tmp/bench.log
+
+if [ -f "/tmp/bench.log" ]; then
+ sed -i '/coremark/d' /etc/crontabs/root
+ crontab /etc/crontabs/root
+fi
