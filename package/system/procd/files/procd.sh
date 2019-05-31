feb39de06086bd286c46b51367b1078a1a0c1736
ope087 <33145975+ope087@users.noreply.github.com>
2019-05-31 17:33:07 +0800

    Update procd.sh
    Add section 'uci_load_validate' for procd.sh only.

--- package/system/procd/files/procd.sh
+++ package/system/procd/files/procd.sh
@@ -462,8 +462,7 @@ procd_add_mdns() {
 	procd_close_data
 }
 
-uci_validate_section()
-{
+uci_validate_section() {
 	local _package="$1"
 	local _type="$2"
 	local _name="$3"
@@ -475,6 +474,23 @@ uci_validate_section()
 	eval "$_result"
 	[ "$_error" = "0" ] || `/sbin/validate_data "$_package" "$_type" "$_name" "$@" 1> /dev/null`
 	return $_error
+}
+
+uci_load_validate() {
+	local _package="$1"
+	local _type="$2"
+	local _name="$3"
+	local _function="$4"
+	local _option
+	local _result
+	shift; shift; shift; shift
+	for _option in "$@"; do
+		eval "local ${_option%%:*}"
+	done
+	uci_validate_section "$_package" "$_type" "$_name" "$@"
+	_result=$?
+	[ -n "$_function" ] || return $_result
+	eval "$_function \"\$_name\" \"\$_result\""
 }
 
 _procd_wrapper \
