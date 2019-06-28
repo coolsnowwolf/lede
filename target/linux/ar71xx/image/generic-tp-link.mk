c028b71d6e917ecb59c8daf961d7d0fef420b39c
huah0235 <huah6571@gmail.com>
2019-06-28 15:59:19 +0800

    add wdr6500v6 support

--- target/linux/ar71xx/image/generic-tp-link.mk
+++ target/linux/ar71xx/image/generic-tp-link.mk
@@ -340,6 +340,19 @@ define Device/tl-wdr6500-v2
 endef
 TARGET_DEVICES += tl-wdr6500-v2
 
+define Device/tl-wdr6500-v6
+$(Device/tplink-8mlzma)
+  DEVICE_TITLE := TP-LINK TL-WDR6500 v6
+  DEVICE_PACKAGES := kmod-ath10k ath10k-firmware-qca9888
+  KERNEL := kernel-bin | patch-cmdline | lzma | uImage lzma
+  KERNEL_INITRAMFS := kernel-bin | patch-cmdline | lzma | uImage lzma | tplink-v1-header
+  BOARDNAME = TL-WDR6500-v6
+  DEVICE_PROFILE = TLWDR6500V6
+  TPLINK_HWID := 0x65000006
+  TPLINK_HEADER_VERSION := 2
+endef
+TARGET_DEVICES += tl-wdr6500-v6
+
 define Device/mw4530r-v1
   $(Device/tl-wdr4300-v1)
   DEVICE_TITLE := Mercury MW4530R v1
