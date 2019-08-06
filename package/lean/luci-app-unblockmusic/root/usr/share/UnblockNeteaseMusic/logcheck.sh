80bdce8bea2cb6c26d6f809902011146b9a7a20e
LEAN-ESX <coolsnowwolf@gmail.com>
2019-08-06 05:46:26 -0700

    fix luci-app-unblockmusic MTV play

--- package/lean/luci-app-unblockmusic/root/usr/share/UnblockNeteaseMusic/logcheck.sh
+++ package/lean/luci-app-unblockmusic/root/usr/share/UnblockNeteaseMusic/logcheck.sh
@@ -7,6 +7,6 @@ while true
 do
 	(( log_size = "$(ls -l "${log_file}" | awk -F ' ' '{print $5}')" / "1024" ))
 	(( "${log_size}" >= "${log_max_size}" )) && echo "" > /tmp/unblockmusic.log
-	sleep 5m
+	sleep 10m
 done
 
