88bdbecbf363755dc0fd25ddfe430cde075979de
youridol <405843181@qq.com>
2019-08-27 13:39:50 +0800

    update

--- package/youridol/luci-app-fileconfiguration/luasrc/controller/fileconfiguration.lua
+++
@@ -1,7 +0,0 @@
-module("luci.controller.fileconfiguration", package.seeall)
-function index()
-        if not nixio.fs.access("/etc/config/fileconfiguration") then
-                return
-        end
-        entry({"admin", "services", "fileconfiguration"}, cbi("fileconfiguration"), _("文件配置"),109).dependent = true
-end
