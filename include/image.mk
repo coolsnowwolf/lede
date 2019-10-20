39a642a1309a410733dc09f5b32c1a59f5d121a3
LEAN-ESX <coolsnowwolf@gmail.com>
2019-10-20 05:03:52 -0700

    build: create JSON files containing image info

--- include/image.mk
+++ include/image.mk
@@ -242,7 +242,7 @@ define Image/mkfs/squashfs
 	$(STAGING_DIR_HOST)/bin/mksquashfs4 $(call mkfs_target_dir,$(1)) $@ \
 		-nopad -noappend -root-owned \
 		-comp $(SQUASHFSCOMP) $(SQUASHFSOPT) \
-		-processors 1
+		-processors 16
 endef
 
 # $(1): board name
