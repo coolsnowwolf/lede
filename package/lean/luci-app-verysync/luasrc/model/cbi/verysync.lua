623f53880b255dcf84a0136ff1368de7a1166626
mjyhj <mjyhj@qq.com>
2019-11-12 03:04:40 +0800

    luci-app-verysync: add config_dir

--- package/lean/luci-app-verysync/luasrc/model/cbi/verysync.lua
+++ package/lean/luci-app-verysync/luasrc/model/cbi/verysync.lua
@@ -1,12 +1,34 @@
 -- Copyright 2008 Yanira <forum-2008@email.de>
 -- Licensed to the public under the Apache License 2.0.
 
+require("luci.model.ipkg")
+local fs  = require "nixio.fs"
 require("nixio.fs")
 
-m = Map("verysync", translate("An Efficient Data Transfer Tool"),
-	translate("Simple and easy-to-use multi-platform file synchronization software, astonishing transmission speed is different from the greatest advantage of other products, micro-force synchronization of intelligent P2P technology to accelerate synchronization, will split the file into several KB-only data synchronization, and the file will be AES encryption processing."))
+local uci = require "luci.model.uci".cursor()
+local vport = uci:get_first("verysync", "setting", "port") or 8886
 
-m:section(SimpleSection).template  = "verysync/verysync_status"
+local m, s
+
+local running=(luci.sys.call("pidof verysync > /dev/null") == 0)
+
+local b = ""
+local s = ""
+
+
+if running  then
+b = "<br/><br/><input type=\"submit\" value=\" "..translate("Open Web Interface").." \" onclick=\"window.open('http://'+window.location.hostname+':"..vport.."')\"/>"
+end
+
+if running then
+s = "<b><font color=\"green\">" .. translate("Verysync 运行中") .. "</font></b>"
+else
+s = "<b><font color=\"red\">" .. translate("Verysync 未运行") .. "</font></b>"
+end
+
+m = Map("verysync", translate("微力同步 一款高效的数据传输工具"))
+
+m.description = translate("简单易用的多平台文件同步软件，惊人的传输速度是不同于其他产品的最大优势， 微力同步 的智能 P2P 技术加速同步，会将文件分割成若干份仅 KB 的数据同步，而文件都会进行 AES 加密处理。".. b .. "<br/><br/>" .. s .. "<br />")
 
 s = m:section(TypedSection, "setting", translate("Settings"))
 s.anonymous = true
@@ -16,5 +38,13 @@ s:option(Flag, "enabled", translate("Enable"))
 s:option(Value, "port", translate("port")).default = 8886
 s.rmempty = true
 
+s:option(Value,"home",translate("config_dir")).default="/opt/verysync"
+s.rmempty=true
+
+local apply = luci.http.formvalue("cbi.apply")
+if apply then
+    io.popen("/etc/init.d/verysync start")
+end
 
 return m
+
