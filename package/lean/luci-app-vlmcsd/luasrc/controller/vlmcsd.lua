5a48d62fa6b6e3bc772b933b1a18b77eb3ccd7e5
Twinzo1 <1282055288@qq.com>
2019-11-19 21:25:16 +0800

    Added my apps

--- package/lean/luci-app-vlmcsd/luasrc/controller/vlmcsd.lua
+++ package/lean/luci-app-vlmcsd/luasrc/controller/vlmcsd.lua
@@ -1,19 +1,33 @@
-module("luci.controller.vlmcsd", package.seeall)
-
-function index()
-	if not nixio.fs.access("/etc/config/vlmcsd") then
-		return
-	end
-	local page
-	page = entry({"admin", "services", "vlmcsd"}, cbi("vlmcsd"), _("KMS Server"), 100)
-	page.i18n = "vlmcsd"
-	page.dependent = true
-	entry({"admin","services","vlmcsd","status"},call("act_status")).leaf=true
+m=Map("vlmcsd")
+m.title=translate("vlmcsd config")
+m.description=translate("A KMS Serever Emulator to active your Windows or Office")
+m:section(SimpleSection).template="vlmcsd/vlmcsd_status"
+s=m:section(TypedSection,"vlmcsd")
+s.addremove=false
+s.anonymous=true
+s:tab("basic",translate("Basic Setting"))
+enable=s:taboption("basic",Flag,"enabled",translate("Enable"))
+enable.rmempty=false
+autoactivate=s:taboption("basic",Flag,"autoactivate",translate("Auto activate"))
+autoactivate.rmempty=false
+-------------------config file-----------------------
+s:tab("config", translate("Config File"))
+s.anonymous=true
+local a="/etc/vlmcsd.ini"
+config=s:taboption("config",TextValue,"Config_File")
+config.description=translate("This file is /etc/vlmcsd.ini.")
+config.rows=18
+config.wrap="off"
+function config.cfgvalue(s,s)
+sylogtext=""
+if a and nixio.fs.access(a)then
+Config_File=luci.sys.exec("tail -n 100 %s"%a)
+end
+return Config_File
+end
+function config.write(t,t,e)
+e=e:gsub("\r\n?","\n")
+nixio.fs.writefile("/etc/vlmcsd.ini",e)
 end
 
-function act_status()
-  local e={}
-  e.running=luci.sys.call("pgrep vlmcsd >/dev/null")==0
-  luci.http.prepare_content("application/json")
-  luci.http.write_json(e)
-end
+return m
