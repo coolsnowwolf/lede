9823711fb4e3d694418ab1bd6782790efa6d7496
LEAN-ESX <coolsnowwolf@gmail.com>
2019-11-23 18:43:43 -0800

    Revert "mac80211: update to version 5.4-rc8"
    This reverts commit 210243ca572e2d2d284da3475e2606df59560d36.

--- package/kernel/mac80211/files/lib/netifd/wireless/mac80211.sh
+++ package/kernel/mac80211/files/lib/netifd/wireless/mac80211.sh
@@ -103,12 +103,10 @@ mac80211_hostapd_setup_base() {
 	[ -n "$acs_exclude_dfs" ] && [ "$acs_exclude_dfs" -gt 0 ] &&
 		append base_cfg "acs_exclude_dfs=1" "$N"
 
-	json_get_vars noscan ht_coex
+	json_get_vars noscan ht_coex vendor_vht
 	json_get_values ht_capab_list ht_capab tx_burst
 
-	set_default noscan 0
-
-	[ "$noscan" -gt 0 ] && hostapd_noscan=1
+	[ -n "$noscan" -a "$noscan" -gt 0 ] && hostapd_noscan=1
 	[ "$tx_burst" = 0 ] && tx_burst=
 
 	ieee80211n=1
@@ -219,7 +217,7 @@ mac80211_hostapd_setup_base() {
 		;;
 	esac
 
-	if [ "$enable_ac" != "0" ]; then
+	if [ "$enable_ac" != "0" -o "$vendor_vht" = "1" ]; then
 		json_get_vars \
 			rxldpc:1 \
 			short_gi_80:1 \
