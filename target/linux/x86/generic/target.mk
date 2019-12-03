7c38a930486d95ac8122f521177c88844aa0e4bb
LEAN-ESX <coolsnowwolf@gmail.com>
2019-12-03 06:40:51 -0800

    x86: add AMD Ryzen (The Zen Core Architecture) target

--- target/linux/x86/generic/target.mk
+++ target/linux/x86/generic/target.mk
@@ -1,11 +1,11 @@
-BOARDNAME:=Generic
-CPU_TYPE :=pentium4
+BOARDNAME:=x86 32bit
+CPU_TYPE :=core2
 FEATURES += audio pci pcie usb
 DEFAULT_PACKAGES += kmod-button-hotplug
 
 define Target/Description
 	Build firmware images for modern x86 based boards with CPUs
-	supporting at least the Intel Pentium 4 instruction set with
-	MMX, SSE and SSE2.
+	supporting at least theIntel Core 2 CPU instruction set with
+	MMX, SSE, SSE2, SSE3 and SSSE3.
 endef
 
--- target/linux/x86/zen/target.mk
+++ target/linux/x86/zen/target.mk
@@ -1,5 +1,6 @@
 ARCH:=x86_64
-BOARDNAME:=x64 64bit
+BOARDNAME:=AMD Ryzen (The Zen Core Architecture)
+CPU_TYPE :=znver1
 DEFAULT_PACKAGES += kmod-button-hotplug kmod-e1000e kmod-e1000 kmod-r8169 kmod-igb kmod-bnx2
 
 define Target/Description
