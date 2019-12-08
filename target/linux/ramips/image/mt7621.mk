40f51aebdec00d634f8b7ad46c4c28861a34cf75
LEAN-ESX <coolsnowwolf@gmail.com>
2019-12-08 07:31:11 -0800

    ramips: add support for XiaoYu XY-C5

--- target/linux/ramips/image/mt7621.mk
+++ target/linux/ramips/image/mt7621.mk
@@ -130,6 +130,15 @@ define Device/mediatek_ap-mt7621a-v60
 endef
 TARGET_DEVICES += mediatek_ap-mt7621a-v60
 
+define Device/xiaoyu_xy-c5
+  MTK_SOC := mt7621
+  IMAGE_SIZE := 32448k
+  DEVICE_VENDOR := XiaoYu
+  DEVICE_MODEL := XY-C5
+  DEVICE_PACKAGES := kmod-ata-core kmod-ata-ahci kmod-usb3
+endef
+TARGET_DEVICES += xiaoyu_xy-c5
+
 define Device/xzwifi_creativebox-v1
   DTS := CreativeBox-v1
   IMAGE_SIZE := $(ralink_default_fw_size_32M)
