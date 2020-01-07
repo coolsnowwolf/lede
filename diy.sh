355040346bdb0faec090d74f9b21981bb36ddfdd
haixing1001 <50311834+haixing1001@users.noreply.github.com>
2020-01-07 12:40:45 +0800

    Build OpenWrt (macOS)

---
+++ diy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#=================================================
+# Description: DIY script
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=================================================
+# Modify default IP
+#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+git clone https://github.com/jefferymvp/luci-app-koolproxyR.git package/luci-app-koolproxyR
