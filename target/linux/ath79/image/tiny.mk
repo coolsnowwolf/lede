8d3ffe77b00bd30126232586de917a0190e335fc
AmadeusGhost <42570690+AmadeusGhost@users.noreply.github.com>
2020-02-03 16:58:51 +0800

    ath79: sync and fix wireless
    This commit sync target ath79 from openwrt master, and revert some commit which will causes wireless to not work.
    Use ath10k-ct-smallbuffers by default, so that small memory devices can normal work.

--- target/linux/ath79/image/tiny.mk
+++ target/linux/ath79/image/tiny.mk
@@ -1,20 +1,41 @@
 include ./common-buffalo.mk
 
 define Device/buffalo_whr-g301n
-  ATH_SOC := ar7240
+  SOC := ar7240
   DEVICE_VENDOR := Buffalo
   DEVICE_MODEL := WHR-G301N
   IMAGE_SIZE := 3712k
   IMAGES += factory.bin tftp.bin
-  IMAGE/default := append-kernel | pad-to $$$$(BLOCKSIZE) | append-rootfs | pad-rootfs | check-size $$$$(IMAGE_SIZE)
-  IMAGE/factory.bin := $$(IMAGE/default) | buffalo-enc WHR-G301N 1.99 | buffalo-tag WHR-G301N 3
+  IMAGE/default := append-kernel | pad-to $$$$(BLOCKSIZE) | append-rootfs | \
+	pad-rootfs | check-size $$$$(IMAGE_SIZE)
+  IMAGE/factory.bin := $$(IMAGE/default) | buffalo-enc WHR-G301N 1.99 | \
+	buffalo-tag WHR-G301N 3
   IMAGE/tftp.bin := $$(IMAGE/default) | buffalo-tftp-header
   SUPPORTED_DEVICES += whr-g301n
 endef
 TARGET_DEVICES += buffalo_whr-g301n
 
+define Device/dlink_dir-615-e4
+  SOC := ar7240
+  DEVICE_VENDOR := D-Link
+  DEVICE_MODEL := DIR-615
+  DEVICE_VARIANT := E4
+  IMAGE_SIZE := 3776k
+  FACTORY_IMAGE_SIZE := 3456k
+  IMAGES += factory.bin
+  IMAGE/default := append-kernel | append-rootfs | pad-rootfs
+  IMAGE/sysupgrade.bin := $$(IMAGE/default) | append-metadata | \
+	check-size $$$$(IMAGE_SIZE)
+  IMAGE/factory.bin := $$(IMAGE/default) | \
+	check-size $$$$(FACTORY_IMAGE_SIZE) | pad-to $$$$(FACTORY_IMAGE_SIZE) | \
+	append-string "AP99-AR7240-RT-091105-05"
+  SUPPORTED_DEVICES += dir-615-e4
+  DEFAULT := n
+endef
+TARGET_DEVICES += dlink_dir-615-e4
+
 define Device/pqi_air-pen
-  ATH_SOC := ar9330
+  SOC := ar9330
   DEVICE_VENDOR := PQI
   DEVICE_MODEL := Air-Pen
   DEVICE_PACKAGES := kmod-usb2
