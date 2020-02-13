e17403ee3718e866307296a7553ce465848d2700
AmadeusGhost <42570690+AmadeusGhost@users.noreply.github.com>
2020-02-13 20:25:40 +0800

    Revert "add package luci-app-nfs (#2908)"
    This reverts commit 9b593a88b07217315c239ca92d68d2754a3d062d.

--- package/lean/luci-app-nfs/luasrc/controller/nfs.lua
+++
@@ -1,8 +0,0 @@
-module("luci.controller.nfs", package.seeall)
-
-function index()
-	if not nixio.fs.access("/etc/config/nfs") then
-		return
-	end
-	entry({"admin", "nas", "nfs"}, cbi("nfs"), _("NFS Manage"), 5).dependent = true
-end
