0ccf9b831d6c985b44de7be864707310bf08a509
coolsnowwolf <coolsnowwolf@gmail.com>
2020-02-18 13:29:49 +0800

    luci-app-unblockmusic: fix log_check sh shell

--- package/lean/luci-app-unblockmusic/root/usr/share/UnblockNeteaseMusic/logcheck.sh
+++ package/lean/luci-app-unblockmusic/root/usr/share/UnblockNeteaseMusic/logcheck.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-log_max_size="100" #使用KB计算
+log_max_size=100
 log_file="/tmp/unblockmusic.log"
+log_size=0
 
 while true
 do
-	sleep 10s
+	sleep 10m
 	icount=`busybox ps -w | grep UnblockNeteaseMusic/app.js |grep -v grep`
 	[ -z "$icount" ] && /etc/init.d/unblockmusic restart 
-	(( log_size = "$(ls -l "${log_file}" | awk -F ' ' '{print $5}')" / "1024" ))
-	(( "${log_size}" >= "${log_max_size}" )) && echo "" > /tmp/unblockmusic.log
-	sleep 10m
+	log_size=$(expr $(ls -l $log_file | awk '{print $5}') / 1024)
+	[ $log_size -ge $log_max_size ] && echo "$(date -R) # Start UnblockNeteaseMusic" >/tmp/unblockmusic.log
 done
