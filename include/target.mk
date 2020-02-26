5fc9d20c749b0ad624d8e2636a3e6e94b3aa2fba
mitsukileung <38887306+mitsukileung@users.noreply.github.com>
2020-02-26 19:53:26 +0800

    Update target.mk

--- include/target.mk
+++ include/target.mk
@@ -16,7 +16,7 @@ DEVICE_TYPE?=router
 DEFAULT_PACKAGES:=base-files libc libgcc busybox dropbear mtd uci opkg netifd fstools uclient-fetch logd urandom-seed urngd \
 block-mount coremark kmod-nf-nathelper kmod-nf-nathelper-extra kmod-ipt-raw wget libustream-openssl ca-certificates \
 default-settings luci luci-app-ddns luci-app-upnp luci-app-adbyby-plus luci-app-autoreboot \
-luci-app-ssr-plus luci-app-unblockmusic \
+luci-app-ssr-plus luci-app-unblockneteasemusic-mini \
 luci-app-arpbind luci-app-vlmcsd luci-app-ramfree \
 luci-app-sfe luci-app-flowoffload \
 ddns-scripts_aliyun ddns-scripts_dnspod
