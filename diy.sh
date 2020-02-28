bd7cc2218d63183a6faa91bb76f59c1b072fa1f3
shake <shake.chen@gmail.com>
2020-02-29 02:01:43 +0800

    change ip

--- diy.sh
+++ diy.sh
@@ -6,4 +6,4 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-#sed -i 's/192.168.199.10/192.168.199.15/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.199.10/192.168.199.15/g' package/base-files/files/bin/config_generate
