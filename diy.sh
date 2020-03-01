0aa1ce5cc11294537a6888d35d65a64877c11109
shake <shake.chen@gmail.com>
2020-03-01 19:28:19 +0800

    try to add outside package

--- diy.sh
+++ diy.sh
@@ -7,3 +7,10 @@
 #=================================================
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.199.10/g' package/base-files/files/bin/config_generate
+
+#拦截广告的app
+
+cd package
+mkdir openwrt-packages
+cd openwrt-packages
+git clone https://github.com/Ameykyl/luci-app-koolproxyR
