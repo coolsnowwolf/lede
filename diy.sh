0b0d2d57e6a90b8a0e1f2139610d068b4d77907a
shake <shake.chen@gmail.com>
2020-03-01 19:48:29 +0800

    change the github address

--- diy.sh
+++ diy.sh
@@ -13,5 +13,5 @@ sed -i 's/192.168.1.1/192.168.199.10/g' package/base-files/files/bin/config_gene
 cd package
 mkdir openwrt-packages
 cd openwrt-packages
-git clone https://github.com/Ameykyl/luci-app-koolproxyR
+git clone https://github.com/Leo-Jo/luci-app-koolproxyR.git
 git clone https://github.com/jerrykuku/luci-theme-argon.git
