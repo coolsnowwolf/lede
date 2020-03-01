22521f654faa28c5691b7438918012e84322299f
SharerMax <mdcw1103@gmail.com>
2020-03-01 05:49:56 +0000

    luci-app-ssr-plus: bump versio 154-2

--- package/lean/luci-app-ssr-plus/root/usr/share/shadowsocksr/subscribe.lua
+++ package/lean/luci-app-ssr-plus/root/usr/share/shadowsocksr/subscribe.lua
@@ -238,6 +238,9 @@ local function processData(szType, content)
 		result.alias = UrlDecode(alias)
 		result.type = "trojan"
 		result.server = host[1]
+		-- 按照官方的建议 默认验证ssl证书
+		result.insecure = "0"
+		result.tls = "1"
 		if host[2]:find("?") then
 			local query = split(host[2], "?")
 			result.server_port = query[1]
@@ -246,12 +249,12 @@ local function processData(szType, content)
 				local t = split(v, '=')
 				params[t[1]] = t[2]
 			end
-			-- 按照官方的建议 默认验证ssl证书
-			result.tls = "1"
+			
 			if params.peer then
 				-- 未指定peer（sni）默认使用remote addr
 				result.tls_host = params.peer
 			end
+			
 			if params.allowInsecure == "1" then
 				result.insecure = "1"
 			else
