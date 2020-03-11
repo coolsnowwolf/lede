4aa5b4a9e7b63acf3be9724a50ee77d2ae1376bc
LEAN-ESX <coolsnowwolf@gmail.com>
2020-03-10 19:50:23 -0700

    luci-app-adbyby-plus: update adblock.sh

--- package/lean/luci-app-adbyby-plus/root/usr/share/adbyby/adblock.sh
+++ package/lean/luci-app-adbyby-plus/root/usr/share/adbyby/adblock.sh
@@ -1,14 +1,8 @@
 #!/bin/sh
 
-rm -f /tmp/dnsmasq.adblock
-
-wget-ssl --no-check-certificate -O- https://easylist-downloads.adblockplus.org/easylistchina+easylist.txt | grep ^\|\|[^\*]*\^$ | sed -e 's:||:address\=\/:' -e 's:\^:/0\.0\.0\.0:' > /tmp/dnsmasq.adblock
-if [ -s "/tmp/dnsmasq.adblock" ];then
-	sed -i '/youku.com/d' /tmp/dnsmasq.adblock
-	if ( ! cmp -s /tmp/dnsmasq.adblock /usr/share/adbyby/dnsmasq.adblock );then
-		mv /tmp/dnsmasq.adblock /usr/share/adbyby/dnsmasq.adblock	
-	fi	
+wget-ssl --no-check-certificate -O - 'https://easylist-downloads.adblockplus.org/easylistchina+easylist.txt' > /tmp/adnew.conf
+if [ -s "/tmp/adnew.conf" ];then
+  /usr/share/adbyby/ad-update
 fi
 
-rm -rf /tmp/adbyby.updated && /etc/init.d/adbyby restart
-
+rm -f /tmp/adbyby.updated && /etc/init.d/adbyby restart
