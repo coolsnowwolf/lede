55cf815a79ade5eed420be198eed1b43c115721e
Jinhy1015 <58725278+Jinhy1015@users.noreply.github.com>
2020-03-22 17:06:17 +0800

    Create diy.sh

---
+++ diy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#=================================================
+# Description: DIY script
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=================================================
+# Modify default IP
+#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
