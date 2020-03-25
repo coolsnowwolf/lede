365433be7c24291db6a0a2537c3c395a603fc6c5
Mattraks <16359027+Mattraks@users.noreply.github.com>
2020-03-26 00:38:57 +0800

    luci-app-ssr-plus:Fix update.lua (#4018)

--- package/lean/luci-app-ssr-plus/root/usr/share/shadowsocksr/update.lua
+++ package/lean/luci-app-ssr-plus/root/usr/share/shadowsocksr/update.lua
@@ -15,7 +15,7 @@ local log = function(...)
 end
 
 log('正在更新【GFW列表】数据库')
-refresh_cmd = "wget-ssl --no-check-certificate -O- " .. uci:get_first('shadowsocksr', 'global', 'gfwlist_url', 'https://cdn.jsdelivr.net/gh/gfwlist/gfwlist/gfwlist.txt') .. ' > /tmp//tmp/gfw.b64'
+refresh_cmd = "wget-ssl --no-check-certificate -O- " .. uci:get_first('shadowsocksr', 'global', 'gfwlist_url', 'https://cdn.jsdelivr.net/gh/gfwlist/gfwlist/gfwlist.txt') .. " > /tmp/gfw.b64"
 sret = luci.sys.call(refresh_cmd .. " 2>/dev/null")
 if sret == 0 then
 	luci.sys.call("/usr/bin/ssr-gfw")
