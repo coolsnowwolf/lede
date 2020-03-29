af4b19df3360a5db2fc84d68e0885696c466e0d8
Mattraks <16359027+Mattraks@users.noreply.github.com>
2020-03-29 19:16:26 +0800

    luci-app-ssr-plus:Code optimization and Fix Bug

--- package/lean/luci-app-ssr-plus/luasrc/model/cbi/shadowsocksr/control.lua
+++ package/lean/luci-app-ssr-plus/luasrc/model/cbi/shadowsocksr/control.lua
@@ -1,5 +1,6 @@
+require "luci.ip"
+require "nixio.fs"
 local m, s, o
-local NXFS = require "nixio.fs"
 
 m = Map("shadowsocksr", translate("IP black-and-white list"))
 
@@ -27,9 +28,9 @@ o.rmempty = false
 o = s:taboption("lan_ac", DynamicList, "lan_ac_ips", translate("LAN Host List"))
 o.datatype = "ipaddr"
 luci.ip.neighbors({ family = 4 }, function(entry)
-		if entry.reachable then
-			o:value(entry.dest:string())
-		end
+	if entry.reachable then
+		o:value(entry.dest:string())
+	end
 end)
 o:depends("lan_ac_mode", "w")
 o:depends("lan_ac_mode", "b")
@@ -37,25 +38,25 @@ o:depends("lan_ac_mode", "b")
 o = s:taboption("lan_ac", DynamicList, "lan_bp_ips", translate("LAN Bypassed Host List"))
 o.datatype = "ipaddr"
 luci.ip.neighbors({ family = 4 }, function(entry)
-		if entry.reachable then
-			o:value(entry.dest:string())
-		end
+	if entry.reachable then
+		o:value(entry.dest:string())
+	end
 end)
 
 o = s:taboption("lan_ac", DynamicList, "lan_fp_ips", translate("LAN Force Proxy Host List"))
 o.datatype = "ipaddr"
 luci.ip.neighbors({ family = 4 }, function(entry)
-		if entry.reachable then
-			o:value(entry.dest:string())
-		end
+	if entry.reachable then
+		o:value(entry.dest:string())
+	end
 end)
 
 o = s:taboption("lan_ac", DynamicList, "lan_gm_ips", translate("Game Mode Host List"))
 o.datatype = "ipaddr"
 luci.ip.neighbors({ family = 4 }, function(entry)
-		if entry.reachable then
-			o:value(entry.dest:string())
-		end
+	if entry.reachable then
+		o:value(entry.dest:string())
+	end
 end)
 
 -- Part of Self
@@ -66,7 +67,7 @@ end)
 -- o:value("2", translatef("Forwarded Proxy"))
 -- o.rmempty = false
 
-s:tab("esc",  translate("Bypass Domain List"))
+s:tab("esc", translate("Bypass Domain List"))
 
 local escconf = "/etc/config/white.list"
 o = s:taboption("esc", TextValue, "escconf")
@@ -74,17 +75,17 @@ o.rows = 13
 o.wrap = "off"
 o.rmempty = true
 o.cfgvalue = function(self, section)
-	return NXFS.readfile(escconf) or ""
+	return nixio.fs.readfile(escconf) or ""
 end
 o.write = function(self, section, value)
-	NXFS.writefile(escconf, value:gsub("\r\n", "\n"))
+	nixio.fs.writefile(escconf, value:gsub("\r\n", "\n"))
 end
 o.remove = function(self, section, value)
-	NXFS.writefile(escconf, "")
+	nixio.fs.writefile(escconf, "")
 end
 
 
-s:tab("block",  translate("Black Domain List"))
+s:tab("block", translate("Black Domain List"))
 
 local blockconf = "/etc/config/black.list"
 o = s:taboption("block", TextValue, "blockconf")
@@ -92,16 +93,16 @@ o.rows = 13
 o.wrap = "off"
 o.rmempty = true
 o.cfgvalue = function(self, section)
-	return NXFS.readfile(blockconf) or " "
+	return nixio.fs.readfile(blockconf) or " "
 end
 o.write = function(self, section, value)
-	NXFS.writefile(blockconf, value:gsub("\r\n", "\n"))
+	nixio.fs.writefile(blockconf, value:gsub("\r\n", "\n"))
 end
 o.remove = function(self, section, value)
-	NXFS.writefile(blockconf, "")
+	nixio.fs.writefile(blockconf, "")
 end
 
-s:tab("netflix",  translate("Netflix Domain List"))
+s:tab("netflix", translate("Netflix Domain List"))
 
 local netflixconf = "/etc/config/netflix.list"
 o = s:taboption("netflix", TextValue, "netflixconf")
@@ -109,16 +110,16 @@ o.rows = 13
 o.wrap = "off"
 o.rmempty = true
 o.cfgvalue = function(self, section)
-	return NXFS.readfile(netflixconf) or " "
+	return nixio.fs.readfile(netflixconf) or " "
 end
 o.write = function(self, section, value)
-	NXFS.writefile(netflixconf, value:gsub("\r\n", "\n"))
+	nixio.fs.writefile(netflixconf, value:gsub("\r\n", "\n"))
 end
 o.remove = function(self, section, value)
-	NXFS.writefile(netflixconf, "")
+	nixio.fs.writefile(netflixconf, "")
 end
 
-s:tab("netflixip",  translate("Netflix IP List"))
+s:tab("netflixip", translate("Netflix IP List"))
 
 local netflixipconf = "/etc/config/netflixip.list"
 o = s:taboption("netflixip", TextValue, "netflixipconf")
@@ -126,13 +127,13 @@ o.rows = 13
 o.wrap = "off"
 o.rmempty = true
 o.cfgvalue = function(self, section)
-	return NXFS.readfile(netflixipconf) or " "
+	return nixio.fs.readfile(netflixipconf) or " "
 end
 o.write = function(self, section, value)
-	NXFS.writefile(netflixipconf, value:gsub("\r\n", "\n"))
+	nixio.fs.writefile(netflixipconf, value:gsub("\r\n", "\n"))
 end
 o.remove = function(self, section, value)
-	NXFS.writefile(netflixipconf, "")
+	nixio.fs.writefile(netflixipconf, "")
 end
 
 return m
