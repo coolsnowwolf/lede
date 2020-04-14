f7cdc5460280173de9cdeb01dabf88836d948e4a
AmadeusGhost <42570690+AmadeusGhost@users.noreply.github.com>
2020-04-14 09:22:11 +0800

    ipq40xx: add missing device config for rt-ac1300uhp (#4300)

--- target/linux/ipq40xx/base-files/lib/upgrade/platform.sh
+++ target/linux/ipq40xx/base-files/lib/upgrade/platform.sh
@@ -172,7 +172,8 @@ platform_do_upgrade() {
 		nand_do_upgrade "$1"
 		;;
 	asus,rt-acrh17|\
-	asus,rt-ac58u)
+	asus,rt-ac58u|\
+	asus,rt-ac1300uhp)
 		local magic=$(get_magic_long "$1")
 		CI_UBIPART="UBI_DEV"
 		CI_KERNPART="linux"
