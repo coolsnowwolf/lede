20c9b0561d6fd48c57ed4a175b5451411de70072
yjw <yjw8886@gmail.com>
2020-05-31 00:57:45 +0800

    use feeds.conf

--- package/lean/luci-app-ssr-plus/root/usr/share/shadowsocksr/chinaipset.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-LOCK_FILE="/var/lock/ssr-chinaipset.lock"
-[ -f "$LOCK_FILE" ] && exit 2
-touch "$LOCK_FILE"
-echo "create china hash:net family inet hashsize 1024 maxelem 65536" >/tmp/china.ipset
-awk '!/^$/&&!/^#/{printf("add china %s'" "'\n",$0)}' /etc/ssr/china_ssr.txt >>/tmp/china.ipset
-ipset -! flush china
-ipset -! restore </tmp/china.ipset 2>/dev/null
-rm -f /tmp/china.ipset $LOCK_FILE
