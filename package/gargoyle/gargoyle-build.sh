a1178d1b0e8d2186228b648512d3c92d9f1997e8
Twinzo1 <1282055288@qq.com>
2020-06-11 06:54:25 +0800

    try to fix gargoyle package

--- package/gargoyle/gargoyle-build.sh
+++ package/gargoyle/gargoyle-build.sh
@@ -8,5 +8,8 @@ scripts/patch-kernel.sh . "$patches_dir/" >/dev/null 2>&1
 # x86还需要一个patch
 sh ./package/gargoyle/netfilter-match-modules/*.sh . ./package/gargoyle/netfilter-match-modules 1 1
 #参考https://github.com/project-openwrt/openwrt/commit/47040d246daceff23739e44f4cbb38ebbf3bb593，为qos-gargoyle打补丁
-
+ifExist=`cat ./package/network/utils/iproute2/Makefile | grep "Hack for qos-gargoyle"`
+ifExist0=`cat ./package/gargoyle/qos-gargoyle/src/Makefile | grep "/usr/lib/iproute2"`
+[ -z "$ifExist" ] && patch -p0<./package/gargoyle/patches/qos-gargoyle.patch
+[ -z "$ifExist0" ] && patch -p0<./package/gargoyle/patches/qosmon.patch
 echo "gargoyle patch complete"
