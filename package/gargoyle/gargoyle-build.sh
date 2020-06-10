b8e172a6f28e3e1d96bc628ce653c969bc3ab1bd
Twinzo1 <44726207+Twinzo1@users.noreply.github.com>
2020-06-10 11:11:57 +0800

    Update gargoyle-build.sh

--- package/gargoyle/gargoyle-build.sh
+++ package/gargoyle/gargoyle-build.sh
@@ -5,7 +5,7 @@ target_name=$(cat .config | egrep  "CONFIG_TARGET_([^_]+)=y" | sed 's/^.*_//g' |
 kernel_a=$(cat .config | grep CONFIG_LINUX | grep "=y" | awk -F "_" '{print $3}')
 kernel_b=$(cat .config | grep CONFIG_LINUX | grep "=y" | sed 's/^.*_//g' | sed 's/=y$//g')
 scripts/patch-kernel.sh . "$patches_dir/" >/dev/null 2>&1
-scripts/patch-kernel.sh . "target/linux/$target_name/patches-${kernel_a}.$kernel_b/" >/dev/null 2>&1
+# x86还需要一个patch
 sh ./package/gargoyle/netfilter-match-modules/*.sh . ./package/gargoyle/netfilter-match-modules 1 1
 #参考https://github.com/project-openwrt/openwrt/commit/47040d246daceff23739e44f4cbb38ebbf3bb593，为qos-gargoyle打补丁
 
