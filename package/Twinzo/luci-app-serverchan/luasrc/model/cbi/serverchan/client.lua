78737e2e2731b8388881d295424f5f2ca9201f5a
Twinzo1 <1282055288@qq.com>
2020-07-21 12:43:57 +0800

    move serverchan to feeds

--- package/Twinzo/luci-app-serverchan/luasrc/model/cbi/serverchan/client.lua
+++
@@ -1,6 +0,0 @@
-f = SimpleForm("serverchan")
-luci.sys.call("/usr/bin/serverchan/serverchan client")
-f.reset = false
-f.submit = false
-f:append(Template("serverchan/client"))
-return f
--- package/Twinzo/luci-app-serverchan/luasrc/model/cbi/serverchan/log.lua
+++
@@ -1,5 +0,0 @@
-f = SimpleForm("serverchan")
-f.reset = false
-f.submit = false
-f:append(Template("serverchan/log"))
-return f
