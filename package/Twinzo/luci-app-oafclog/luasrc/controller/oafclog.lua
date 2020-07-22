b82c988b979dd79f17fff71b896bb7fbdf6a76e3
Twinzo1 <1282055288@qq.com>
2020-07-22 10:29:19 +0800

    插件以feeds形式安装

--- package/Twinzo/luci-app-oafclog/luasrc/controller/oafclog.lua
+++
@@ -1,8 +0,0 @@
-module("luci.controller.oafclog",package.seeall)
-function index()
-	
-	local page
-	
-	page = entry({"admin", "control", "oafControlLog"}, cbi("oafclog"), _("应用日志"), 46)
-	page.dependent = true
-end
