4f3625217cae337497f8b1bb8334e99b5c40712f
Twinzo1 <44726207+Twinzo1@users.noreply.github.com>
2020-07-30 07:56:05 +0800

    Update configEditor.lua

--- package/Twinzo/luci-app-CE/luasrc/controller/configEditor.lua
+++ package/Twinzo/luci-app-CE/luasrc/controller/configEditor.lua
@@ -98,7 +98,7 @@ function get_upgrade()
 	if id == "read" then
 		retstring = "1"
 		keep_d = sys.exec("ls /lib/upgrade/keep.d")
-		ETC_keep_d = sys.exec("ls /lib/upgrade/keep.d")
+		ETC_keep_d = sys.exec("ls /etc/keep.d")
 		list = sys.exec(
 				"( find $(sed -ne '/^[[:space:]]*$/d; /^#/d; p' /etc/sysupgrade.conf " ..
 				"/lib/upgrade/keep.d/* 2>/dev/null) -type f 2>/dev/null; " ..
