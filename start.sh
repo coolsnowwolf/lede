33dc948bd01bd5b3cb68d8752ee5453d60694b96
Rainy season boy <58517274+chenaidairong@users.noreply.github.com>
2020-08-20 23:28:39 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 #此脚本仅用于Ubuntu
 sudo apt-get -y install build-essential asciidoc binutils bzip2 gawk gettext git libncurses5-dev libz-dev patch python3 python2.7 unzip zlib1g-dev lib32gcc1 libc6-dev-i386 subversion flex uglifyjs git-core gcc-multilib p7zip p7zip-full msmtp libssl-dev texinfo libglib2.0-dev xmlto qemu-utils upx libelf-dev autoconf automake libtool autopoint device-tree-compiler g++-multilib antlr3 gperf wget swig rsync
 echo "创建用户：tt"
-sudo adduser tt
+sudo adduser 123
 cd /home/tt
 #github加速
 git https://hub.fastgit.org/chenaidairong/lede.git
