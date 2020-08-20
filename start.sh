c47aab14b273704f03b193ff88d8d40a824839df
Rainy season boy <58517274+chenaidairong@users.noreply.github.com>
2020-08-20 23:27:56 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,11 +3,9 @@
 sudo apt-get -y install build-essential asciidoc binutils bzip2 gawk gettext git libncurses5-dev libz-dev patch python3 python2.7 unzip zlib1g-dev lib32gcc1 libc6-dev-i386 subversion flex uglifyjs git-core gcc-multilib p7zip p7zip-full msmtp libssl-dev texinfo libglib2.0-dev xmlto qemu-utils upx libelf-dev autoconf automake libtool autopoint device-tree-compiler g++-multilib antlr3 gperf wget swig rsync
 echo "创建用户：tt"
 sudo adduser tt
-echo "请输入密码"
-su tt
-cd
+cd /home/tt
 #github加速
-https://hub.fastgit.org/chenaidairong/lede.git
+git https://hub.fastgit.org/chenaidairong/lede.git
 cd lede
 ./scripts/feeds update -a
 ./scripts/feeds install -a
