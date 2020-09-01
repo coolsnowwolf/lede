88d85bb574ad8583b1ecd9057bcba114b0d9229e
chongshengB <53459145+chongshengB@users.noreply.github.com>
2020-09-01 17:21:00 +0800

    kernel: bump 5.4 to 5.4.61 (#5399)
    Manually merged:
      backport-5.4
         011-kbuild-export-SUBARCH.patch
      layerscape
        701-net-0262-net-dsa-ocelot-add-tagger-for-Ocelot-Felix-switches.patch
    
    All other modifications made by update_kernel.sh
    
    Build-tested: x86/64, lantiq/xrx200, ramips/mt7621
    Run-tested: ipq806x (R7800), lantiq/xrx200, x86/64, ramips (RT-AC57U)
    
    No dmesg regressions, everything functional
    
    Signed-off-by: John Audia <graysky@archlinux.us>
    [minor commit message adjustments]
    Signed-off-by: Adrian Schmutzler <freifunk@adrianschmutzler.de>
    
    Co-authored-by: John Audia <graysky@archlinux.us>

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -8,11 +8,11 @@ endif
 
 LINUX_VERSION-4.14 = .195
 LINUX_VERSION-4.19 = .138
-LINUX_VERSION-5.4 = .60
+LINUX_VERSION-5.4 = .61
 
 LINUX_KERNEL_HASH-4.14.195 = 394f28798670240baacd9e2cce521fbd79f8da5e1fc191695b0e11381445a021
 LINUX_KERNEL_HASH-4.19.138 = d15c27d05f6c527269b75b30cc72972748e55720e7e00ad8abbaa4fe3b1d5e02
-LINUX_KERNEL_HASH-5.4.60 = add2ab2385c40fc9a3dfebe403e56da8500b633dc7dc42cf0c670c61d151a223
+LINUX_KERNEL_HASH-5.4.61 = 86f13d050f6389c5a1727fa81510ee8eceac795297bc584f443354609617fea4
 
 remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
 sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
