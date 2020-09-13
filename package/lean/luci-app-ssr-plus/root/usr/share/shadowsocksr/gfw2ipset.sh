0ed31b093672871ae0d3f74066c7b3da13952248
Rainy season boy <58517274+chenaidairong@users.noreply.github.com>
2020-09-13 16:58:01 +0800

    Add files via upload

---
+++ package/lean/luci-app-ssr-plus/root/usr/share/shadowsocksr/gfw2ipset.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+awk '!/^$/&&!/^#/{printf("ipset=/.%s/'"gfwlist"'\n",$0)}' /etc/config/gfw.list > /etc/dnsmasq.ssr/custom_forward.conf
+awk '!/^$/&&!/^#/{printf("server=/.%s/'"127.0.0.1#5335"'\n",$0)}' /etc/config/gfw.list >> /etc/dnsmasq.ssr/custom_forward.conf
+
