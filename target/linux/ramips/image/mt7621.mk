fe9493b3aad87ec4e28af186b74e465399323aba
langliang3241 <49267148+langliang3241@users.noreply.github.com>
2020-12-07 11:35:57 +0800

    更改京东云  IMAGE_SIZE := 324488k
    完全利用32M Flash空间

--- target/linux/ramips/image/mt7621.mk
+++ target/linux/ramips/image/mt7621.mk
@@ -513,7 +513,7 @@ endef
 TARGET_DEVICES += jcg_y2
 
 define Device/jdcloud_re-sp-01b
-  IMAGE_SIZE := 27328k
+  IMAGE_SIZE := 32448k
   DEVICE_VENDOR := JDCloud
   DEVICE_MODEL := RE-SP-01B
   DEVICE_PACKAGES := kmod-fs-ext4 kmod-mt7603 kmod-mt7615e kmod-sdhci-mt7620 \
