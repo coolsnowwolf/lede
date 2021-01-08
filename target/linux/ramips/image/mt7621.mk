f6381d20944098e0079eccc18737bd2ef2ec4d7b
lean <coolsnowwolf@gmail.com>
2021-01-08 22:55:29 +0800

    revert: fix k2p 32m reboot problems

--- target/linux/ramips/image/mt7621.mk
+++ target/linux/ramips/image/mt7621.mk
@@ -833,7 +833,7 @@ define Device/phicomm_k2p-32m
   $(Device/phicomm_k2p)
   IMAGE_SIZE := 32128k
   DEVICE_VARIANT := 32M
-  SUPPORTED_DEVICES += k2p-32m
+  SUPPORTED_DEVICES += k2p-32M
 endef
 TARGET_DEVICES += phicomm_k2p-32m
 
