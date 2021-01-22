66d7f9bdaa4d8523e75f7ce242e42d3d45133c17
harleywong <34861262+harleywong@users.noreply.github.com>
2021-01-22 09:02:16 +0800

    京东签到
    京东签到

--- package/lean/luci-app-jd-dailybonus/luasrc/model/cbi/jd-dailybonus/client.lua
+++ package/lean/luci-app-jd-dailybonus/luasrc/model/cbi/jd-dailybonus/client.lua
@@ -85,7 +85,7 @@ o.default = 1
 o.rmempty = true
 o:depends('auto_update', '1')
 
-o = s:option(ListValue, 'remote_url', translate('Source Update Url'))
+o = s:option(ListValue, 'remote_url', translate('更新源地址'))
 o:value('https://raw.githubusercontent.com/NobyDa/Script/master/JD-DailyBonus/JD_DailyBonus.js', translate('GitHub'))
 o:value('https://gitee.com/jerrykuku/staff/raw/master/JD_DailyBonus.js', translate('Gitee'))
 o.default = 'nil'
