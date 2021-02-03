84bb1aaef69e02ccde2810493aa12db91c727df2
dreamcmi <dreamcmi@qq.com>
2021-02-02 20:36:25 -0600

    add mt1300 wireless support (#6311)
    * 添加glinet-mt1300支持
    
    * 优化更新mt1300支持
    
    * Fix mt1300 wireless unavailability problem
    
    * Fix mt1300 wireless unavailability problem

--- target/linux/ramips/image/mt7621.mk
+++ target/linux/ramips/image/mt7621.mk
@@ -360,7 +360,7 @@ define Device/glinet_gl-mt1300
   IMAGE_SIZE := 32448k
   DEVICE_VENDOR := GL.iNet
   DEVICE_MODEL := GL-MT1300
-  DEVICE_PACKAGES := kmod-mt7615e kmod-mt7615-firmware kmod-usb3
+  DEVICE_PACKAGES := kmod-usb3  kmod-mt7615d_dbdc
 endef
 TARGET_DEVICES += glinet_gl-mt1300
 
