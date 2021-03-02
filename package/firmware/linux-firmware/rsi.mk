e0b9637b8d5b79c58e22f5fc580bb97bb0d38660
whave <61507061+whave@users.noreply.github.com>
2021-03-02 16:44:53 +0800

    Delete package/firmware directory

--- package/firmware/linux-firmware/rsi.mk
+++
@@ -1,6 +0,0 @@
-Package/rs9113-firmware = $(call Package/firmware-default,RedPine Signals rs9113 firmware)
-define Package/rs9113-firmware/install
-	$(INSTALL_DIR) $(1)/lib/firmware/rsi
-	$(INSTALL_DATA) $(PKG_BUILD_DIR)/rsi/rs9113_wlan_qspi.rps $(1)/lib/firmware/rsi
-endef
-$(eval $(call BuildPackage,rs9113-firmware))
