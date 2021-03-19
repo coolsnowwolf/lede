coolsnowwolf/lede

commit f4b1b358fe8b4f2c92f5136139ef7d23cd88dbfe

rockchip: sync latest codes from ImmortalWrt (#6522)
uboot:
 - immortalwrt/immortalwrt@7a042afdee uboot-rockchip: fix RockPro64 boot from eMMC
 - immortalwrt/immortalwrt@f450acefb0 uboot-rockchip: update to v2021.01
 - immortalwrt/immortalwrt@5cc1d3df78 rockchip: improve support for NanoPi R4S
 - immortalwrt/immortalwrt@83d3fb9634 uboot-rockchip: add NanoPi R4S support
 - immortalwrt/immortalwrt@3d86ffedd7 uboot-rockchip: update to v2020.10
 - immortalwrt/immortalwrt@fe5fa0c7ab Revert "uboot-rockchip: fix nanopi-r2s failed to boot on some sd cards"

target:
 - immortalwrt/immortalwrt@f67522e532 rockchip: add support for Radxa Rock Pi 4
 - immortalwrt/immortalwrt@cd7fd0c77b rockchip: add NanoPi R4S support
 - immortalwrt/immortalwrt@02395293a2 rockchip: fix SUPPORTED_DEVICES for Radxa ROCK Pi 4
 - immortalwrt/immortalwrt@c7b200b8a2 rockchip: re-order patches
 - immortalwrt/immortalwrt@aee038e773 rockchip: refresh Kconfig
 - immortalwrt/immortalwrt@22061b2880 kernel: enable CONFIG_LEDS_TRIGGER_HEARTBEAT
 - immortalwrt/immortalwrt@b1150de9e4 rockchip: add missing Kconfig symbols
 - immortalwrt/immortalwrt@ab28d07c3b treewide: fix spelling 'seperate' -> 'separate'
 - immortalwrt/immortalwrt@6401fca74d rockchip: use native mac address for NanoPi R4S

Signed-off-by: Tianling Shen <cnsztl@immortalwrt.org>

target/linux/rockchip/armv8/base-files/etc/board.d/02_network
@@ -9,8 +9,7 @@ rockchip_setup_interfaces()
 
 	case "$board" in
 	friendlyarm,nanopi-r2s|\
-	friendlyarm,nanopi-r4s-1gb|\
-	friendlyarm,nanopi-r4s-4gb)
+	friendlyarm,nanopi-r4s)
 		ucidef_set_interfaces_lan_wan 'eth1' 'eth0'
 		;;
 	*)
@@ -19,9 +18,9 @@ rockchip_setup_interfaces()
 	esac
 }
 
-nanopi_generate_mac()
+nanopi_r2s_generate_mac()
 {
-	local sd_hash=$(sha256sum /sys/devices/platform/*.dwmmc/mmc_host/mmc0/mmc0:*/cid)
+	local sd_hash=$(sha256sum /sys/devices/platform/ff500000.dwmmc/mmc_host/mmc0/mmc0:*/cid)
 	local mac_base=$(macaddr_canonicalize "$(echo "${sd_hash}" | dd bs=1 count=12 2>/dev/null)")
 	echo "$(macaddr_unsetbit_mc "$(macaddr_setbit_la "${mac_base}")")"
 }
@@ -34,10 +33,12 @@ rockchip_setup_macs()
 	local label_mac=""
 
 	case "$board" in
-	friendlyarm,nanopi-r2s|\
-	friendlyarm,nanopi-r4s-1gb|\
-	friendlyarm,nanopi-r4s-4gb)
-		wan_mac=$(nanopi_generate_mac)
+	friendlyarm,nanopi-r2s)
+		wan_mac=$(nanopi_r2s_generate_mac)
+		lan_mac=$(macaddr_add "$wan_mac" +1)
+		;;
+	friendlyarm,nanopi-r4s)
+		wan_mac=$(cat /sys/class/net/eth0/address)
 		lan_mac=$(macaddr_add "$wan_mac" +1)
 		;;
 	esac

target/linux/rockchip/config-default
@@ -122,8 +122,6 @@ CONFIG_DTC=y
 CONFIG_EDAC_SUPPORT=y
 CONFIG_EFI_EARLYCON=y
 CONFIG_F2FS_FS=y
-CONFIG_F2FS_FS_XATTR=y
-CONFIG_F2FS_STAT_FS=y
 CONFIG_FIX_EARLYCON_MEM=y
 CONFIG_FONT_8x16=y
 CONFIG_FONT_AUTOSELECT=y

target/linux/rockchip/image/armv8.mk
@@ -1,9 +1,6 @@
-# 
+# SPDX-License-Identifier: GPL-2.0-only
+#
 # Copyright (C) 2020 Tobias Maedel
-#
-# This is free software, licensed under the GNU General Public License v2.
-# See /LICENSE for more information.
-#
 
 define Device/friendlyarm_nanopi-r2s
   DEVICE_VENDOR := FriendlyARM
@@ -11,7 +8,7 @@ define Device/friendlyarm_nanopi-r2s
   SOC := rk3328
   UBOOT_DEVICE_NAME := nanopi-r2s-rk3328
   IMAGE/sysupgrade.img.gz := boot-common | boot-script nanopi-r2s | pine64-img | gzip | append-metadata
-  DEVICE_PACKAGES := kmod-usb-net kmod-usb-net-rtl8152
+  DEVICE_PACKAGES := kmod-usb-net-rtl8152
 endef
 TARGET_DEVICES += friendlyarm_nanopi-r2s
 
@@ -19,23 +16,11 @@ define Device/friendlyarm_nanopi-r4s
   DEVICE_VENDOR := FriendlyARM
   DEVICE_MODEL := NanoPi R4S
   SOC := rk3399
+  UBOOT_DEVICE_NAME := nanopi-r4s-rk3399
   IMAGE/sysupgrade.img.gz := boot-common | boot-script nanopi-r4s | pine64-img | gzip | append-metadata
-  DEVICE_PACKAGES := kmod-r8168
+  DEVICE_PACKAGES := kmod-r8168 -urngd
 endef
-
-define Device/friendlyarm_nanopi-r4s-1gb
-  $(Device/friendlyarm_nanopi-r4s)
-  DEVICE_MODEL += 1GB
-  UBOOT_DEVICE_NAME := nanopi-r4s-1gb-rk3399
-endef
-TARGET_DEVICES += friendlyarm_nanopi-r4s-1gb
-
-define Device/friendlyarm_nanopi-r4s-4gb
-  $(Device/friendlyarm_nanopi-r4s)
-  DEVICE_MODEL += 4GB
-  UBOOT_DEVICE_NAME := nanopi-r4s-4gb-rk3399
-endef
-TARGET_DEVICES += friendlyarm_nanopi-r4s-4gb
+TARGET_DEVICES += friendlyarm_nanopi-r4s
 
 define Device/pine64_rockpro64
   DEVICE_VENDOR := Pine64
@@ -43,5 +28,17 @@ define Device/pine64_rockpro64
   SOC := rk3399
   UBOOT_DEVICE_NAME := rockpro64-rk3399
   IMAGE/sysupgrade.img.gz := boot-common | boot-script | pine64-img | gzip | append-metadata
+  DEVICE_PACKAGES := -urngd
 endef
 TARGET_DEVICES += pine64_rockpro64
+
+define Device/radxa_rock-pi-4
+  DEVICE_VENDOR := Radxa
+  DEVICE_MODEL := ROCK Pi 4
+  SOC := rk3399
+  SUPPORTED_DEVICES := radxa,rockpi4
+  UBOOT_DEVICE_NAME := rock-pi-4-rk3399
+  IMAGE/sysupgrade.img.gz := boot-common | boot-script | pine64-img | gzip | append-metadata
+  DEVICE_PACKAGES:= -urngd
+endef
+TARGET_DEVICES += radxa_rock-pi-4

package/boot/uboot-rockchip/Makefile
@@ -5,10 +5,10 @@
 include $(TOPDIR)/rules.mk
 include $(INCLUDE_DIR)/kernel.mk
 
-PKG_VERSION:=2020.07
-PKG_RELEASE:=3
+PKG_VERSION:=2021.01
+PKG_RELEASE:=1
 
-PKG_HASH:=c1f5bf9ee6bb6e648edbf19ce2ca9452f614b08a9f886f1a566aa42e8cf05f6a
+PKG_HASH:=b407e1510a74e863b8b5cb42a24625344f0e0c2fc7582d8c866bd899367d0454
 
 PKG_MAINTAINER:=Tobias Maedel <openwrt@tbspace.de>
 
@@ -38,22 +38,12 @@ endef
 
 # RK3399 boards
 
-define U-Boot/nanopi-r4s-1gb-rk3399
+define U-Boot/nanopi-r4s-rk3399
   BUILD_SUBTARGET:=armv8
-  NAME:=NanoPi R4S 1GB
+  NAME:=NanoPi R4S
   BUILD_DEVICES:= \
-    friendlyarm_nanopi-r4s-1gb
-  DEPENDS:=+PACKAGE_u-boot-nanopi-r4s-1gb-rk3399:arm-trusted-firmware-rockchip
-  PKG_BUILD_DEPENDS:=arm-trusted-firmware-rockchip
-  ATF:=rk3399_bl31.elf
-endef
-
-define U-Boot/nanopi-r4s-4gb-rk3399
-  BUILD_SUBTARGET:=armv8
-  NAME:=NanoPi R4S 4GB
-  BUILD_DEVICES:= \
-    friendlyarm_nanopi-r4s-4gb
-  DEPENDS:=+PACKAGE_u-boot-nanopi-r4s-4gb-rk3399:arm-trusted-firmware-rockchip
+    friendlyarm_nanopi-r4s
+  DEPENDS:=+PACKAGE_u-boot-nanopi-r4s-rk3399:arm-trusted-firmware-rockchip
   PKG_BUILD_DEPENDS:=arm-trusted-firmware-rockchip
   ATF:=rk3399_bl31.elf
 endef
@@ -79,8 +69,7 @@ define U-Boot/rockpro64-rk3399
 endef
 
 UBOOT_TARGETS := \
-  nanopi-r4s-1gb-rk3399 \
-  nanopi-r4s-4gb-rk3399 \
+  nanopi-r4s-rk3399 \
   rock-pi-4-rk3399 \
   rockpro64-rk3399 \
   nanopi-r2s-rk3328

package/boot/uboot-rockchip/patches/002-spl-remove-dtoc-of-pdata-generation.patch
@@ -17,7 +17,7 @@ Signed-off-by: David Bauer <mail@david-bauer.net>
 
 --- a/scripts/Makefile.spl
 +++ b/scripts/Makefile.spl
-@@ -320,12 +320,6 @@ PHONY += dts_dir
+@@ -321,12 +321,6 @@ PHONY += dts_dir
  dts_dir:
  	$(shell [ -d $(obj)/dts ] || mkdir -p $(obj)/dts)
  

package/boot/uboot-rockchip/patches/100-rockchip-rk3328-Add-support-for-FriendlyARM-NanoPi-R.patch
@@ -1,6 +1,6 @@
-From 67f4c228c2bf515386cd54073104dc2e6eae85ea Mon Sep 17 00:00:00 2001
+From 4189a8db90ca7edc16cf9509576ca2e74f028c1c Mon Sep 17 00:00:00 2001
 From: David Bauer <mail@david-bauer.net>
-Date: Fri, 10 Jul 2020 14:58:30 +0200
+Date: Thu, 7 Jan 2021 00:05:46 +0100
 Subject: [PATCH] rockchip: rk3328: Add support for FriendlyARM NanoPi R2S
 
 This adds support for the NanoPi R2S from FriendlyArm.
@@ -17,18 +17,18 @@ WAN - LAN - SYS LED
 Signed-off-by: David Bauer <mail@david-bauer.net>
 ---
  arch/arm/dts/Makefile                      |   1 +
- arch/arm/dts/rk3328-nanopi-r2s-u-boot.dtsi |  34 +++
- arch/arm/dts/rk3328-nanopi-r2s.dts         | 334 +++++++++++++++++++++
+ arch/arm/dts/rk3328-nanopi-r2s-u-boot.dtsi |  40 +++
+ arch/arm/dts/rk3328-nanopi-r2s.dts         | 370 +++++++++++++++++++++
  board/rockchip/evb_rk3328/MAINTAINERS      |   7 +
- configs/nanopi-r2s-rk3328_defconfig        |  99 ++++++
- 5 files changed, 475 insertions(+)
+ configs/nanopi-r2s-rk3328_defconfig        |  98 ++++++
+ 5 files changed, 516 insertions(+)
  create mode 100644 arch/arm/dts/rk3328-nanopi-r2s-u-boot.dtsi
  create mode 100644 arch/arm/dts/rk3328-nanopi-r2s.dts
  create mode 100644 configs/nanopi-r2s-rk3328_defconfig
 
 --- a/arch/arm/dts/Makefile
 +++ b/arch/arm/dts/Makefile
-@@ -106,6 +106,7 @@ dtb-$(CONFIG_ROCKCHIP_RK3308) += \
+@@ -110,6 +110,7 @@ dtb-$(CONFIG_ROCKCHIP_RK3308) += \
  
  dtb-$(CONFIG_ROCKCHIP_RK3328) += \
  	rk3328-evb.dtb \
@@ -38,7 +38,7 @@ Signed-off-by: David Bauer <mail@david-bauer.net>
  	rk3328-rock-pi-e.dtb
 --- /dev/null
 +++ b/arch/arm/dts/rk3328-nanopi-r2s-u-boot.dtsi
-@@ -0,0 +1,34 @@
+@@ -0,0 +1,40 @@
 +// SPDX-License-Identifier: GPL-2.0+
 +/*
 + * (C) Copyright 2018-2019 Rockchip Electronics Co., Ltd
@@ -73,9 +73,15 @@ Signed-off-by: David Bauer <mail@david-bauer.net>
 +&vcc_sd {
 +	u-boot,dm-spl;
 +};
++
++&gmac2io {
++	snps,reset-gpio = <&gpio1 RK_PC2 GPIO_ACTIVE_LOW>;
++	snps,reset-active-low;
++	snps,reset-delays-us = <0 10000 50000>;
++};
 --- /dev/null
 +++ b/arch/arm/dts/rk3328-nanopi-r2s.dts
-@@ -0,0 +1,334 @@
+@@ -0,0 +1,370 @@
 +// SPDX-License-Identifier: (GPL-2.0+ OR MIT)
 +/*
 + * Copyright (c) 2020 David Bauer <mail@david-bauer.net>
@@ -88,92 +94,91 @@ Signed-off-by: David Bauer <mail@david-bauer.net>
 +#include "rk3328.dtsi"
 +
 +/ {
-+	model = "FriendlyARM NanoPi R2S";
++	model = "FriendlyElec NanoPi R2S";
 +	compatible = "friendlyarm,nanopi-r2s", "rockchip,rk3328";
 +
 +	chosen {
 +		stdout-path = "serial2:1500000n8";
 +	};
 +
-+	gmac_clkin: external-gmac-clock {
++	gmac_clk: gmac-clock {
 +		compatible = "fixed-clock";
 +		clock-frequency = <125000000>;
 +		clock-output-names = "gmac_clkin";
 +		#clock-cells = <0>;
 +	};
 +
-+	vcc_sd: sdmmc-regulator {
-+		compatible = "regulator-fixed";
-+		gpio = <&gpio0 RK_PD6 GPIO_ACTIVE_LOW>;
++	keys {
++		compatible = "gpio-keys";
++		pinctrl-0 = <&reset_button_pin>;
 +		pinctrl-names = "default";
-+		pinctrl-0 = <&sdmmc0m1_gpio>;
-+		regulator-name = "vcc_sd";
-+		regulator-min-microvolt = <3300000>;
-+		regulator-max-microvolt = <3300000>;
-+		vin-supply = <&vcc_io>;
-+	};
 +
-+	vcc_sdio: sdmmcio-regulator {
-+		compatible = "regulator-gpio";
-+		gpios = <&gpio1 RK_PD4 GPIO_ACTIVE_HIGH>;
-+		enable-active-high;
-+		states = <1800000 0x1
-+			  3300000 0x0>;
-+		pinctrl-names = "default";
-+		pinctrl-0 = <&sdio_vcc_pin>;
-+		regulator-always-on;
-+		regulator-min-microvolt = <1800000>;
-+		regulator-max-microvolt = <3300000>;
-+		regulator-name = "vcc_sdio";
-+		regulator-settling-time-us = <5000>;
-+		regulator-type = "voltage";
-+		vin-supply = <&vcc_io>;
-+	};
-+
-+	vcc_sys: vcc-sys {
-+		compatible = "regulator-fixed";
-+		regulator-name = "vcc_sys";
-+		regulator-always-on;
-+		regulator-boot-on;
-+		regulator-min-microvolt = <5000000>;
-+		regulator-max-microvolt = <5000000>;
++		reset {
++			label = "reset";
++			gpios = <&gpio0 RK_PA0 GPIO_ACTIVE_LOW>;
++			linux,code = <KEY_RESTART>;
++			debounce-interval = <50>;
++		};
 +	};
 +
 +	leds {
 +		compatible = "gpio-leds";
-+
++		pinctrl-0 = <&lan_led_pin>,  <&sys_led_pin>, <&wan_led_pin>;
 +		pinctrl-names = "default";
-+		pinctrl-0 = <&led_pins>;
 +
-+		sys {
-+			gpios = <&gpio0 RK_PA2 GPIO_ACTIVE_HIGH>;
-+			label = "nanopi-r2s:red:sys";
-+		};
-+
-+		lan {
++		lan_led: led-0 {
 +			gpios = <&gpio2 RK_PB7 GPIO_ACTIVE_HIGH>;
 +			label = "nanopi-r2s:green:lan";
 +		};
 +
-+		wan {
++		sys_led: led-1 {
++			gpios = <&gpio0 RK_PA2 GPIO_ACTIVE_HIGH>;
++			label = "nanopi-r2s:red:sys";
++		};
++
++		wan_led: led-2 {
 +			gpios = <&gpio2 RK_PC2 GPIO_ACTIVE_HIGH>;
 +			label = "nanopi-r2s:green:wan";
 +		};
 +	};
 +
-+	gpio_keys {
-+		compatible = "gpio-keys-polled";
-+		poll-interval = <100>;
-+
++	vcc_io_sdio: sdmmcio-regulator {
++		compatible = "regulator-gpio";
++		enable-active-high;
++		gpios = <&gpio1 RK_PD4 GPIO_ACTIVE_HIGH>;
++		pinctrl-0 = <&sdio_vcc_pin>;
 +		pinctrl-names = "default";
-+		pinctrl-0 = <&button_pins>;
++		regulator-name = "vcc_io_sdio";
++		regulator-always-on;
++		regulator-min-microvolt = <1800000>;
++		regulator-max-microvolt = <3300000>;
++		regulator-settling-time-us = <5000>;
++		regulator-type = "voltage";
++		startup-delay-us = <2000>;
++		states = <1800000 0x1
++			  3300000 0x0>;
++		vin-supply = <&vcc_io_33>;
++	};
 +
-+		reset {
-+			label = "Reset Button";
-+			gpios = <&gpio0 RK_PA0 GPIO_ACTIVE_LOW>;
-+			linux,code = <KEY_RESTART>;
-+			debounce-interval = <50>;
-+		};
++	vcc_sd: sdmmc-regulator {
++		compatible = "regulator-fixed";
++		gpio = <&gpio0 RK_PD6 GPIO_ACTIVE_LOW>;
++		pinctrl-0 = <&sdmmc0m1_gpio>;
++		pinctrl-names = "default";
++		regulator-name = "vcc_sd";
++		regulator-boot-on;
++		regulator-min-microvolt = <3300000>;
++		regulator-max-microvolt = <3300000>;
++		vin-supply = <&vcc_io_33>;
++	};
++
++	vdd_5v: vdd-5v {
++		compatible = "regulator-fixed";
++		regulator-name = "vdd_5v";
++		regulator-always-on;
++		regulator-boot-on;
++		regulator-min-microvolt = <5000000>;
++		regulator-max-microvolt = <5000000>;
 +	};
 +};
 +
@@ -195,19 +200,16 @@ Signed-off-by: David Bauer <mail@david-bauer.net>
 +
 +&gmac2io {
 +	assigned-clocks = <&cru SCLK_MAC2IO>, <&cru SCLK_MAC2IO_EXT>;
-+	assigned-clock-parents = <&gmac_clkin>, <&gmac_clkin>;
++	assigned-clock-parents = <&gmac_clk>, <&gmac_clk>;
 +	clock_in_out = "input";
-+	phy-supply = <&vcc_io>;
 +	phy-handle = <&rtl8211e>;
 +	phy-mode = "rgmii";
-+	pinctrl-names = "default";
++	phy-supply = <&vcc_io_33>;
 +	pinctrl-0 = <&rgmiim1_pins>;
-+	snps,aal;
-+	snps,reset-gpio = <&gpio1 RK_PC2 GPIO_ACTIVE_LOW>;
-+	snps,reset-active-low;
-+	snps,reset-delays-us = <0 10000 50000>;
-+	tx_delay = <0x24>;
++	pinctrl-names = "default";
 +	rx_delay = <0x18>;
++	snps,aal;
++	tx_delay = <0x24>;
 +	status = "okay";
 +
 +	mdio {
@@ -215,8 +217,15 @@ Signed-off-by: David Bauer <mail@david-bauer.net>
 +		#address-cells = <1>;
 +		#size-cells = <0>;
 +
-+		rtl8211e: ethernet-phy@0 {
-+			reg = <0>;
++		rtl8211e: ethernet-phy@1 {
++			compatible = "ethernet-phy-id001c.c915",
++				     "ethernet-phy-ieee802.3-c22";
++			reg = <1>;
++			pinctrl-0 = <&eth_phy_reset_pin>;
++			pinctrl-names = "default";
++			reset-assert-us = <10000>;
++			reset-deassert-us = <50000>;
++			reset-gpios = <&gpio1 RK_PC2 GPIO_ACTIVE_LOW>;
 +		};
 +	};
 +};
@@ -224,35 +233,36 @@ Signed-off-by: David Bauer <mail@david-bauer.net>
 +&i2c1 {
 +	status = "okay";
 +
-+	rk805: rk805@18 {
++	rk805: pmic@18 {
 +		compatible = "rockchip,rk805";
 +		reg = <0x18>;
-+		interrupt-parent = <&gpio2>;
-+		interrupts = <6 IRQ_TYPE_LEVEL_LOW>;
++		interrupt-parent = <&gpio1>;
++		interrupts = <24 IRQ_TYPE_LEVEL_LOW>;
 +		#clock-cells = <1>;
 +		clock-output-names = "xin32k", "rk805-clkout2";
 +		gpio-controller;
 +		#gpio-cells = <2>;
-+		pinctrl-names = "default";
 +		pinctrl-0 = <&pmic_int_l>;
++		pinctrl-names = "default";
 +		rockchip,system-power-controller;
 +		wakeup-source;
 +
-+		vcc1-supply = <&vcc_sys>;
-+		vcc2-supply = <&vcc_sys>;
-+		vcc3-supply = <&vcc_sys>;
-+		vcc4-supply = <&vcc_sys>;
-+		vcc5-supply = <&vcc_io>;
-+		vcc6-supply = <&vcc_sys>;
++		vcc1-supply = <&vdd_5v>;
++		vcc2-supply = <&vdd_5v>;
++		vcc3-supply = <&vdd_5v>;
++		vcc4-supply = <&vdd_5v>;
++		vcc5-supply = <&vcc_io_33>;
++		vcc6-supply = <&vdd_5v>;
 +
 +		regulators {
-+			vdd_logic: DCDC_REG1 {
-+				regulator-name = "vdd_logic";
++			vdd_log: DCDC_REG1 {
++				regulator-name = "vdd_log";
++				regulator-always-on;
++				regulator-boot-on;
 +				regulator-min-microvolt = <712500>;
 +				regulator-max-microvolt = <1450000>;
 +				regulator-ramp-delay = <12500>;
-+				regulator-always-on;
-+				regulator-boot-on;
++
 +				regulator-state-mem {
 +					regulator-on-in-suspend;
 +					regulator-suspend-microvolt = <1000000>;
@@ -261,11 +271,12 @@ Signed-off-by: David Bauer <mail@david-bauer.net>
 +
 +			vdd_arm: DCDC_REG2 {
 +				regulator-name = "vdd_arm";
++				regulator-always-on;
++				regulator-boot-on;
 +				regulator-min-microvolt = <712500>;
 +				regulator-max-microvolt = <1450000>;
 +				regulator-ramp-delay = <12500>;
-+				regulator-always-on;
-+				regulator-boot-on;
++
 +				regulator-state-mem {
 +					regulator-on-in-suspend;
 +					regulator-suspend-microvolt = <950000>;
@@ -276,17 +287,19 @@ Signed-off-by: David Bauer <mail@david-bauer.net>
 +				regulator-name = "vcc_ddr";
 +				regulator-always-on;
 +				regulator-boot-on;
++
 +				regulator-state-mem {
 +					regulator-on-in-suspend;
 +				};
 +			};
 +
-+			vcc_io: DCDC_REG4 {
-+				regulator-name = "vcc_io";
-+				regulator-min-microvolt = <3300000>;
-+				regulator-max-microvolt = <3300000>;
++			vcc_io_33: DCDC_REG4 {
++				regulator-name = "vcc_io_33";
 +				regulator-always-on;
 +				regulator-boot-on;
++				regulator-min-microvolt = <3300000>;
++				regulator-max-microvolt = <3300000>;
++
 +				regulator-state-mem {
 +					regulator-on-in-suspend;
 +					regulator-suspend-microvolt = <3300000>;
@@ -295,10 +308,11 @@ Signed-off-by: David Bauer <mail@david-bauer.net>
 +
 +			vcc_18: LDO_REG1 {
 +				regulator-name = "vcc_18";
-+				regulator-min-microvolt = <1800000>;
-+				regulator-max-microvolt = <1800000>;
 +				regulator-always-on;
 +				regulator-boot-on;
++				regulator-min-microvolt = <1800000>;
++				regulator-max-microvolt = <1800000>;
++
 +				regulator-state-mem {
 +					regulator-on-in-suspend;
 +					regulator-suspend-microvolt = <1800000>;
@@ -307,10 +321,11 @@ Signed-off-by: David Bauer <mail@david-bauer.net>
 +
 +			vcc18_emmc: LDO_REG2 {
 +				regulator-name = "vcc18_emmc";
-+				regulator-min-microvolt = <1800000>;
-+				regulator-max-microvolt = <1800000>;
 +				regulator-always-on;
 +				regulator-boot-on;
++				regulator-min-microvolt = <1800000>;
++				regulator-max-microvolt = <1800000>;
++
 +				regulator-state-mem {
 +					regulator-on-in-suspend;
 +					regulator-suspend-microvolt = <1800000>;
@@ -319,10 +334,11 @@ Signed-off-by: David Bauer <mail@david-bauer.net>
 +
 +			vdd_10: LDO_REG3 {
 +				regulator-name = "vdd_10";
-+				regulator-min-microvolt = <1000000>;
-+				regulator-max-microvolt = <1000000>;
 +				regulator-always-on;
 +				regulator-boot-on;
++				regulator-min-microvolt = <1000000>;
++				regulator-max-microvolt = <1000000>;
++
 +				regulator-state-mem {
 +					regulator-on-in-suspend;
 +					regulator-suspend-microvolt = <1000000>;
@@ -333,35 +349,46 @@ Signed-off-by: David Bauer <mail@david-bauer.net>
 +};
 +
 +&io_domains {
-+	status = "okay";
-+
-+	vccio1-supply = <&vcc_io>;
++	pmuio-supply = <&vcc_io_33>;
++	vccio1-supply = <&vcc_io_33>;
 +	vccio2-supply = <&vcc18_emmc>;
-+	vccio3-supply = <&vcc_sdio>;
++	vccio3-supply = <&vcc_io_sdio>;
 +	vccio4-supply = <&vcc_18>;
-+	vccio5-supply = <&vcc_io>;
-+	vccio6-supply = <&vcc_io>;
-+	pmuio-supply = <&vcc_io>;
++	vccio5-supply = <&vcc_io_33>;
++	vccio6-supply = <&vcc_io_33>;
++	status = "okay";
 +};
 +
 +&pinctrl {
-+	leds {
-+		led_pins: led-pins {
-+			rockchip,pins = <0 RK_PA2 RK_FUNC_GPIO &pcfg_pull_none>,
-+							<2 RK_PB7 RK_FUNC_GPIO &pcfg_pull_none>,
-+							<2 RK_PC2 RK_FUNC_GPIO &pcfg_pull_none>;
++	button {
++		reset_button_pin: reset-button-pin {
++			rockchip,pins = <0 RK_PA0 RK_FUNC_GPIO &pcfg_pull_none>;
 +		};
 +	};
 +
-+	button {
-+		button_pins: button-pins {
-+			rockchip,pins = <0 RK_PA0 RK_FUNC_GPIO &pcfg_pull_none>;
++	ethernet-phy {
++		eth_phy_reset_pin: eth-phy-reset-pin {
++			rockchip,pins = <1 RK_PC2 RK_FUNC_GPIO &pcfg_pull_down>;
++		};
++	};
++
++	leds {
++		lan_led_pin: lan-led-pin {
++			rockchip,pins = <2 RK_PB7 RK_FUNC_GPIO &pcfg_pull_none>;
++		};
++
++		sys_led_pin: sys-led-pin {
++			rockchip,pins = <0 RK_PA2 RK_FUNC_GPIO &pcfg_pull_none>;
++		};
++
++		wan_led_pin: wan-led-pin {
++			rockchip,pins = <2 RK_PC2 RK_FUNC_GPIO &pcfg_pull_none>;
 +		};
 +	};
 +
 +	pmic {
 +		pmic_int_l: pmic-int-l {
-+			rockchip,pins = <2 RK_PA6 RK_FUNC_GPIO &pcfg_pull_up>;
++			rockchip,pins = <1 RK_PD0 RK_FUNC_GPIO &pcfg_pull_up>;
 +		};
 +	};
 +
@@ -372,16 +399,22 @@ Signed-off-by: David Bauer <mail@david-bauer.net>
 +	};
 +};
 +
++&pwm2 {
++	status = "okay";
++};
++
 +&sdmmc {
 +	bus-width = <4>;
-+	cap-mmc-highspeed;
 +	cap-sd-highspeed;
 +	disable-wp;
-+	max-frequency = <150000000>;
++	pinctrl-0 = <&sdmmc0_clk>, <&sdmmc0_cmd>, <&sdmmc0_dectn>, <&sdmmc0_bus4>;
 +	pinctrl-names = "default";
-+	pinctrl-0 = <&sdmmc0_clk &sdmmc0_cmd &sdmmc0_dectn &sdmmc0_bus4>;
++	sd-uhs-sdr12;
++	sd-uhs-sdr25;
++	sd-uhs-sdr50;
++	sd-uhs-sdr104;
 +	vmmc-supply = <&vcc_sd>;
-+	vqmmc-supply = <&vcc_sdio>;
++	vqmmc-supply = <&vcc_io_sdio>;
 +	status = "okay";
 +};
 +
@@ -391,16 +424,25 @@ Signed-off-by: David Bauer <mail@david-bauer.net>
 +	status = "okay";
 +};
 +
++&u2phy {
++	status = "okay";
++};
++
++&u2phy_host {
++	status = "okay";
++};
++
++&u2phy_otg {
++	status = "okay";
++};
++
 +&uart2 {
 +	status = "okay";
 +};
 +
-+&u2phy {
++&usb20_otg {
 +	status = "okay";
-+
-+	u2phy_host: host-port {
-+		status = "okay";
-+	};
++	dr_mode = "host";
 +};
 +
 +&usb_host0_ehci {
@@ -428,7 +470,7 @@ Signed-off-by: David Bauer <mail@david-bauer.net>
  M:      Chen-Yu Tsai <wens@csie.org>
 --- /dev/null
 +++ b/configs/nanopi-r2s-rk3328_defconfig
-@@ -0,0 +1,99 @@
+@@ -0,0 +1,98 @@
 +CONFIG_ARM=y
 +CONFIG_ARCH_ROCKCHIP=y
 +CONFIG_SYS_TEXT_BASE=0x00200000
@@ -443,7 +485,7 @@ Signed-off-by: David Bauer <mail@david-bauer.net>
 +CONFIG_NR_DRAM_BANKS=1
 +CONFIG_DEBUG_UART_BASE=0xFF130000
 +CONFIG_DEBUG_UART_CLOCK=24000000
-+CONFIG_SMBIOS_PRODUCT_NAME="nanopi_r2s_rk3328"
++CONFIG_SYSINFO=y
 +CONFIG_DEBUG_UART=y
 +CONFIG_TPL_SYS_MALLOC_F_LEN=0x800
 +# CONFIG_ANDROID_BOOT_IMAGE is not set
@@ -527,4 +569,3 @@ Signed-off-by: David Bauer <mail@david-bauer.net>
 +CONFIG_SPL_TINY_MEMSET=y
 +CONFIG_TPL_TINY_MEMSET=y
 +CONFIG_ERRNO_STR=y
-+CONFIG_SMBIOS_MANUFACTURER="pine64"

package/boot/uboot-rockchip/patches/101-rock64pro-disable-CONFIG_USE_PREBOOT.patch
@@ -1,0 +1,27 @@
+From 2114d68b3c755ec8043ae9e43ac8e9753e0cec84 Mon Sep 17 00:00:00 2001
+From: Marty Jones <mj8263788@gmail.com>
+Date: Sun, 17 Jan 2021 15:26:09 -0500
+Subject: [PATCH] rockpro64: disable CONFIG_USE_PREBOOT
+
+On commit https://github.com/u-boot/u-boot/commit/f81f9f0ebac596bae7f27db095f4f0272b606cc3
+CONFIG_USE_PREBOOT was enabled on the RockPro64.
+
+When the board is booting, U-Boot hangs as soon as it disables the USB
+controller. This is a workaround until a final solution is deployed
+upstream.
+
+Signed-off-by: Marty Jones <mj8263788@gmail.com>
+---
+ configs/rockpro64-rk3399_defconfig | 1 -
+ 1 file changed, 1 deletion(-)
+
+--- a/configs/rockpro64-rk3399_defconfig
++++ b/configs/rockpro64-rk3399_defconfig
+@@ -12,7 +12,6 @@ CONFIG_SPL_SPI_FLASH_SUPPORT=y
+ CONFIG_SPL_SPI_SUPPORT=y
+ CONFIG_DEFAULT_DEVICE_TREE="rk3399-rockpro64"
+ CONFIG_DEBUG_UART=y
+-CONFIG_USE_PREBOOT=y
+ CONFIG_DEFAULT_FDT_FILE="rockchip/rk3399-rockpro64.dtb"
+ CONFIG_DISPLAY_BOARDINFO_LATE=y
+ CONFIG_MISC_INIT_R=y

package/boot/uboot-rockchip/patches/101-rockchip-rk3328-fix-nanopi-r2s-failed-to-boot-on-som.patch
@@ -1,38 +1,0 @@
-From 0000000000000000000000000000000000000000 Mon Sep 17 00:00:00 2001
-From: Yuan Tao <ty@wevs.org>
-Date: Sun, 9 Aug 2020 09:02:55 +0800
-Subject: [PATCH] rockchip: rk3328: fix nanopi-r2s failed to boot on some sd cards
-
-Issues:
-When booting on some SD cards an error message appears as:
-"spl: mmc init failed with error: -95"
-
-Solutions:
-Add regulator-boot-on parameters to vcc_sd.
-Add startup-delay-us parameters to vcc_sdio.
-This will improve the problem that in some SD cards are failing to boot up.
-
-Tested environment:
-SD Card: Netac Extreme P500 Pro 32GB
-
-Signed-off-by: Yuan Tao <ty@wevs.org>
----
-
---- a/arch/arm/dts/rk3328-nanopi-r2s.dts
-+++ b/arch/arm/dts/rk3328-nanopi-r2s.dts
-@@ -32,6 +32,7 @@
- 		regulator-name = "vcc_sd";
- 		regulator-min-microvolt = <3300000>;
- 		regulator-max-microvolt = <3300000>;
-+		regulator-boot-on;
- 		vin-supply = <&vcc_io>;
- 	};
- 
-@@ -49,6 +50,7 @@
- 		regulator-name = "vcc_sdio";
- 		regulator-settling-time-us = <5000>;
- 		regulator-type = "voltage";
-+		startup-delay-us = <2000>;
- 		vin-supply = <&vcc_io>;
- 	};
- 

commit 0e8749ae341b2958dc1782dc5803aced25a27c58

rockchip: add execute permission for network setup script
Fixes: f4b1b358fe8b ("rockchip: sync latest codes from ImmortalWrt (#6522)")

Closes: #6534
Closes: #6548
Closes: #6563

Signed-off-by: Tianling Shen <cnsztl@immortalwrt.org>

target/linux/rockchip/config-default
@@ -122,6 +122,8 @@ CONFIG_DTC=y
 CONFIG_EDAC_SUPPORT=y
 CONFIG_EFI_EARLYCON=y
 CONFIG_F2FS_FS=y
+CONFIG_F2FS_FS_XATTR=y
+CONFIG_F2FS_STAT_FS=y
 CONFIG_FIX_EARLYCON_MEM=y
 CONFIG_FONT_8x16=y
 CONFIG_FONT_AUTOSELECT=y

target/linux/rockchip/image/armv8.mk
@@ -39,6 +39,6 @@ define Device/radxa_rock-pi-4
   SUPPORTED_DEVICES := radxa,rockpi4
   UBOOT_DEVICE_NAME := rock-pi-4-rk3399
   IMAGE/sysupgrade.img.gz := boot-common | boot-script | pine64-img | gzip | append-metadata
-  DEVICE_PACKAGES:= -urngd
+  DEVICE_PACKAGES := -urngd
 endef
 TARGET_DEVICES += radxa_rock-pi-4