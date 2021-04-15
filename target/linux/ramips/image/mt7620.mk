b2369aef918906b0f32b951ab55f5ec7fdf13b3c
brightsm <brightsm@163.com>
2021-04-15 08:00:48 +0800

    Update mt7620.mk

--- target/linux/ramips/image/mt7620.mk
+++ target/linux/ramips/image/mt7620.mk
@@ -1126,17 +1126,6 @@ define Device/zbtlink_zbt-wa05
 endef
 TARGET_DEVICES += zbtlink_zbt-wa05
 
-define Device/whtlink_wht-rx01
-  SOC := mt7620n
-  IMAGE_SIZE := 16064k
-  DEVICE_VENDOR := whtlink
-  DEVICE_MODEL := WHT-rx01
-  DEVICE_VARIANT := 16M
-  DEVICE_PACKAGES := kmod-usb2 kmod-usb-ohci
-  SUPPORTED_DEVICES += wht-rx01
-endef
-TARGET_DEVICES += whtlink_wht-rx-01
-
 define Device/zbtlink_zbt-we1026-5g-16m
   SOC := mt7620a
   IMAGE_SIZE := 16064k
