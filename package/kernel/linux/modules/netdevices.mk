41929bf379040915417f3e26d2a9cba3b8ba44c7
steveyangpi <504722363@qq.com>
2021-04-19 04:09:13 +0800

    Update netdevices.mk

--- package/kernel/linux/modules/netdevices.mk
+++ package/kernel/linux/modules/netdevices.mk
@@ -1272,3 +1272,18 @@ define KernelPackage/sfc-falcon/description
 endef
 
 $(eval $(call KernelPackage,sfc-falcon))
+
+define KernelPackage/jme
+  SUBMENU:=$(NETWORK_DEVICES_MENU)
+  TITLE:=JMicron(R) PCI-Express Gigabit Ethernet support
+  DEPENDS:=@PCI_SUPPORT +kmod-mii
+  KCONFIG:=CONFIG_JME
+  FILES:=$(LINUX_DIR)/drivers/net/ethernet/jme.ko
+  AUTOLOAD:=$(call AutoProbe,jme)
+endef
+
+define KernelPackage/jme/description
+ Kernel modules for JMicron(R) PCI-Express Gigabit Ethernet adapters
+endef
+
+$(eval $(call KernelPackage,jme))
