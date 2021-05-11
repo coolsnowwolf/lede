67932267aa2118419a1345de19a9940b8d5d537f
John Audia <graysky@archlinux.us>
2021-05-11 11:50:27 -0400

    kernel: bump 5.4 to 5.4.118
    Manually rebased:
      ath79/patches-5.4/0033-spi-ath79-drop-pdata-support.patch
    
    Removed uneeded patch:
      ath79/patches-5.4/0050-spi-ath79-remove-spi-master-setup-and-cleanup-assign.patch
    
    All other patches automatically rebased.
    
    Build system: x86_64
    Build-tested: ipq806x/R7800
    Run-tested: ipq806x/R7800
    
    No dmesg regressions, everything functional
    
    Signed-off-by: John Audia <graysky@archlinux.us>
    Signed-off-by: Boos4721 <3.1415926535boos@gmail.com>
    
    Conflicts:
    	include/kernel-version.mk
    	target/linux/generic/backport-5.4/080-wireguard-0052-crypto-x86-curve25519-support-assemblers-with-no-adx.patch

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -1,43 +1,43 @@
 # Use the default kernel version if the Makefile doesn't override it
-
-LINUX_RELEASE?=1
-
-ifdef CONFIG_TESTING_KERNEL
-  KERNEL_PATCHVER:=$(KERNEL_TESTING_PATCHVER)
-endif
-
-LINUX_VERSION-4.14 = .195
-LINUX_VERSION-4.19 = .138
-LINUX_VERSION-5.4 = .117
-
-LINUX_KERNEL_HASH-4.14.195 = 394f28798670240baacd9e2cce521fbd79f8da5e1fc191695b0e11381445a021
-LINUX_KERNEL_HASH-4.19.138 = d15c27d05f6c527269b75b30cc72972748e55720e7e00ad8abbaa4fe3b1d5e02
-LINUX_KERNEL_HASH-5.4.117 = 4e989b5775830092e5c76b5cca65ebff862ad0c87d0b58c3a20d415c3d4ec770
-
-remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
-sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
-
-ifneq ($(call qstrip,$(CONFIG_KERNEL_GIT_CLONE_URI)),)
-  LINUX_VERSION:=$(call sanitize_uri,$(call remove_uri_prefix,$(CONFIG_KERNEL_GIT_CLONE_URI)))
-  ifeq ($(call qstrip,$(CONFIG_KERNEL_GIT_REF)),)
-    CONFIG_KERNEL_GIT_REF:=HEAD
-  endif
-  LINUX_VERSION:=$(LINUX_VERSION)-$(call sanitize_uri,$(CONFIG_KERNEL_GIT_REF))
-else
-ifdef KERNEL_PATCHVER
-  LINUX_VERSION:=$(KERNEL_PATCHVER)$(strip $(LINUX_VERSION-$(KERNEL_PATCHVER)))
-endif
-ifdef KERNEL_TESTING_PATCHVER
-  LINUX_TESTING_VERSION:=$(KERNEL_TESTING_PATCHVER)$(strip $(LINUX_VERSION-$(KERNEL_TESTING_PATCHVER)))
-endif
-endif
-
-split_version=$(subst ., ,$(1))
-merge_version=$(subst $(space),.,$(1))
-KERNEL_BASE=$(firstword $(subst -, ,$(LINUX_VERSION)))
-KERNEL=$(call merge_version,$(wordlist 1,2,$(call split_version,$(KERNEL_BASE))))
-KERNEL_PATCHVER ?= $(KERNEL)
-
-# disable the md5sum check for unknown kernel versions
+#
+# LINUX_RELEASE?=1
+#
+# ifdef CONFIG_TESTING_KERNEL
+#   KERNEL_PATCHVER:=$(KERNEL_TESTING_PATCHVER)
+#   endif
+#
+#   LINUX_VERSION-4.14 = .195
+#   LINUX_VERSION-4.19 = .138
+#   LINUX_VERSION-5.4 = .118
+#
+#   LINUX_KERNEL_HASH-4.14.195 = 394f28798670240baacd9e2cce521fbd79f8da5e1fc191695b0e11381445a021
+#   LINUX_KERNEL_HASH-4.19.138 = d15c27d05f6c527269b75b30cc72972748e55720e7e00ad8abbaa4fe3b1d5e02
+#   LINUX_KERNEL_HASH-5.4.118 = d970509c274fd2f5a9257a63c56f7a8b3e50ec12015bd6864dc4b76b38dc821b
+#
+#   remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
+#   sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
+#
+#   ifneq ($(call qstrip,$(CONFIG_KERNEL_GIT_CLONE_URI)),)
+#     LINUX_VERSION:=$(call sanitize_uri,$(call remove_uri_prefix,$(CONFIG_KERNEL_GIT_CLONE_URI)))
+#       ifeq ($(call qstrip,$(CONFIG_KERNEL_GIT_REF)),)
+#           CONFIG_KERNEL_GIT_REF:=HEAD
+#             endif
+#               LINUX_VERSION:=$(LINUX_VERSION)-$(call sanitize_uri,$(CONFIG_KERNEL_GIT_REF))
+#               else
+#               ifdef KERNEL_PATCHVER
+#                 LINUX_VERSION:=$(KERNEL_PATCHVER)$(strip $(LINUX_VERSION-$(KERNEL_PATCHVER)))
+#                 endif
+#                 ifdef KERNEL_TESTING_PATCHVER
+#                   LINUX_TESTING_VERSION:=$(KERNEL_TESTING_PATCHVER)$(strip $(LINUX_VERSION-$(KERNEL_TESTING_PATCHVER)))
+#                   endif
+#                   endif
+#
+#                   split_version=$(subst ., ,$(1))
+#                   merge_version=$(subst $(space),.,$(1))
+#                   KERNEL_BASE=$(firstword $(subst -, ,$(LINUX_VERSION)))
+#                   KERNEL=$(call merge_version,$(wordlist 1,2,$(call split_version,$(KERNEL_BASE))))
+#                   KERNEL_PATCHVER ?= $(KERNEL)
+#
+#                   # disable the md5sum check for unknown kernel versions
 LINUX_KERNEL_HASH:=$(LINUX_KERNEL_HASH-$(strip $(LINUX_VERSION)))
 LINUX_KERNEL_HASH?=x
