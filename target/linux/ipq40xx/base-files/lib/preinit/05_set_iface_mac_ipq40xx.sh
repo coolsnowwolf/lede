dbdbcc20160688fd7189375f44f4070db3b99bb0
AmadeusGhost <42570690+AmadeusGhost@users.noreply.github.com>
2021-06-19 00:20:06 +0800

    ipq40xx: some typo and bug fixes

--- target/linux/ipq40xx/base-files/lib/preinit/05_set_iface_mac_ipq40xx.sh
+++ target/linux/ipq40xx/base-files/lib/preinit/05_set_iface_mac_ipq40xx.sh
@@ -3,17 +3,17 @@
 preinit_set_mac_address() {
 	case $(board_name) in
 	asus,map-ac2200)
-		base_mac=$(mtd_get_mac_binary_ubi Factory 4102)
-		ip link set dev eth0 address $(macaddr_add "$base_mac" +1)
+		base_mac=$(mtd_get_mac_binary_ubi Factory 0x1006)
+		ip link set dev eth0 address $(macaddr_add "$base_mac" 1)
 		;;
 	asus,rt-acrh17|\
 	asus,rt-ac58u)
 		CI_UBIPART=UBI_DEV
-		base_mac=$(mtd_get_mac_binary_ubi Factory 4102)
-		ip link set dev eth0 address $(macaddr_add "$base_mac" +1)
+		base_mac=$(mtd_get_mac_binary_ubi Factory 0x1006)
+		ip link set dev eth0 address $(macaddr_add "$base_mac" 1)
 		;;
 	ezviz,cs-w3-wd1200g-eup)
-		ip link set dev eth0 address $(mtd_get_mac_binary "ART" 6)
+		ip link set dev eth0 address $(mtd_get_mac_binary "ART" 0x6)
 		;;
 	engenius,eap2200)
 		base_mac=$(cat /sys/class/net/eth0/address)
@@ -23,12 +23,12 @@ preinit_set_mac_address() {
 		ip link set dev eth0 address "${base_mac}"
 		;;
 	meraki,mr33)
-		mac_lan=$(get_mac_binary "/sys/bus/i2c/devices/0-0050/eeprom" 102)
+		mac_lan=$(get_mac_binary "/sys/bus/i2c/devices/0-0050/eeprom" 0x66)
 		[ -n "$mac_lan" ] && ip link set dev eth0 address "$mac_lan"
 		;;
 	zyxel,nbg6617)
 		base_mac=$(cat /sys/class/net/eth0/address)
-		ip link set dev eth0 address $(macaddr_add "$base_mac" +2)
+		ip link set dev eth0 address $(macaddr_add "$base_mac" 2)
 		;;
 	esac
 }
