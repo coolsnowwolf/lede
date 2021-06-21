f8046684f81445cee734cd7fbd46c2ac8b22180c
lean <coolsnowwolf@gmail.com>
2021-06-21 11:10:49 +0800

    luci-app-arpbind: use ip-tiny replace ip-full

--- include/target.mk
+++ include/target.mk
@@ -49,9 +49,10 @@ DEFAULT_PACKAGES.router:=\
 	ppp-mod-pppoe \
 	luci-newapi block-mount coremark kmod-nf-nathelper kmod-nf-nathelper-extra kmod-ipt-raw \
 	default-settings luci luci-app-ddns luci-app-upnp luci-app-autoreboot \
-	luci-app-filetransfer luci-app-vsftpd luci-app-ssr-plus luci-app-unblockmusic \
-	luci-app-arpbind luci-app-vlmcsd luci-app-wol luci-app-ramfree \
-	luci-app-flowoffload luci-app-nlbwmon luci-app-accesscontrol luci-app-cpufreq
+	luci-app-filetransfer luci-app-vsftpd luci-app-ssr-plus luci-app-unblockmusic luci-app-arpbind \
+	luci-app-vlmcsd luci-app-wol luci-app-ramfree \
+	luci-app-flowoffload luci-app-nlbwmon luci-app-accesscontrol luci-app-cpufreq \
+	ddns-scripts_aliyun ddns-scripts_dnspod
 
 ifneq ($(DUMP),)
   all: dumpinfo
