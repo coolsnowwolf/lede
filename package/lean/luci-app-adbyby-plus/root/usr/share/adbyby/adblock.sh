1e60e3186e635320e09f52e97ccfb2c3c41491db
lean <coolsnowwolf@gmail.com>
2021-06-21 09:27:02 +0800

    luci-app-adbyby-plus: replace wget-ssl with uclient-fetchuclient

--- package/lean/luci-app-adbyby-plus/root/usr/share/adbyby/adblock.sh
+++ package/lean/luci-app-adbyby-plus/root/usr/share/adbyby/adblock.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-wget-ssl --no-check-certificate -O - 'https://easylist-downloads.adblockplus.org/easylistchina+easylist.txt' > /tmp/adnew.conf
+uclient-fetch --no-check-certificate -O - 'https://easylist-downloads.adblockplus.org/easylistchina+easylist.txt' > /tmp/adnew.conf
 if [ -s "/tmp/adnew.conf" ];then
   /usr/share/adbyby/ad-update
 fi
