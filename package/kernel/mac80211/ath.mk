5e0c8034784bbe887e0609155860b07e68b4a088
coolsnowwolf <coolsnowwolf@gmail.com>
2021-06-27 01:17:59 +0800

    mac80211: enable ATH10K THERMAL

--- package/kernel/mac80211/ath.mk
+++ package/kernel/mac80211/ath.mk
@@ -282,6 +282,7 @@ define KernelPackage/ath10k/config
 
        config ATH10K_THERMAL
                bool "Enable thermal sensors and throttling support"
+               default y
                depends on PACKAGE_kmod-ath10k
 
 endef
