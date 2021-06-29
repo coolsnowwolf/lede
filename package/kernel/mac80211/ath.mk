9faf3ea90d64b89985eb6aefa5abbe9ba3986835
coolsnowwolf <coolsnowwolf@gmail.com>
2021-06-29 16:19:40 +0800

    mac80211: add ipq807x dep for ath11k nss

--- package/kernel/mac80211/ath.mk
+++ package/kernel/mac80211/ath.mk
@@ -292,7 +292,7 @@ define KernelPackage/ath11k
   TITLE:=Qualcomm 802.11ax wireless chipset support (common code)
   URL:=https://wireless.wiki.kernel.org/en/users/drivers/ath11k
   DEPENDS+= +kmod-ath +@DRIVER_11N_SUPPORT +@DRIVER_11AC_SUPPORT +@DRIVER_11AX_SUPPORT \
-	+kmod-crypto-michael-mic +kmod-qca-nss-drv +ATH11K_THERMAL:kmod-hwmon-core +ATH11K_THERMAL:kmod-thermal
+	+kmod-crypto-michael-mic +ATH11K_THERMAL:kmod-hwmon-core +ATH11K_THERMAL:kmod-thermal +@ipq807x:kmod-qca-nss-drv
   FILES:=$(PKG_BUILD_DIR)/drivers/net/wireless/ath/ath11k/ath11k.ko
   AUTOLOAD:=$(call AutoProbe,ath11k)
 endef
