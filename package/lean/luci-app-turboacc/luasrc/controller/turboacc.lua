f6ac73eeebb0ea3cabfea07166ac24cd7655cfbf
Tianling Shen <cnsztl@immortalwrt.org>
2021-06-29 20:38:35 +0800

    luci-app-turboacc: add the new offloading controller
    The new LuCI controller rewritten by Tianling, based on the previous
    version of luci-app-flowoffload written by Lean and modified by ntlf9t.
    
    This app merged flow offloading and shortcut-fe support into a single
    package, and you can switch to each of them easily without conflicts
    or rebooting. Also adapted the Qualcomm QCA NSS ECM driver
    (controlling is not supported yet).
    
    For DNS caching, the new promgrams dnsforwarder and dnsproxy were
    supported, the latter one supports more advanced procotols like DoT,
    DoH, and QUIC. It is useful for users whose ISP hijacked their DNS
    requests.
    
    Re-licensed under GPL-3.0-only.
    
    Signed-off-by: Tianling Shen <cnsztl@immortalwrt.org>

---
+++ package/lean/luci-app-turboacc/luasrc/controller/turboacc.lua
@@ -0,0 +1,39 @@
+module("luci.controller.turboacc", package.seeall)
+
+function index()
+	if not nixio.fs.access("/etc/config/turboacc") then
+		return
+	end
+	local page
+	page = entry({"admin", "network", "turboacc"}, cbi("turboacc"), _("Turbo ACC Center"), 101)
+	page.i18n = "turboacc"
+	page.dependent = true
+	
+	entry({"admin", "network", "turboacc", "status"}, call("action_status"))
+end
+
+local function fastpath_status()
+	return luci.sys.call("/etc/init.d/turboacc check_status fastpath") == 0
+end
+
+local function bbr_status()
+	return luci.sys.call("/etc/init.d/turboacc check_status bbr") == 0
+end
+
+local function fullconenat_status()
+	return luci.sys.call("/etc/init.d/turboacc check_status fullconenat") == 0
+end
+
+local function dnscaching_status()
+	return luci.sys.call("/etc/init.d/turboacc check_status dns") == 0
+end
+
+function action_status()
+	luci.http.prepare_content("application/json")
+	luci.http.write_json({
+		fastpath_state = fastpath_status(),
+		fullconenat_state = fullconenat_status(),
+		bbr_state = bbr_status(),
+		dnscaching_state = dnscaching_status()
+	})
+end
