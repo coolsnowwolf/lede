a8ad0940bf17ddc07a138be6014c64b35f37e47a
Beginner <70857188+Beginner-Go@users.noreply.github.com>
2021-07-21 13:33:35 +0800

    kernel: bump 5.10 to 5.10.51 (#7437)
    * kernel: bump 5.10 to 5.10.51
    
    No deleted or manually refreshed patches.
    
    Signed-off-by: Rui Salvaterra <rsalvaterra@gmail.com>
    
    * linux/rockchip: update the USB 3.0 controller node patch
    
    This has been added in Linux 5.10.51 [1], but it's broken/incomplete. Update our
    patch and refresh the remaining patches.
    
    [1] https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/commit/?h=v5.10.51&id=421aff50af5e4cdc56b3ac8d6b670e09697bc8ac
    
    Signed-off-by: Rui Salvaterra <rsalvaterra@gmail.com>
    
    Co-authored-by: Rui Salvaterra <rsalvaterra@gmail.com>

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -7,10 +7,10 @@ ifdef CONFIG_TESTING_KERNEL
 endif
 
 LINUX_VERSION-5.4 = .132
-LINUX_VERSION-5.10 = .50
+LINUX_VERSION-5.10 = .51
 
 LINUX_KERNEL_HASH-5.4.132 = 8466adbfb3579e751ede683496df7bb20f258b5f882250f3dd82be63736d00ef
-LINUX_KERNEL_HASH-5.10.50 = 8bda327a7d95acfff8f87fb6ef4223e3194fa22195f5551249a9aa3393bfb436
+LINUX_KERNEL_HASH-5.10.51 = 95bae893c274ccc3a8a6271f377bcc7fd3badcb7990ecd41b05b2731f1d67ae2
 
 remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
 sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
