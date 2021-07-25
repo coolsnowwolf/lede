3977f162147d4a60430b8f64ce69e745f566a7ae
coolsnowwolf <coolsnowwolf@gmail.com>
2021-07-25 17:53:08 +0800

    ipq807x: add autocore-arm and some packages to target

--- include/target.mk
+++ include/target.mk
@@ -49,7 +49,7 @@ DEFAULT_PACKAGES.router:=\
 	luci-newapi block-mount coremark kmod-nf-nathelper kmod-nf-nathelper-extra kmod-ipt-raw \
 	default-settings luci luci-app-ddns luci-app-upnp luci-app-autoreboot \
 	luci-app-filetransfer luci-app-vsftpd luci-app-ssr-plus luci-app-unblockmusic luci-app-arpbind \
-	luci-app-vlmcsd luci-app-wol luci-app-ramfree luci-app-pptp-server \
+	luci-app-vlmcsd luci-app-wol luci-app-ramfree \
 	luci-app-turboacc luci-app-nlbwmon luci-app-accesscontrol ddns-scripts_aliyun ddns-scripts_dnspod
 
 ifneq ($(DUMP),)
