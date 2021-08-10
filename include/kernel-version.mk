f399e6abc3a3d89b8496fb26fcb1533e6dce7c62
boos4721 <3.1415926535boos@gmail.com>
2021-08-10 15:26:54 +0800

    kernel: bump 5.10 to 5.10.57 No deleted or manually refreshed patches.
    Build system: x86_64
    Build-tested: ipq807x/AX3600
    Run-tested: ipq807x/AX3600
    
    Signed-off-by: boos4721 <3.1415926535boos@gmail.com>

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -7,10 +7,10 @@ ifdef CONFIG_TESTING_KERNEL
 endif
 
 LINUX_VERSION-5.4 = .132
-LINUX_VERSION-5.10 = .56
+LINUX_VERSION-5.10 = .57
 
 LINUX_KERNEL_HASH-5.4.132 = 8466adbfb3579e751ede683496df7bb20f258b5f882250f3dd82be63736d00ef
-LINUX_KERNEL_HASH-5.10.56 = 4d6a0d5f9c50fa44890e0b61e1fb63f6efe6be448ceddfe1ad7c0cbd2890ec6b
+LINUX_KERNEL_HASH-5.10.57 = 00bbaeaac17f82d9a6d93cbc42cafd39d3b2fa3a6087333503d2344fa5e3142da
 
 remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
 sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
