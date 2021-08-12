3bf488ec6afa65c7c3336ee424cf55e75a58be15
Rui Salvaterra <rsalvaterra@gmail.com>
2021-08-13 00:16:29 +0100

    kernel: bump 5.10 to 5.10.58
    No deleted or manually refreshed patches.
    
    Signed-off-by: Rui Salvaterra <rsalvaterra@gmail.com>

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -7,10 +7,10 @@ ifdef CONFIG_TESTING_KERNEL
 endif
 
 LINUX_VERSION-5.4 = .138
-LINUX_VERSION-5.10 = .57
+LINUX_VERSION-5.10 = .58
 
 LINUX_KERNEL_HASH-5.4.138 = a43957727a54e5e1035d7372a1e64203ae8e3060c52a3816916157967f9a8657
-LINUX_KERNEL_HASH-5.10.57 = 00bbaeaac17f82d9a6d93cbc42cafd39d3b2fa3a6087333503d2344fa5e3142d
+LINUX_KERNEL_HASH-5.10.58 = 732cc56be539e65894621c3b6b2640597d8cf5da17319b82da6e7b4f7828f210
 
 remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
 sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
