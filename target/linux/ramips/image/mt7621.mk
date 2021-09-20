3cda23fcfe8f3e56acf8aca9f555cc8667bbc917
AmadeusGhost <42570690+AmadeusGhost@users.noreply.github.com>
2021-09-20 23:29:40 +0800

    ramips: add support for JCG JHR-AC945M
    Hardware specs:
      SoC: MediaTek MT7621AT
      Flash: SPI NOR 16MB
      RAM: DDR3 256MB
      2.4GHz: MT7615N bgn 4x4
      5GHz: MT7615N nac 4x4
      Ethernet: 1 x WAN and 4 x LAN
      USB: 1 x 3.0
      Button: Reset, WPS
    
    Unsupported chips:
      ATMEGA328P-AU
      HCLTEK HT1621B
    
    Flash instructions:
      Upload factory.bin in stock firmware's upgrade page.
    
    Signed-off-by: AmadeusGhost <amadeus@jmu.edu.cn>

--- target/linux/ramips/image/mt7621.mk
+++ target/linux/ramips/image/mt7621.mk
@@ -759,6 +759,19 @@ define Device/jcg_jhr-ac876m
 endef
 TARGET_DEVICES += jcg_jhr-ac876m
 
+define Device/jcg_jhr-ac945m
+  $(Device/dsa-migration)
+  $(Device/uimage-lzma-loader)
+  IMAGE_SIZE := 16064k
+  IMAGES += factory.bin
+  IMAGE/factory.bin := $$(sysupgrade_bin) | check-size | jcg-header 91.1
+  JCG_MAXSIZE := 16064k
+  DEVICE_VENDOR := JCG
+  DEVICE_MODEL := JHR-AC945M
+  DEVICE_PACKAGES := kmod-mt7615e kmod-mt7615-firmware kmod-usb3
+endef
+TARGET_DEVICES += jcg_jhr-ac945m
+
 define Device/jcg_q20
   $(Device/dsa-migration)
   BLOCKSIZE := 128k
