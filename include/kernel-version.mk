73480767b31e34e4ec2c3e79321ce4613452873c
Rui Salvaterra <rsalvaterra@gmail.com>
2021-09-27 09:20:47 +0100

    kernel: bump 5.10 to 5.10.69
    Patches automatically refreshed.
    
    Signed-off-by: Rui Salvaterra <rsalvaterra@gmail.com>

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -7,10 +7,10 @@ ifdef CONFIG_TESTING_KERNEL
 endif
 
 LINUX_VERSION-5.4 = .150
-LINUX_VERSION-5.10 = .68
+LINUX_VERSION-5.10 = .69
 
 LINUX_KERNEL_HASH-5.4.150 = f424a9bbb05007f04c17f96a2e4f041a8001554a9060d2c291606e8a97c62aa2
-LINUX_KERNEL_HASH-5.10.68 = 1baa830e3d359464e3762c30b96c1ba450a34d97834a57e455618c99de229421
+LINUX_KERNEL_HASH-5.10.69 = 232f9ec53bccca768868831b7cc286f7d8bbbac9f481bbac1495700231a20cca
 
 remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
 sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
