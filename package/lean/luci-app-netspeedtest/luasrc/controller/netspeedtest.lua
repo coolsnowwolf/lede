a4ae1925bc12e858d32c2195791e86dcb63bf812
Beginner-Go <70857188+Beginner-Go@users.noreply.github.com>
2021-09-28 13:10:29 +0000

    luci-app-netspeedtest: rewrite makefile
    Signed-off-by: Beginner-Go <70857188+Beginner-Go@users.noreply.github.com>

--- package/lean/luci-app-netspeedtest/luasrc/controller/netspeedtest.lua
+++ package/lean/luci-app-netspeedtest/luasrc/controller/netspeedtest.lua
@@ -1,20 +1,17 @@
-
 module("luci.controller.netspeedtest", package.seeall)
 
 function index()
+	if not nixio.fs.access("/etc/config/netspeedtest") then
+		return
+	end
+	
+	entry({"admin", "network", "netspeedtest"}, cbi("netspeedtest", {hideapplybtn=true, hidesavebtn=true, hideresetbtn=true}),_("Netspeedtest"),91).dependent = true
+	entry({"admin", "network", "test_iperf0"}, post("test_iperf0"), nil).leaf = true
+	entry({"admin", "network", "test_iperf1"}, post("test_iperf1"), nil).leaf = true
 
-	entry({"admin","network","netspeedtest"},cbi("netspeedtest/netspeedtest", {hideapplybtn=true, hidesavebtn=true, hideresetbtn=true}),_("网速测试"),91).dependent=true
-
-	entry({"admin", "network","test_iperf0"}, post("test_iperf0"), nil).leaf = true
-
-	entry({"admin", "network","test_iperf1"}, post("test_iperf1"), nil).leaf = true
-
-	entry({"admin","network","netspeedtest", "run"}, call("run"))
-
+	entry({"admin", "network", "netspeedtest", "run"}, call("run"))
 	entry({"admin", "network", "netspeedtest", "realtime_log"}, call("get_log")) 
-
 end
-
 
 function testlan(cmd, addr)
 		luci.http.prepare_content("text/plain")
@@ -45,15 +42,15 @@ function test_iperf1(addr)
 end
 
 function get_log()
-    local fs = require "nixio.fs"
-    local e = {}
-    e.running = luci.sys.call("busybox ps -w | grep netspeedtest | grep -v grep >/dev/null") == 0
-    e.log = fs.readfile("/var/log/netspeedtest.log") or ""
+	local fs = require "nixio.fs"
+	local e = {}
+	e.running = luci.sys.call("busybox ps -w | grep netspeedtest | grep -v grep >/dev/null") == 0
+	e.log = fs.readfile("/var/log/netspeedtest.log") or ""
 	luci.http.prepare_content("application/json")
 	luci.http.write_json(e)
 end
 
 function run()
-    testwan("/etc/init.d/netspeedtest nstest ")
+	testwan("/etc/init.d/netspeedtest nstest ")
 	luci.http.redirect(luci.dispatcher.build_url("admin","network","netspeedtest"))
 end
