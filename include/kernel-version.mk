a9c5c7aa6b48e1249f65bd42c1b6648d7498652b
Beginner <70857188+Beginner-Go@users.noreply.github.com>
2021-10-04 22:25:52 +0800

    kernel: bump 5.10 to 5.10.70 (#7976)
    * kernel: bump 5.10 to 5.10.65
    
    Manually refreshed:
    generic/pending-5.10/800-bcma-get-SoC-device-struct-copy-its-DMA-params-to-th.patch
    bcm27xx/patches-5.10/950-0053-firmware-bcm2835-Support-ARCH_BCM270x.patch
    bcm27xx/patches-5.10/950-0087-firmware-raspberrypi-Add-backward-compatible-get_thr.patch
    
    Signed-off-by: Rui Salvaterra <rsalvaterra@gmail.com>
    
    * kernel: bump 5.10 to 5.10.66
    
    No patches refreshed.
    
    Signed-off-by: Rui Salvaterra <rsalvaterra@gmail.com>
    
    * kernel: bump 5.10 to 5.10.67
    
    Deleted (upstreamed):
    bcm27xx/patches-5.10/950-0312-vc4_hdmi-Set-HD_CTL_WHOLSMP-and-HD_CTL_CHALIGN_SET.patch
    
    Signed-off-by: Rui Salvaterra <rsalvaterra@gmail.com>
    
    * kernel: bump 5.10 to 5.10.68
    
    Deleted (upstreamed):
    generic/backport-5.10/610-v5.13-02-netfilter-Fix-fall-through-warnings-for-Clang.patch
    generic/backport-5.10/792-v5.15-0001-net-dsa-b53-Fix-calculating-number-of-switch-ports.patch
    generic/backport-5.10/792-v5.15-0002-net-dsa-b53-Set-correct-number-of-ports-in-the-DSA-s.patch
    generic/backport-5.10/792-v5.15-0003-net-dsa-b53-Fix-IMP-port-setup-on-BCM5301x.patch
    generic/backport-5.10/840-0001-PCI-of-Don-t-fail-devm_pci_alloc_host_bridge-on-miss.patch
    generic/backport-5.10/840-0002-PCI-iproc-Fix-BCMA-probe-resource-handling.patch
    generic/pending-5.10/498-mtd-mtdconcat-select-readwrite-function.patch
    
    Signed-off-by: Rui Salvaterra <rsalvaterra@gmail.com>
    
    * kernel: bump 5.10 to 5.10.69
    
    Patches automatically refreshed.
    
    Signed-off-by: Rui Salvaterra <rsalvaterra@gmail.com>
    
    * kernel: bump 5.10 to 5.10.70
    
    Deleted (upstreamed):
    bcm53xx/patches-5.10/181-Revert-USB-bcma-Add-a-check-for-devm_gpiod_get.patch
    
    Signed-off-by: Rui Salvaterra <rsalvaterra@gmail.com>
    
    Co-authored-by: Rui Salvaterra <rsalvaterra@gmail.com>

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -7,10 +7,10 @@ ifdef CONFIG_TESTING_KERNEL
 endif
 
 LINUX_VERSION-5.4 = .150
-LINUX_VERSION-5.10 = .64
+LINUX_VERSION-5.10 = .70
 
 LINUX_KERNEL_HASH-5.4.150 = f424a9bbb05007f04c17f96a2e4f041a8001554a9060d2c291606e8a97c62aa2
-LINUX_KERNEL_HASH-5.10.64 = 3eb84bd24a2de2b4749314e34597c02401c5d6831b055ed5224adb405c35e30a
+LINUX_KERNEL_HASH-5.10.70 = 75736c33711d7f298889ee4981b18774882045323e5c46af9ad3b022082db232
 
 remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
 sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
