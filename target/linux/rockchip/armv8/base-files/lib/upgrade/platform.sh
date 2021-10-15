coolsnowwolf/lede

commit b1b36c86af6d9fa3ba57eb2140025d2039996b81

rockchip: add yt85xx eth support for R2C

target/linux/rockchip/armv8/base-files/lib/preinit/79_move_config
@@ -5,8 +5,8 @@ move_config() {
 
 	if export_bootdevice && export_partdevice partdev 1; then
 		if mount -o rw,noatime "/dev/$partdev" /mnt; then
-			if [ -f "/mnt/$BACKUP_FILE" ]; then
-				mv -f "/mnt/$BACKUP_FILE" /
+			if [ -f "/mnt/sysupgrade.tgz" ]; then
+				mv -f "/mnt/sysupgrade.tgz" /
 			fi
 			umount /mnt
 		fi

target/linux/rockchip/armv8/base-files/lib/upgrade/platform.sh
@@ -30,7 +30,7 @@ platform_copy_config() {
 
 	if export_partdevice partdev 1; then
 		mount -o rw,noatime "/dev/$partdev" /mnt
-		cp -af "$UPGRADE_BACKUP" "/mnt/$BACKUP_FILE"
+		cp -af "$CONF_TAR" "/mnt/sysupgrade.tgz"
 		umount /mnt
 	fi
 }
@@ -45,7 +45,7 @@ platform_do_upgrade() {
 
 	sync
 
-	if [ "$UPGRADE_OPT_SAVE_PARTITIONS" = "1" ]; then
+	if [ "$SAVE_PARTITIONS" = "1" ]; then
 		get_partitions "/dev/$diskdev" bootdisk
 
 		#extract the boot sector from the image

target/linux/rockchip/Makefile
@@ -17,8 +17,7 @@ endef
 include $(INCLUDE_DIR)/target.mk
 
 DEFAULT_PACKAGES += uboot-envtools partx-utils e2fsprogs mkf2fs kmod-gpio-button-hotplug \
-		    automount autocore-arm autosamba fdisk cfdisk e2fsprogs ethtool haveged htop \
-		    luci-app-zerotier luci-app-ipsec-vpnd
+		    automount autocore-arm fdisk e2fsprogs
 
 KERNELNAME:=Image dtbs
 

target/linux/rockchip/armv8/base-files/etc/hotplug.d/net/40-net-smp-affinity
@@ -4,8 +4,14 @@
 
 get_device_irq() {
 	local device="$1"
+	local line
+	local seconds
 
-	local line=$(grep -m 1 "${device}\$" /proc/interrupts)
+	# wait up to 10 seconds for the irq/device to appear
+	for seconds in $(seq 0 9); do
+		line=$(grep -m 1 "${device}\$" /proc/interrupts) && break
+		sleep 1
+	done
 	echo ${line} | sed 's/:.*//'
 }
 
@@ -22,6 +28,7 @@ set_interface_core() {
 }
 
 case "$(board_name)" in
+friendlyarm,nanopi-r2c|\
 friendlyarm,nanopi-r2s|\
 xunlong,orangepi-r1-plus|\
 embedfire,doornet1)

target/linux/rockchip/armv8/config-5.10
@@ -18,9 +18,6 @@ CONFIG_ARC_EMAC_CORE=y
 CONFIG_ARM64=y
 CONFIG_ARM64_4K_PAGES=y
 CONFIG_ARM64_CNP=y
-# CONFIG_ARM64_ERRATUM_1165522 is not set
-# CONFIG_ARM64_ERRATUM_1286807 is not set
-# CONFIG_ARM64_ERRATUM_1418040 is not set
 CONFIG_ARM64_ERRATUM_819472=y
 CONFIG_ARM64_ERRATUM_824069=y
 CONFIG_ARM64_ERRATUM_826319=y
@@ -92,7 +89,6 @@ CONFIG_BLOCK_COMPAT=y
 CONFIG_BRCMSTB_GISB_ARB=y
 CONFIG_BSD_PROCESS_ACCT=y
 CONFIG_BSD_PROCESS_ACCT_V3=y
-# CONFIG_CAVIUM_TX2_ERRATUM_219 is not set
 # CONFIG_CHARGER_BQ25980 is not set
 CONFIG_CHARGER_GPIO=y
 CONFIG_CLKDEV_LOOKUP=y
@@ -223,7 +219,6 @@ CONFIG_FRAME_WARN=2048
 CONFIG_FS_IOMAP=y
 CONFIG_FS_MBCACHE=y
 CONFIG_FS_POSIX_ACL=y
-# CONFIG_FUJITSU_ERRATUM_010001 is not set
 CONFIG_FW_LOADER_PAGED_BUF=y
 CONFIG_GENERIC_ALLOCATOR=y
 CONFIG_GENERIC_ARCH_TOPOLOGY=y
@@ -375,6 +370,7 @@ CONFIG_MMC_SDHCI_OF_DWCMSHC=y
 # CONFIG_MMC_SDHCI_PCI is not set
 CONFIG_MMC_SDHCI_PLTFM=y
 CONFIG_MODULES_USE_ELF_RELA=y
+CONFIG_MOTORCOMM_PHY=y
 CONFIG_MQ_IOSCHED_DEADLINE=y
 # CONFIG_MTD_CFI is not set
 CONFIG_MTD_CMDLINE_PARTS=y
@@ -492,10 +488,6 @@ CONFIG_RANDOMIZE_MODULE_REGION_FULL=y
 CONFIG_RAS=y
 CONFIG_RATIONAL=y
 # CONFIG_RAVE_SP_CORE is not set
-CONFIG_RCU_CPU_STALL_TIMEOUT=21
-# CONFIG_RCU_EXPERT is not set
-CONFIG_RCU_NEED_SEGCBLIST=y
-CONFIG_RCU_STALL_COMMON=y
 CONFIG_RCU_TRACE=y
 CONFIG_REALTEK_PHY=y
 CONFIG_REGMAP=y
@@ -590,7 +582,6 @@ CONFIG_SRAM=y
 CONFIG_SRCU=y
 CONFIG_STACKPROTECTOR=y
 CONFIG_STACKPROTECTOR_STRONG=y
-# CONFIG_STAGING is not set
 CONFIG_STMMAC_ETH=y
 CONFIG_STMMAC_PLATFORM=y
 # CONFIG_STMMAC_SELFTESTS is not set

target/linux/rockchip/armv8/config-5.4
@@ -19,9 +19,6 @@ CONFIG_ARM64=y
 CONFIG_ARM64_4K_PAGES=y
 CONFIG_ARM64_CNP=y
 CONFIG_ARM64_CONT_SHIFT=4
-# CONFIG_ARM64_ERRATUM_1165522 is not set
-# CONFIG_ARM64_ERRATUM_1286807 is not set
-# CONFIG_ARM64_ERRATUM_1418040 is not set
 CONFIG_ARM64_ERRATUM_819472=y
 CONFIG_ARM64_ERRATUM_824069=y
 CONFIG_ARM64_ERRATUM_826319=y
@@ -91,7 +88,6 @@ CONFIG_BLOCK_COMPAT=y
 CONFIG_BRCMSTB_GISB_ARB=y
 CONFIG_BSD_PROCESS_ACCT=y
 CONFIG_BSD_PROCESS_ACCT_V3=y
-# CONFIG_CAVIUM_TX2_ERRATUM_219 is not set
 CONFIG_CHARGER_GPIO=y
 CONFIG_CLKDEV_LOOKUP=y
 CONFIG_CLKSRC_MMIO=y
@@ -183,6 +179,7 @@ CONFIG_DMA_REMAP=y
 CONFIG_DMA_SHARED_BUFFER=y
 CONFIG_DNOTIFY=y
 CONFIG_DRM_RCAR_WRITEBACK=y
+# CONFIG_DRM_ROCKCHIP is not set
 CONFIG_DTC=y
 CONFIG_DT_IDLE_STATES=y
 CONFIG_DUMMY_CONSOLE=y
@@ -209,7 +206,6 @@ CONFIG_FRAME_WARN=2048
 CONFIG_FS_IOMAP=y
 CONFIG_FS_MBCACHE=y
 CONFIG_FS_POSIX_ACL=y
-# CONFIG_FUJITSU_ERRATUM_010001 is not set
 CONFIG_FW_LOADER_PAGED_BUF=y
 CONFIG_GENERIC_ALLOCATOR=y
 CONFIG_GENERIC_ARCH_TOPOLOGY=y
@@ -280,6 +276,7 @@ CONFIG_INPUT_KEYBOARD=y
 CONFIG_INPUT_LEDS=y
 CONFIG_INPUT_MATRIXKMAP=y
 # CONFIG_INPUT_MISC is not set
+# CONFIG_INPUT_RK805_PWRKEY is not set
 CONFIG_IOMMU_API=y
 # CONFIG_IOMMU_DEBUGFS is not set
 # CONFIG_IOMMU_DEFAULT_PASSTHROUGH is not set
@@ -355,6 +352,7 @@ CONFIG_MMC_SDHCI_OF_DWCMSHC=y
 # CONFIG_MMC_SDHCI_PCI is not set
 CONFIG_MMC_SDHCI_PLTFM=y
 CONFIG_MODULES_USE_ELF_RELA=y
+CONFIG_MOTORCOMM_PHY=y
 CONFIG_MQ_IOSCHED_DEADLINE=y
 # CONFIG_MTD_CFI is not set
 CONFIG_MTD_CMDLINE_PARTS=y
@@ -470,10 +468,6 @@ CONFIG_RANDOMIZE_MODULE_REGION_FULL=y
 CONFIG_RAS=y
 CONFIG_RATIONAL=y
 # CONFIG_RAVE_SP_CORE is not set
-CONFIG_RCU_CPU_STALL_TIMEOUT=21
-# CONFIG_RCU_EXPERT is not set
-CONFIG_RCU_NEED_SEGCBLIST=y
-CONFIG_RCU_STALL_COMMON=y
 CONFIG_RCU_TRACE=y
 CONFIG_REALTEK_PHY=y
 CONFIG_REFCOUNT_FULL=y
@@ -490,6 +484,8 @@ CONFIG_REGULATOR_RK808=y
 CONFIG_RELOCATABLE=y
 CONFIG_RESET_CONTROLLER=y
 CONFIG_RFS_ACCEL=y
+# CONFIG_ROCKCHIP_ANALOGIX_DP is not set
+# CONFIG_ROCKCHIP_CDN_DP is not set
 CONFIG_ROCKCHIP_EFUSE=y
 CONFIG_ROCKCHIP_GRF=y
 CONFIG_ROCKCHIP_IODOMAIN=y
@@ -497,6 +493,9 @@ CONFIG_ROCKCHIP_IOMMU=y
 CONFIG_ROCKCHIP_MBOX=y
 CONFIG_ROCKCHIP_PHY=y
 CONFIG_ROCKCHIP_PM_DOMAINS=y
+# CONFIG_ROCKCHIP_RGB is not set
+# CONFIG_ROCKCHIP_RK3066_HDMI is not set
+# CONFIG_ROCKCHIP_SARADC is not set
 CONFIG_ROCKCHIP_THERMAL=y
 CONFIG_ROCKCHIP_TIMER=y
 CONFIG_RODATA_FULL_DEFAULT_ENABLED=y
@@ -540,6 +539,7 @@ CONFIG_SG_POOL=y
 CONFIG_SIMPLE_PM_BUS=y
 CONFIG_SLUB_DEBUG=y
 CONFIG_SMP=y
+# CONFIG_SND_SOC_ROCKCHIP is not set
 CONFIG_SPARSEMEM=y
 CONFIG_SPARSEMEM_EXTREME=y
 CONFIG_SPARSEMEM_MANUAL=y
@@ -562,7 +562,6 @@ CONFIG_SRAM=y
 CONFIG_SRCU=y
 CONFIG_STACKPROTECTOR=y
 CONFIG_STACKPROTECTOR_STRONG=y
-# CONFIG_STAGING is not set
 CONFIG_STMMAC_ETH=y
 CONFIG_STMMAC_PLATFORM=y
 # CONFIG_STMMAC_SELFTESTS is not set

target/linux/rockchip/image/armv8.mk
@@ -71,4 +71,5 @@ define Device/embedfire_doornet1
   IMAGE/sysupgrade.img.gz := boot-common | boot-script nanopi-r2s | pine64-bin | gzip | append-metadata
   DEVICE_PACKAGES := kmod-usb-net-rtl8152 
 endef
-TARGET_DEVICES += embedfire_doornet1
+TARGET_DEVICES += embedfire_doornet1 
+

commit fb8a887bdff73c286838583c22696304235299ba

rockchip: revert upgrade part change again
Fixes: b1b36c86af6d9fa3ba57eb2140025d2039996b81

target/linux/rockchip/armv8/base-files/lib/preinit/79_move_config
@@ -5,8 +5,8 @@ move_config() {
 
 	if export_bootdevice && export_partdevice partdev 1; then
 		if mount -o rw,noatime "/dev/$partdev" /mnt; then
-			if [ -f "/mnt/sysupgrade.tgz" ]; then
-				mv -f "/mnt/sysupgrade.tgz" /
+			if [ -f "/mnt/$BACKUP_FILE" ]; then
+				mv -f "/mnt/$BACKUP_FILE" /
 			fi
 			umount /mnt
 		fi

target/linux/rockchip/armv8/base-files/lib/upgrade/platform.sh
@@ -30,7 +30,7 @@ platform_copy_config() {
 
 	if export_partdevice partdev 1; then
 		mount -o rw,noatime "/dev/$partdev" /mnt
-		cp -af "$CONF_TAR" "/mnt/sysupgrade.tgz"
+		cp -af "$UPGRADE_BACKUP" "/mnt/$BACKUP_FILE"
 		umount /mnt
 	fi
 }
@@ -45,7 +45,7 @@ platform_do_upgrade() {
 
 	sync
 
-	if [ "$SAVE_PARTITIONS" = "1" ]; then
+	if [ "$UPGRADE_OPT_SAVE_PARTITIONS" = "1" ]; then
 		get_partitions "/dev/$diskdev" bootdisk
 
 		#extract the boot sector from the image