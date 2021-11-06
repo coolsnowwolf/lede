ae5218ab9d59ef2685e83622579f9c0570760491
svswmw <47269902+SVSWMW@users.noreply.github.com>
2021-11-06 17:48:05 +0800

    Update mac80211.sh

--- package/kernel/mac80211/files/lib/wifi/mac80211.sh
+++ package/kernel/mac80211/files/lib/wifi/mac80211.sh
@@ -182,7 +182,8 @@ detect_mac80211() {
 			set wireless.default_radio${devidx}.network=lan
 			set wireless.default_radio${devidx}.mode=ap
 			set wireless.default_radio${devidx}.ssid=S2109
-			set wireless.default_radio${devidx}.encryption=none
+			set wireless.default_radio${devidx}.encryption=psk2
+			set wireless.default_radio${devidx}.key=dsvw4466
 EOF
 		uci -q commit wireless
 
