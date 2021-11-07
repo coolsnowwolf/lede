0734de10acf02e03f4797fd45d9873ea55ce239b
Beginner-Go <70857188+Beginner-Go@users.noreply.github.com>
2021-11-07 14:38:59 +0000

    luci-app-xlnetacc: tidy up code

--- package/lean/luci-app-xlnetacc/luasrc/model/cbi/xlnetacc.lua
+++ package/lean/luci-app-xlnetacc/luasrc/model/cbi/xlnetacc.lua
@@ -1,51 +1,57 @@
 local m,s,o
-local uci=luci.model.uci.cursor()
+local uci = luci.model.uci.cursor()
 
-m=Map("xlnetacc","%s - %s"%{translate("XLNetAcc"),translate("Settings")},translate("XLNetAcc is a Thunder joint broadband operators launched a commitment to help users solve the low broadband, slow Internet access, poor Internet experience of professional-grade broadband upgrade software."))
-m:append(Template("xlnetacc/xlnetacc_status"))
+m = Map("xlnetacc")
+m.title = translate("XLNetAcc - Settings")
+m.description = translate("XLNetAcc is a Thunder joint broadband operators launched a commitment to help users solve the low broadband, slow Internet access, poor Internet experience of professional-grade broadband upgrade software.")
 
-s=m:section(NamedSection,"general","general",translate("General Settings"))
-s.anonymous=true
-s.addremove=false
+m:section(SimpleSection).template = "xlnetacc/xlnetacc_status"
 
-o=s:option(Flag,"enabled",translate("Enabled"))
-o.rmempty=false
+s = m:section(NamedSection, "general", "general")
+s.title = translate("General Settings")
+s.anonymous = true
+s.addremove = false
 
-o=s:option(Flag,"down_acc",translate("Enable DownLink Upgrade"))
+o = s:option(Flag, "enabled", translate("Enabled"))
+o.rmempty = false
 
-o=s:option(Flag,"up_acc",translate("Enable UpLink Upgrade"))
+o = s:option(Flag, "down_acc", translate("Enable DownLink Upgrade"))
 
-o=s:option(Flag,"logging",translate("Enable Logging"))
-o.default=1
+o = s:option(Flag, "up_acc", translate("Enable UpLink Upgrade"))
 
-o=s:option(Flag,"verbose",translate("Enable verbose logging"))
+o = s:option(Flag, "logging", translate("Enable Logging"))
+o.default = 1
+
+o = s:option(Flag, "verbose", translate("Enable verbose logging"))
 o:depends("logging",1)
 
-o=s:option(ListValue,"network",translate("Upgrade interface"))
+o = s:option(ListValue, "network", translate("Upgrade interface"))
 uci:foreach("network","interface",function(section)
 	if section[".name"]~="loopback" then
 		o:value(section[".name"])
 	end
 end)
 
-o=s:option(Value,"keepalive",translate("Keepalive interval"),"5-60 "..translate("minutes"))
+o = s:option(Value, "keepalive", translate("Keepalive interval"))
+o.description = translate("5 ~ 60 minutes")
 for _,v in ipairs({5,10,20,30,60}) do
 	o:value(v,v.." "..translate("minutes"))
 end
-o.datatype="range(5,60)"
-o.default=10
+o.datatype = "range(5,60)"
+o.default = 10
 
-o=s:option(Value,"relogin",translate("Account relogin"),"1-48 "..translate("hours"))
-o:value(0,translate("Not enabled"))
+o = s:option(Value, "relogin", translate("Account relogin"))
+o.description = translate("1 ~ 48 hours")
+o:value(0, translate("Not enabled"))
 for _,v in ipairs({3,12,18,24,30}) do
 	o:value(v,v.." "..translate("hours"))
 end
-o.datatype="max(48)"
-o.default=0
+o.datatype = "max(48)"
+o.default = 0
 
-o=s:option(Value,"account",translate("XLNetAcc account"))
+o = s:option(Value, "account", translate("XLNetAcc account"))
 
-o=s:option(Value,"password",translate("XLNetAcc password"))
-o.password=true
+o = s:option(Value, "password", translate("XLNetAcc password"))
+o.password = true
 
 return m
