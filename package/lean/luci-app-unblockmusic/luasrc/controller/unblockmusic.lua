fed785d92f4b7ca8350b070ecbf1219cbfee065e
Beginner <70857188+Beginner-Go@users.noreply.github.com>
2021-11-10 15:44:55 +0800

    luci-app-unblockmusic: tidy up code (#8197)

--- package/lean/luci-app-unblockmusic/luasrc/controller/unblockmusic.lua
+++ package/lean/luci-app-unblockmusic/luasrc/controller/unblockmusic.lua
@@ -1,4 +1,3 @@
-
 module("luci.controller.unblockmusic", package.seeall)
 
 function index()
@@ -6,17 +5,15 @@ function index()
 		return
 	end
 
-	entry({"admin", "services", "unblockmusic"}, firstchild(), _("Unblock Netease Music"), 50).dependent = false
-	
-	entry({"admin", "services", "unblockmusic", "general"}, cbi("unblockmusic/unblockmusic"), _("Base Setting"), 1)
-	entry({"admin", "services", "unblockmusic", "log"}, form("unblockmusic/unblockmusiclog"), _("Log"), 2)
-  
+	entry({"admin", "services", "unblockmusic"}, alias("admin", "services", "unblockmusic", "general"), _("Unblock Netease Music"), 50).dependent = true
+	entry({"admin", "services", "unblockmusic", "general"}, cbi("unblockmusic/unblockmusic"), _("Base Setting"), 1).leaf = true
+	entry({"admin", "services", "unblockmusic", "log"}, form("unblockmusic/unblockmusic_log"), _("Log"), 2).leaf = true
 	entry({"admin", "services", "unblockmusic", "status"}, call("act_status")).leaf = true
 end
 
 function act_status()
-	local e={}
-	e.running=luci.sys.call("busybox ps -w | grep UnblockNeteaseMusic | grep -v grep | grep -v logcheck.sh >/dev/null")==0
+	local e = {}
+	e.running = luci.sys.call("busybox ps -w | grep UnblockNeteaseMusic | grep -v grep | grep -v logcheck.sh >/dev/null") == 0
 	luci.http.prepare_content("application/json")
 	luci.http.write_json(e)
 end
