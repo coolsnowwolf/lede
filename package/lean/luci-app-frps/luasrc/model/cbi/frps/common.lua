afa000aeeb796404aa3894149f7b25babe981a4a
zxlhhyccc <45259624+zxlhhyccc@users.noreply.github.com>
2021-11-27 16:13:04 +0800

    Update common.lua

--- package/lean/luci-app-frps/luasrc/model/cbi/frps/common.lua
+++ package/lean/luci-app-frps/luasrc/model/cbi/frps/common.lua
@@ -46,6 +46,7 @@ o:value("error",translate("Error"))
 o.default = "Warn"
 
 o = s:taboption("general", ListValue, "log_max_days", translate("Log max days"))
+o.description = translate("Maximum number of days to keep log files is 3 day.")
 o:depends("enable_logging", "1")
 o.datatype = "uinteger"
 o:value("1", translate("1"))
