5cd9acc5635f9c9eddd42e845347f6bea9ff4a6e
svswmw <47269902+SVSWMW@users.noreply.github.com>
2021-12-02 21:30:57 +0800

    Add files via upload

--- target/linux/ramips/image/mt7621.mk
+++ target/linux/ramips/image/mt7621.mk
@@ -1825,7 +1825,7 @@ define Device/zte_e8820s
 	check-size
   DEVICE_VENDOR := ZTE
   DEVICE_MODEL := E8820S
-  DEVICE_PACKAGES := kmod-mt7603 kmod-mt76x2 kmod-usb2 \
+  DEVICE_PACKAGES := kmod-mt7603e kmod-mt76x2e kmod-usb2 \
 	kmod-usb-ledtrig-usbport luci-app-mtwifi
 endef
 TARGET_DEVICES += zte_e8820s
