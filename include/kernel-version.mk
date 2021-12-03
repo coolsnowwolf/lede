0cd887f7b50847de8be1a53c9a936a9ed592669c
Beginner <70857188+Beginner-Go@users.noreply.github.com>
2021-12-03 12:10:21 +0800

    kernel: bump 5.10 to 5.10.83 (#8374)
    Removed upstreamed:
        bcm53xx/patches-5.10/033-v5.16-0024-ARM-dts-BCM5301X-Fix-I2C-controller-interrupt.patch[1]
        bcm53xx/patches-5.10/033-v5.16-0025-ARM-dts-BCM5301X-Add-interrupt-properties-to-GPIO-no.patch[2]
    
    All other patches automatically rebased.
    
    1. https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/commit/?h=v5.10.83&id=b2cd6fdcbe0a5cb44e4610a08cc58261d494a885
    2. https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/commit/?h=v5.10.83&id=9db1d4a3c2700e1cc84c3d61199411d75c2a3ec6
    
    Build system: x86_64*
    Build-tested: bcm2711/RPi4B, ipq806x/R7800†
    Run-tested: bcm2711/RPi4B, ipq806x/R7800†
    
    * Had to revert 7c99085bd69742f66207d61e9f2da5ec4f8f9d2f in order to build
      (latest bump of ca-certificates)
    
    † Had to revert 7f1edbd41295dff9f2127b169fbc086c0fb2c14e in order to build
      (binutils 2.37, https://bugs.openwrt.org/index.php?do=details&task_id=4149)
    
    Signed-off-by: graysky <graysky@archlinux.us>
    
    Co-authored-by: graysky <graysky@archlinux.us>

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -7,10 +7,10 @@ ifdef CONFIG_TESTING_KERNEL
 endif
 
 LINUX_VERSION-5.4 = .162
-LINUX_VERSION-5.10 = .82
+LINUX_VERSION-5.10 = .83
 
 LINUX_KERNEL_HASH-5.4.162 = c12d72ddaac78189305a5e98825295ecb02282970033b052276035e83189e25b
-LINUX_KERNEL_HASH-5.10.82 = d66f7c7eed02f84ae550b3c91a26c70eea13682a6d672432f07bce66ff8ce350
+LINUX_KERNEL_HASH-5.10.83 = ef259a43f33ddb56001283f4f4e50af29b8a48fa066aed7371a90ebf38c29b70
 
 remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
 sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
