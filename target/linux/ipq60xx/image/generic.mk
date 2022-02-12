49d5063ee304b7db22b73abad6175a74e0e8f8b8
lean <coolsnowwolf@gmail.com>
2022-02-12 19:41:23 +0800

    ipq60xx: add WF HR6001 support

--- target/linux/ipq60xx/image/generic.mk
+++ target/linux/ipq60xx/image/generic.mk
@@ -22,3 +22,17 @@ define Device/UbiFit
 	IMAGE/nand-factory.ubi := append-ubi
 	IMAGE/nand-sysupgrade.bin := sysupgrade-tar | append-metadata
 endef
+
+define Device/wf_hr6001
+	$(call Device/FitImage)
+	$(call Device/UbiFit)
+	DEVICE_VENDOR := WF
+	DEVICE_MODEL := HR6001
+	KERNEL_LOADADDR := 0x41000000
+	BLOCKSIZE := 128k
+	PAGESIZE := 2048
+	DEVICE_DTS_CONFIG := config@cp01-c1
+	SOC := ipq6018
+	DEVICE_PACKAGES :=  uboot-envtools
+endef
+TARGET_DEVICES += wf_hr6001
