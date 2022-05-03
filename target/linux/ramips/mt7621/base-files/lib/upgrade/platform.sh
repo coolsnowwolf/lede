79f3c3c1f8854725c6b4fecdef0c744dadaa7871
Lancet-z <55058538+Lancet-z@users.noreply.github.com>
2022-05-03 08:41:36 +0800

    Update platform.sh

--- target/linux/ramips/mt7621/base-files/lib/upgrade/platform.sh
+++ target/linux/ramips/mt7621/base-files/lib/upgrade/platform.sh
@@ -84,7 +84,8 @@ platform_do_upgrade() {
 	xiaomi,mi-router-4|\
 	xiaomi,mi-router-ac2100|\
 	xiaomi,mi-router-cr660x|\
-	xiaomi,redmi-router-ac2100)
+	xiaomi,redmi-router-ac2100|\
+	zte,e8820s)
 		nand_do_upgrade "$1"
 		;;
 	iodata,wn-ax1167gr2|\
