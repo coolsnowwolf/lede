ee7d9cff629778e16d1a34abc04ea3d6524d56bb
AmadeusGhost <42570690+AmadeusGhost@users.noreply.github.com>
2022-06-07 14:29:20 +0800

    Revert "kernel: bump 5.15 to 5.15.44 (#9457)"
    This reverts commit 91b3516, fixes: #9483

--- package/kernel/linux/modules/netsupport.mk
+++ package/kernel/linux/modules/netsupport.mk
@@ -1290,6 +1290,7 @@ define KernelPackage/wireguard
   SUBMENU:=$(NETWORK_SUPPORT_MENU)
   TITLE:=WireGuard secure network tunnel
   DEPENDS:= \
+	  +kmod-crypto-lib-blake2s \
 	  +kmod-crypto-lib-chacha20poly1305 \
 	  +kmod-crypto-lib-curve25519 \
 	  +kmod-udptunnel4 \
