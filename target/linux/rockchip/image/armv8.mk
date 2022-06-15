bbc1b28d1b96a4cd603b6141b627d6265c7410ed
Katteria <39751846+kisaragychihaya@users.noreply.github.com>
2022-06-15 20:50:43 +0800

    Drop R4S

--- target/linux/rockchip/image/armv8.mk
+++ target/linux/rockchip/image/armv8.mk
@@ -51,16 +51,6 @@ define Device/friendlyarm_nanopi-r2s
 endef
 TARGET_DEVICES += friendlyarm_nanopi-r2s
 
-define Device/friendlyarm_nanopi-r4s
-  DEVICE_VENDOR := FriendlyARM
-  DEVICE_MODEL := NanoPi R4S
-  SOC := rk3399
-  UBOOT_DEVICE_NAME := nanopi-r4s-rk3399
-  IMAGE/sysupgrade.img.gz := boot-common | boot-script nanopi-r4s | pine64-bin | gzip | append-metadata
-  DEVICE_PACKAGES := kmod-r8168 -urngd
-endef
-TARGET_DEVICES += friendlyarm_nanopi-r4s
-
 define Device/pine64_rockpro64
   DEVICE_VENDOR := Pine64
   DEVICE_MODEL := RockPro64
