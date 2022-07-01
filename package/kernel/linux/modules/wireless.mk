79b5e1dd518722cdb60e140714f16cc89d170026
Christian Lamparter <chunkeey@gmail.com>
2022-07-01 21:39:10 +0200

    module/firmware: remove intersil PRISM54 support
    the legacy driver was dropped in linux 5.14-rc3:
    commit d249ff28b1d8 ("intersil: remove obsolete prism54 wireless driver")
    
    Quoting Lukas Bulwahn:
    "p54 replaces prism54 so users should be unaffected."
    
    Reported-by: Marius Dinu <m95d+git@psihoexpert.ro>
    Signed-off-by: Christian Lamparter <chunkeey@gmail.com>

--- package/kernel/linux/modules/wireless.mk
+++ package/kernel/linux/modules/wireless.mk
@@ -7,23 +7,6 @@
 
 WIRELESS_MENU:=Wireless Drivers
 
-define KernelPackage/net-prism54
-  SUBMENU:=$(WIRELESS_MENU)
-  TITLE:=Intersil Prism54 support
-  DEPENDS:=@PCI_SUPPORT +@DRIVER_WEXT_SUPPORT +prism54-firmware
-  KCONFIG:=CONFIG_PRISM54
-  FILES:= \
-	$(LINUX_DIR)/drivers/net/wireless/intersil/prism54/prism54.ko
-  AUTOLOAD:=$(call AutoProbe,prism54)
-endef
-
-define KernelPackage/net-prism54/description
- Kernel modules for Intersil Prism54 support
-endef
-
-$(eval $(call KernelPackage,net-prism54))
-
-
 define KernelPackage/net-rtl8192su
   SUBMENU:=$(WIRELESS_MENU)
   TITLE:=RTL8192SU support (staging)
