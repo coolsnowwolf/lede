d52eb58fca5bf91c945bc38bfe9468314f695926
Christian Marangi <ansuelsmth@gmail.com>
2022-10-20 03:13:50 +0200

    kernel: modules: adapt for kernel 6.1
    Adapt kernel modules to support kernel 6.1.
    
    Fixes: #11210
    Signed-off-by: Christian Marangi <ansuelsmth@gmail.com>

--- package/kernel/linux/modules/hwmon.mk
+++ package/kernel/linux/modules/hwmon.mk
@@ -52,7 +52,7 @@ define KernelPackage/hwmon-adt7410
 	$(LINUX_DIR)/drivers/hwmon/adt7x10.ko \
 	$(LINUX_DIR)/drivers/hwmon/adt7410.ko
   AUTOLOAD:=$(call AutoLoad,60,adt7x10 adt7410)
-  $(call AddDepends/hwmon,+kmod-i2c-core)
+  $(call AddDepends/hwmon,+kmod-i2c-core +LINUX_6_1:kmod-regmap-core)
 endef
 
 define KernelPackage/hwmon-adt7410/description
