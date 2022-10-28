8e68a5d788194c8f96b3a5280164384292ba758e
coolsnowwolf <coolsnowwolf@gmail.com>
2022-10-28 11:43:33 +0800

    ipq60xx: add CMIOT-AX18/ZN-M2 support

---
+++ target/linux/ipq60xx/base-files/lib/upgrade/platform.sh
@@ -0,0 +1,22 @@
+
+PART_NAME=firmware
+REQUIRE_IMAGE_METADATA=1
+
+RAMFS_COPY_BIN='fw_printenv fw_setenv'
+RAMFS_COPY_DATA='/etc/fw_env.config /var/lock/fw_printenv.lock'
+
+platform_check_image() {
+	return 0;
+}
+
+platform_do_upgrade() {
+	case "$(board_name)" in
+	cmiot,ax18|\
+	zn,m2)
+		nand_do_upgrade "$1"
+		;;
+    *)
+		default_do_upgrade "$1"
+		;;
+	esac
+}
