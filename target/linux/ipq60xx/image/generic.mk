44d01897be7603e01c344a234e137085a0bed9ec
coolsnowwolf <coolsnowwolf@gmail.com>
2022-11-02 15:41:33 +0800

    ipq60xx: add UBI FIT image for Linksys MR7350

--- target/linux/ipq60xx/image/generic.mk
+++ target/linux/ipq60xx/image/generic.mk
@@ -35,14 +35,17 @@ define Device/cmiot_ax18
 endef
 TARGET_DEVICES += cmiot_ax18
 
-define Device/mr7350
+define Device/linksys_mr7350
 	$(call Device/FitImage)
+	$(call Device/UbiFit)
 	DEVICE_VENDOR := Linksys
 	DEVICE_MODEL := MR7350
+	BLOCKSIZE := 128k
+	PAGESIZE := 2048
 	SOC := ipq6018
 	DEVICE_PACKAGES := kmod-leds-pca963x
 endef
-TARGET_DEVICES += mr7350
+TARGET_DEVICES += linksys_mr7350
 
 define Device/cp01-c1
 	$(call Device/FitImage)
