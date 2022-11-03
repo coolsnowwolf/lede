1c7edb5a0bc3c2d511ce0d24b9ed88605d17dd31
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2022-11-03 16:17:35 +0100

    Update generic.mk

--- target/linux/ipq807x/image/generic.mk
+++ target/linux/ipq807x/image/generic.mk
@@ -57,3 +57,25 @@ define Device/tplink_xtr10890
 	DEVICE_PACKAGES := ipq-wifi-tplink_xtr10890 uboot-envtools
 endef
 TARGET_DEVICES += tplink_xtr10890
+
+define Device/redmi_ax6
+	$(call Device/xiaomi_ax3600)
+	DEVICE_VENDOR := Redmi
+	DEVICE_MODEL := AX6
+	DEVICE_PACKAGES := ipq-wifi-redmi_ax6 uboot-envtools
+endef
+TARGET_DEVICES += redmi_ax6
+
+define Device/xiaomi_ax3600
+	$(call Device/FitImage)
+	$(call Device/UbiFit)
+	DEVICE_VENDOR := Xiaomi
+	DEVICE_MODEL := AX3600
+	BLOCKSIZE := 128k
+	PAGESIZE := 2048
+	DEVICE_DTS_CONFIG := config@ac04
+	SOC := ipq8071
+	DEVICE_PACKAGES := ath10k-firmware-qca9887-ct ipq-wifi-xiaomi_ax3600 \
+	kmod-ath10k-ct uboot-envtools
+endef
+TARGET_DEVICES += xiaomi_ax3600
