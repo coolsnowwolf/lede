2a4ccbe41f6307c15ce2328456441cbb45dcd6ec
coolsnowwolf <coolsnowwolf@gmail.com>
2022-11-08 16:50:31 +0800

    ipq60xx: splite Qihoo 360v6 dts file

--- target/linux/ipq60xx/image/generic.mk
+++ target/linux/ipq60xx/image/generic.mk
@@ -47,25 +47,37 @@ define Device/linksys_mr7350
 endef
 TARGET_DEVICES += linksys_mr7350
 
-define Device/cp01-c1
-	$(call Device/FitImage)
-	DEVICE_VENDOR := Qihoo
-	DEVICE_MODEL := 360v6
-	SOC := ipq6018
-endef
-TARGET_DEVICES += cp01-c1
+#define Device/cp01-c1
+#	$(call Device/FitImage)
+#	DEVICE_VENDOR := Qualcomm Technologies
+#	DEVICE_MODEL := AP-CP01-C1
+#	SOC := ipq6018
+#endef
+#TARGET_DEVICES += cp01-c1
 
-define Device/eap610-outdoor
+#define Device/eap610-outdoor
+#	$(call Device/FitImage)
+#	$(call Device/UbiFit)
+#	DEVICE_VENDOR := TP-Link
+#	DEVICE_MODEL := EAP610-Outdoor
+#	BLOCKSIZE := 128k
+#	PAGESIZE := 2048
+#	SOC := ipq6018
+#	IMAGE/nand-factory.ubi := append-ubi | qsdk-ipq-factory-nand
+#endef
+#TARGET_DEVICES += eap610-outdoor
+
+define Device/360_v6
 	$(call Device/FitImage)
 	$(call Device/UbiFit)
-	DEVICE_VENDOR := TP-Link
-	DEVICE_MODEL := EAP610-Outdoor
+	DEVICE_VENDOR := Qihoo 360
+	DEVICE_MODEL := V6
 	BLOCKSIZE := 128k
 	PAGESIZE := 2048
+	DEVICE_DTS_CONFIG := config@cp03-c1
 	SOC := ipq6018
-	IMAGE/nand-factory.ubi := append-ubi | qsdk-ipq-factory-nand
 endef
-TARGET_DEVICES += eap610-outdoor
+TARGET_DEVICES += 360_v6
 
 define Device/zn_m2
 	$(call Device/FitImage)
