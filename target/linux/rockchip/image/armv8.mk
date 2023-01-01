55ce447162b7646bf79ab8ed0fb21b71c86ccbfe
coolsnowwolf <coolsnowwolf@gmail.com>
2023-01-01 16:18:44 +0800

    rockchip: add usb-serial cp210x for HINLINK H66K/H68K

--- target/linux/rockchip/image/armv8.mk
+++ target/linux/rockchip/image/armv8.mk
@@ -17,7 +17,7 @@ define Device/hinlink_common
   SOC := rk3568
   UBOOT_DEVICE_NAME := opc-h68k-rk3568
   IMAGE/sysupgrade.img.gz := boot-common | boot-script nanopi-r5s | pine64-img | gzip | append-metadata
-  DEVICE_PACKAGES := kmod-ata-ahci-platform kmod-mt7921e kmod-r8125 wpad-openssl
+  DEVICE_PACKAGES := kmod-ata-ahci-platform kmod-mt7921e kmod-r8125 kmod-usb-serial-cp210x wpad-openssl
 endef
 
 define Device/hinlink_opc-h66k
