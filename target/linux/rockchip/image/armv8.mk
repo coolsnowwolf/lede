abeef9c0899917efff51c78139a799b378ed3362
coolsnowwolf <31687149+coolsnowwolf@users.noreply.github.com>
2023-02-06 18:01:30 +0800

    Update armv8.mk

--- target/linux/rockchip/image/armv8.mk
+++ target/linux/rockchip/image/armv8.mk
@@ -126,7 +126,7 @@ define Device/friendlyarm_nanopi-r5c
   DEVICE_MODEL := NanoPi R5C
   SOC := rk3568
   UBOOT_DEVICE_NAME := nanopi-r5c-rk3568
-  IMAGE/sysupgrade.img.gz := boot-common | boot-script nanopi-r5c | pine64-img | gzip | append-metadata
+  IMAGE/sysupgrade.img.gz := boot-common | boot-script nanopi-r5s | pine64-img | gzip | append-metadata
   DEVICE_PACKAGES := kmod-r8125
 endef
 TARGET_DEVICES += friendlyarm_nanopi-r5c
