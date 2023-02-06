62133a8392787016cd87377a75776b680c3ff518
Nuc <nj8u7cc@gmail.com>
2023-02-06 18:03:27 +0800

    rockchip: add FriendlyElec NanoPi R5C support
    * add NanoPi R5C board
    
    * Update armv8.mk
    
    * Delete nanopi-r5c.bootscript
    
    ---------
    
    Co-authored-by: coolsnowwolf <31687149+coolsnowwolf@users.noreply.github.com>

--- target/linux/rockchip/image/armv8.mk
+++ target/linux/rockchip/image/armv8.mk
@@ -121,6 +121,16 @@ define Device/friendlyarm_nanopi-r5s
 endef
 TARGET_DEVICES += friendlyarm_nanopi-r5s
 
+define Device/friendlyarm_nanopi-r5c
+  DEVICE_VENDOR := FriendlyARM
+  DEVICE_MODEL := NanoPi R5C
+  SOC := rk3568
+  UBOOT_DEVICE_NAME := nanopi-r5c-rk3568
+  IMAGE/sysupgrade.img.gz := boot-common | boot-script nanopi-r5s | pine64-img | gzip | append-metadata
+  DEVICE_PACKAGES := kmod-r8125
+endef
+TARGET_DEVICES += friendlyarm_nanopi-r5c
+
 define Device/firefly_station-p2
   DEVICE_VENDOR := Firefly
   DEVICE_MODEL := Station P2
