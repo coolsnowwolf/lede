03d09646270842a54a57370e624fd2a4d1f31f80
Tim Harvey <tharvey@gateworks.com>
2023-03-07 13:41:13 -0800

    kernel: modules: package Microchip LAN743x PCIe gigE driver
    Package the Microchip LAN743x PCIe gigE driver
    
    Signed-off-by: Tim Harvey <tharvey@gateworks.com>

--- package/kernel/linux/modules/netdevices.mk
+++ package/kernel/linux/modules/netdevices.mk
@@ -1439,3 +1439,19 @@ endef
 
 $(eval $(call KernelPackage,atlantic))
 
+
+define KernelPackage/lan743x
+  SUBMENU:=$(NETWORK_DEVICES_MENU)
+  TITLE:=Microchip LAN743x PCI Express Gigabit Ethernet NIC
+  DEPENDS:=@PCI_SUPPORT +kmod-ptp +kmod-mdio-devres
+  KCONFIG:=CONFIG_LAN743X
+  FILES:=$(LINUX_DIR)/drivers/net/ethernet/microchip/lan743x.ko
+  AUTOLOAD:=$(call AutoProbe,lan743x)
+endef
+
+define KernelPackage/lan743x/description
+  Kernel module for Microchip LAN743x PCI Express Gigabit Ethernet NIC
+endef
+
+$(eval $(call KernelPackage,lan743x))
+
