9b9efea7af8490024ca7d4145e32fac115a01829
AmadeusGhost <42570690+AmadeusGhost@users.noreply.github.com>
2023-04-03 23:02:25 +0800

    netfilter: fixes flow offload for kernel 5.4
    Fixes: 7a50383 ("add kernel 5.10 support and sync with upstream")

--- package/kernel/linux/modules/netfilter.mk
+++ package/kernel/linux/modules/netfilter.mk
@@ -174,10 +174,16 @@ define KernelPackage/nf-flow
 	CONFIG_NF_FLOW_TABLE \
 	CONFIG_NF_FLOW_TABLE_HW
   DEPENDS:=+kmod-nf-conntrack
+ifeq ($(SUBTARGET),mt7621)
+  FILES:= \
+	$(LINUX_DIR)/net/netfilter/nf_flow_table.ko
+  AUTOLOAD:=$(call AutoProbe,nf_flow_table)
+else
   FILES:= \
 	$(LINUX_DIR)/net/netfilter/nf_flow_table.ko \
-	$(if $(CONFIG_LINUX_5_4),$(LINUX_DIR)/net/netfilter/nf_flow_table_hw.ko)
+	$(LINUX_DIR)/net/netfilter/nf_flow_table_hw.ko
   AUTOLOAD:=$(call AutoProbe,nf_flow_table nf_flow_table_hw)
+endif
 endef
 
 $(eval $(call KernelPackage,nf-flow))
