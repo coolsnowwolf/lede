8d7e741a8bacfb10b46d242b60865d0f08e9a83e
qgzqgz <97427962+qgzqgz@users.noreply.github.com>
2023-04-14 04:08:37 +0800

    Update build.sh

--- helper/build.sh
+++ helper/build.sh
@@ -1,1 +1,17 @@
+#!/bin/sh
+JOBS=`grep -c ^processor /proc/cpuinfo`
+cat ./HATLab-GateBoard-One.feeds > ./feeds.conf.default
+./scripts/feeds update -a
 
+rm -rf ./feeds/packages/net/smartdns/
+git clone https://github.com/pymumu/openwrt-smartdns ./feeds/packages/net/smartdns/
+rm -rf ./feeds/luci/applications/luci-app-smartdns/
+git clone https://github.com/pymumu/luci-app-smartdns ./feeds/luci/applications/luci-app-smartdns/
+
+./scripts/feeds install -a
+rm -f ./.config*
+touch ./.config
+cat ./HATLab-GateBoard-One.config > ./.config
+make defconfig
+make download -j${JOBS}
+make -j${JOBS
