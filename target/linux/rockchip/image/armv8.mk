5c871c6fd5b6ccac109fcdf10b7f0381fe0e7bd4
Lollipop907 <34392880+Lollipop907@users.noreply.github.com>
2023-04-13 20:06:09 +0800

    rockchip:add support for dilusense dlfr100

--- target/linux/rockchip/image/armv8.mk
+++ target/linux/rockchip/image/armv8.mk
@@ -242,6 +242,16 @@ define Device/sharevdi_guangmiao-g4c
 endef
 TARGET_DEVICES += sharevdi_guangmiao-g4c
 
+define Device/dilusense_dlfr100
+  DEVICE_VENDOR := Dilusense
+  DEVICE_MODEL := DLFR100
+  SOC := rk3399
+  UBOOT_DEVICE_NAME := dilusense-dlfr100-rk3399
+  IMAGE/sysupgrade.img.gz := boot-common | boot-script | pine64-bin | gzip | append-metadata
+  DEVICE_PACKAGES := kmod-r8168 -urngd
+endef
+TARGET_DEVICES += dilusense_dlfr100
+
 define Device/xunlong_orangepi-r1-plus
   DEVICE_VENDOR := Xunlong
   DEVICE_MODEL := Orange Pi R1 Plus
