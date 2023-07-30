d321dd01ccd98fc21387312396d5425b573fd432
closewrt <closewrt@gmail.com>
2023-07-30 14:50:01 +0800

    rockchip: add support for NLnet XiGuaPi Board

--- target/linux/rockchip/image/armv8.mk
+++ target/linux/rockchip/image/armv8.mk
@@ -72,6 +72,19 @@ $(call Device/fastrhino_common)
 endef
 TARGET_DEVICES += fastrhino_r68s
 
+define Device/nlnet_common
+  DEVICE_VENDOR := NLnet
+  SOC := rk3568
+  UBOOT_DEVICE_NAME := xgp-rk3568
+  IMAGE/sysupgrade.img.gz := boot-common | boot-script nanopi-r5s | pine64-img | gzip | append-metadata
+endef
+
+define Device/nlnet_xgp
+$(call Device/nlnet_common)
+  DEVICE_MODEL := XiGuaPi
+endef
+TARGET_DEVICES += nlnet_xgp
+
 define Device/friendlyarm_nanopi-neo3
   DEVICE_VENDOR := FriendlyARM
   DEVICE_MODEL := NanoPi NEO3
