2fdceb465c3d45fa506e5989540ede33433430e0
Patricia Lee <patricialee320@gmail.com>
2023-08-30 19:50:54 +0800

    mediatek: add support for Cetron CT3003
    **Hardware specification:**
    
    - SoC: MediaTek MT7981B 2x A53
    - Flash: ESMT F50L1G41LB 128MB
    - RAM: Nanya NT5CC128M16JR-EK 256MB
    - Ethernet: 4 x 10/100/1000 Mbps
    - Switch: MediaTek MT7531AE
    - WiFi: MediaTek MT7976C
    - Button: Reset, Mesh
    - Power: DC 12V 1A
    - UART: 3.3v, 115200n8
    
    **Flash instructions:**
    
    1. Rename `openwrt-mediatek-filogic-cetron_ct3003-squashfs-factory.bin` to `factory.bin`.
    2. Upload the `factory.bin` using the device's Web interface.
    3. Click the upgrade button and wait for the process to finish.
    4. Access the OpenWrt interface using the same password.
    5. Use the 'Restore' function to reset the firmware to its initial state.
    
    **Notes:**
    
    If you plan to recovery the stock firmware in the future, it's advisable
    to connect the device via the serial port and enter failsafe mode to
    back up all the MTD partitions before proceeding the steps above.
    
    Signed-off-by: Patricia Lee <patricialee320@gmail.com>

--- target/linux/mediatek/image/filogic.mk
+++ target/linux/mediatek/image/filogic.mk
@@ -67,6 +67,21 @@ define Build/append-gl-metadata
 	}
 endef
 
+define Build/cetron-header
+	$(eval magic=$(word 1,$(1)))
+	$(eval model=$(word 2,$(1)))
+	( \
+		dd if=/dev/zero bs=856 count=1 2>/dev/null; \
+		printf "$(model)," | dd bs=128 count=1 conv=sync 2>/dev/null; \
+		md5sum $@ | cut -f1 -d" " | dd bs=32 count=1 2>/dev/null; \
+		printf "$(magic)" | dd bs=4 count=1 conv=sync 2>/dev/null; \
+		cat $@; \
+	) > $@.tmp
+	fw_crc=$$(gzip -c $@.tmp | tail -c 8 | od -An -N4 -tx4 --endian little | tr -d ' \n'); \
+	printf "$$(echo $$fw_crc | sed 's/../\\x&/g')" | cat - $@.tmp > $@
+	rm $@.tmp
+endef
+
 define Device/asus_tuf-ax4200
   DEVICE_VENDOR := ASUS
   DEVICE_MODEL := TUF-AX4200
@@ -133,23 +148,19 @@ TARGET_DEVICES += bananapi_bpi-r3
 
 
 define Device/cetron_ct3003
-  DEVICE_VENDOR := CETRON
+  DEVICE_VENDOR := Cetron
   DEVICE_MODEL := CT3003
   DEVICE_DTS := mt7981b-cetron-ct3003
   DEVICE_DTS_DIR := ../dts
+  SUPPORTED_DEVICES += mediatek,mt7981-spim-snand-rfb
   DEVICE_PACKAGES := kmod-mt7981-firmware mt7981-wo-firmware
   UBINIZE_OPTS := -E 5
   BLOCKSIZE := 128k
   PAGESIZE := 2048
-  IMAGE_SIZE := 113152k
   KERNEL_IN_UBI := 1
-  IMAGES += factory.bin
-  IMAGE/factory.bin := append-ubi | check-size $$$$(IMAGE_SIZE)
   IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
-  KERNEL = kernel-bin | lzma | \
-	fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb
-  KERNEL_INITRAMFS = kernel-bin | lzma | \
-	fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb with-initrd
+  IMAGES += factory.bin
+  IMAGE/factory.bin := $$(IMAGE/sysupgrade.bin) | cetron-header rd30 CT3003
 endef
 TARGET_DEVICES += cetron_ct3003
 
