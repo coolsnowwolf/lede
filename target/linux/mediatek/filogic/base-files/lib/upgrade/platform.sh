eb3e9ad2e339c0eb841ca435b2746fddbdb4744f
coolsnowwolf <coolsnowwolf@gmail.com>
2024-02-03 21:31:05 +0800

    mediatek: filogic: add support for CMCC RAX3000M (NAND and eMMC version)

--- target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
+++ target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
@@ -29,6 +29,7 @@ platform_do_upgrade() {
 			;;
 		esac
 		;;
+	cmcc,rax3000m-emmc|\
 	glinet,gl-mt2500|\
 	glinet,gl-mt6000|\
 	jdcloud,re-cs-05)
@@ -76,6 +77,7 @@ platform_copy_config() {
 			;;
 		esac
 		;;
+	cmcc,rax3000m-emmc|\
 	glinet,gl-mt2500|\
 	glinet,gl-mt6000|\
 	jdcloud,re-cs-05)
