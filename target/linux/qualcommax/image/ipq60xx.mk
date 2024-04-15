a95c23c180dc48a7049bdf2a79b6bb5448b85034
coolsnowwolf <coolsnowwolf@gmail.com>
2024-04-15 23:00:37 +0800

    qualcommax: ipq60xx: add JDCloud AX1800 Pro and AX6600 support

--- target/linux/qualcommax/image/ipq60xx.mk
+++ target/linux/qualcommax/image/ipq60xx.mk
@@ -18,9 +18,9 @@ define Device/UbiFit
 endef
 
 define Device/EmmcImage
-	IMAGES += factory.bin sysupgrade.bin
-	IMAGE/factory.bin := append-rootfs | pad-rootfs | pad-to 64k
-	IMAGE/sysupgrade.bin/squashfs := append-rootfs | pad-to 64k | sysupgrade-tar rootfs=$$$$@ | append-metadata
+	IMAGES := factory.bin recovery.bin sysupgrade.bin
+	IMAGE/factory.bin := append-kernel | pad-to 12288k | append-rootfs | append-metadata
+	IMAGE/recovery.bin := append-kernel | pad-to 6144k | append-rootfs | append-metadata
 endef
 
 define Device/cmiot_ax18
@@ -60,6 +60,32 @@ define Device/glinet_gl-axt1800
 	DEVICE_PACKAGES := ipq-wifi-glinet_gl-axt1800 kmod-hwmon-gpiofan
 endef
 TARGET_DEVICES += glinet_gl-axt1800
+
+define Device/jdcloud_re-cs-02
+	$(call Device/FitImage)
+	$(call Device/EmmcImage)
+	DEVICE_VENDOR := JDCloud
+	DEVICE_MODEL := AX6600
+	SOC := ipq6010
+	BLOCKSIZE := 64k
+	KERNEL_SIZE := 6144k
+	DEVICE_DTS_CONFIG := config@cp03-c3
+	DEVICE_PACKAGES := ipq-wifi-jdcloud_ax6600 kmod-ath11k-pci ath11k-firmware-qcn9074
+endef
+TARGET_DEVICES += jdcloud_re-cs-02
+
+define Device/jdcloud_re-ss-01
+	$(call Device/FitImage)
+	$(call Device/EmmcImage)
+	DEVICE_VENDOR := JDCloud
+	DEVICE_MODEL := AX1800 Pro
+	SOC := ipq6000
+	BLOCKSIZE := 64k
+	KERNEL_SIZE := 6144k
+	DEVICE_DTS_CONFIG := config@cp03-c2
+	DEVICE_PACKAGES := ipq-wifi-jdcloud_ax1800pro
+endef
+TARGET_DEVICES += jdcloud_re-ss-01
 
 define Device/linksys_mr7350
 	$(call Device/FitImage)
