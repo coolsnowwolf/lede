1ede124cc852ab5668f1bc698b6a0838beeb37b9
zhusir <smallprogram@foxmail.com>
2024-06-28 23:39:32 +0800

    Update qmi_wwan_q.c

--- package/wwan/driver/quectel_QMI_WWAN/src/qmi_wwan_q.c
+++ package/wwan/driver/quectel_QMI_WWAN/src/qmi_wwan_q.c
@@ -828,26 +828,26 @@ static struct rtnl_link_stats64 *_rmnet_vnd_get_stats64(struct net_device *net, 
 		stats64 = per_cpu_ptr(dev->stats64, cpu);
 
 		do {
-#if (LINUX_VERSION_CODE < KERNEL_VERSION( 6,6,0 ))
 			start = u64_stats_fetch_begin_irq(&stats64->syncp);
-#else
-			start = u64_stats_fetch_begin(&stats64->syncp);
-#endif
+#if LINUX_VERSION_CODE < KERNEL_VERSION(5,18,0)
 			rx_packets = stats64->rx_packets;
 			rx_bytes = stats64->rx_bytes;
 			tx_packets = stats64->tx_packets;
 			tx_bytes = stats64->tx_bytes;
-#if (LINUX_VERSION_CODE < KERNEL_VERSION( 6,6,0 ))
-		} while (u64_stats_fetch_retry_irq(&stats64->syncp, start));
 #else
-		} while (u64_stats_fetch_retry(&stats64->syncp, start));
+			rx_packets = u64_stats_read(&stats64->rx_packets);
+			rx_bytes = u64_stats_read(&stats64->rx_bytes);
+			tx_packets = u64_stats_read(&stats64->tx_packets);
+			tx_bytes = u64_stats_read(&stats64->tx_bytes);
 #endif
+		} while (u64_stats_fetch_retry_irq(&stats64->syncp, start));
 
-        stats->rx_packets += u64_stats_read(&rx_packets);
-		stats->rx_bytes += u64_stats_read(&rx_bytes);
-		stats->tx_packets += u64_stats_read(&tx_packets);
-		stats->tx_bytes += u64_stats_read(&tx_bytes);
-#endif
+
+		stats->rx_packets += rx_packets;
+		stats->rx_bytes += rx_bytes;
+		stats->tx_packets += tx_packets;
+		stats->tx_bytes += tx_bytes;
+
 	}
 
 	return stats;
