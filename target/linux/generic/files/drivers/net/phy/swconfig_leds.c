f441b4c9abba49f0c14105fe65d94c9cf6acd74c
coolsnowwolf <coolsnowwolf@gmail.com>
2024-07-01 00:40:22 +0800

    package: add fitblk util to release /dev/fit* devices

--- target/linux/generic/files/drivers/net/phy/swconfig_leds.c
+++ target/linux/generic/files/drivers/net/phy/swconfig_leds.c
@@ -85,7 +85,7 @@ swconfig_trig_update_port_mask(struct led_trigger *trigger)
 	sw_trig = (void *) trigger;
 
 	port_mask = 0;
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 18, 0)
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5,16,0)
 	spin_lock(&trigger->leddev_list_lock);
 #else
 	read_lock(&trigger->leddev_list_lock);
@@ -102,11 +102,12 @@ swconfig_trig_update_port_mask(struct led_trigger *trigger)
 			read_unlock(&trig_data->lock);
 		}
 	}
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 18, 0)
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5,16,0)
 	spin_unlock(&trigger->leddev_list_lock);
 #else
 	read_unlock(&trigger->leddev_list_lock);
 #endif
+
 	sw_trig->port_mask = port_mask;
 
 	if (port_mask)
@@ -425,7 +426,7 @@ swconfig_trig_update_leds(struct switch_led_trigger *sw_trig)
 	struct led_trigger *trigger;
 
 	trigger = &sw_trig->trig;
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 18, 0)
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5,16,0)
 	spin_lock(&trigger->leddev_list_lock);
 #else
 	read_lock(&trigger->leddev_list_lock);
@@ -436,7 +437,7 @@ swconfig_trig_update_leds(struct switch_led_trigger *sw_trig)
 		led_cdev = list_entry(entry, struct led_classdev, trig_list);
 		swconfig_trig_led_event(sw_trig, led_cdev);
 	}
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 18, 0)
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5,16,0)
 	spin_unlock(&trigger->leddev_list_lock);
 #else
 	read_unlock(&trigger->leddev_list_lock);
