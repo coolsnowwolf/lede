08aaf074b9bbda431542d7d4dbbc544230e78c07
Daniel Golle <daniel@makrotopia.org>
2024-07-01 13:36:10 +0800

    mediatek: add support for BananaPi BPI-R4 board
    Hardware
    --------
    SOC:    MediaTek MT7988A (4x Cortex-A73)
    RAM:    4 GiB DDR4
    Flash:  128 MiB Winbond SPI-NAND
    MMC:    8 GiB eMMC *or* microSD (cannot be used both)
    ETH:    4x 1GE (1x WAN, 3x LAN)
            2x SFP+ (10G, 5G, 2.5G, 1G)
    USB:    on-board USB 3.2 4-port hub
            1x USB 3.2 port (type A connector)
            1x M.2 for 4G/5G modem
            2x mPCIe for additional modems
    WiFi:   optional MediaTek MT7996 Wi-Fi 7 module
            (using 2x PCIe gen3 x2 on the mPCIe slots and 12V power)
    
    Installation
    ------------
    1. Decompress and write the sdcard image to a micro SD card and use that
    to boot the R4 (both dip switches in upper position).
    
    2. Use the bootloader menu accessible via the serial console to install
    to SPI-NAND.
    
    3. Switch to boot from SPI-NAND and install to eMMC.
    
    Signed-off-by: Daniel Golle <daniel@makrotopia.org>

--- target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
+++ target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
@@ -20,7 +20,9 @@ platform_do_upgrade() {
 		CI_KERNPART="linux"
 		nand_do_upgrade "$1"
 		;;
-	bananapi,bpi-r3)
+	bananapi,bpi-r3|\
+	bananapi,bpi-r4|\
+	bananapi,bpi-r4-poe)
 		local rootdev="$(cmdline_get_var root)"
 		rootdev="${rootdev##*/}"
 		rootdev="${rootdev%p[0-9]*}"
@@ -63,7 +65,9 @@ platform_check_image() {
 	[ "$#" -gt 1 ] && return 1
 
 	case "$board" in
-	bananapi,bpi-r3)
+	bananapi,bpi-r3|\
+	bananapi,bpi-r4|\
+	bananapi,bpi-r4-poe)
 		[ "$magic" != "d00dfeed" ] && {
 			echo "Invalid image type."
 			return 1
@@ -72,7 +76,7 @@ platform_check_image() {
 		;;
 	*)
 		nand_do_platform_check "$board" "$1"
-		return 0
+		return $?
 		;;
 	esac
 
@@ -81,7 +85,9 @@ platform_check_image() {
 
 platform_copy_config() {
 	case "$(board_name)" in
-	bananapi,bpi-r3)
+	bananapi,bpi-r3|\
+	bananapi,bpi-r4|\
+	bananapi,bpi-r4-poe)
 		case "$(cmdline_get_var root)" in
 		/dev/mmc*)
 			emmc_copy_config
@@ -95,8 +101,8 @@ platform_copy_config() {
 		emmc_copy_config
 		;;
 	esac
- }
- 
+}
+
 platform_pre_upgrade() {
 	local board=$(board_name)
 
