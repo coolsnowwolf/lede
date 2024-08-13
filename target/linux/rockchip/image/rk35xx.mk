f2cc89ebaa60b53a42ad1a2afaf40bacc727bad5
coolsnowwolf <coolsnowwolf@gmail.com>
2024-08-13 18:30:29 +0800

    rockchip: add ArmSom RK3588 Sige7 Board support

--- target/linux/rockchip/image/rk35xx.mk
+++ target/linux/rockchip/image/rk35xx.mk
@@ -7,6 +7,14 @@ define Device/rk3528
   IMAGE/sysupgrade.img.gz := boot-common | boot-script rk3528 | pine64-img | gzip | append-metadata
 endef
 
+define Device/rk3588
+  SOC := rk3588
+  DEVICE_DTS_DIR := ../dts/rk3588
+  DEVICE_DTS = $$(SOC)-$$(lastword $$(subst _, ,$$(DEVICE_NAME)))
+  UBOOT_DEVICE_NAME := rk3588-evb
+  IMAGE/sysupgrade.img.gz := boot-common | boot-script rk3588 | pine64-img | gzip | append-metadata
+endef
+
 define Device/armsom_sige1
 $(call Device/rk3528)
   DEVICE_VENDOR := ArmSoM
@@ -15,6 +23,17 @@ $(call Device/rk3528)
   DEVICE_PACKAGES := kmod-r8125 kmod-thermal kmod-brcmfmac brcmfmac-firmware-43752-sdio brcmfmac-nvram-43752-sdio
 endef
 TARGET_DEVICES += armsom_sige1
+
+define Device/armsom_sige7
+$(call Device/rk3588)
+  DEVICE_VENDOR := ArmSoM
+  DEVICE_MODEL := Sige7
+  DEVICE_DTS := rk3588-sige7-v1-1
+  SUPPORTED_DEVICES += armsom,sige7 armsom,sige7-v1 armsom,sige7-v1.0
+  DEVICE_PACKAGES := kmod-r8125 kmod-nvme kmod-hwmon-pwmfan kmod-thermal \
+	kmod-brcmfmac brcmfmac-firmware-43752-pcie brcmfmac-nvram-43752-pcie
+endef
+TARGET_DEVICES += armsom_sige7
 
 define Device/mangopi_m28k
 $(call Device/rk3528)
