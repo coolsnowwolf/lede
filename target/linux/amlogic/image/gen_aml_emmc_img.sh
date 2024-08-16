318254acff7b37264b7c4ada963d8aa7aa380b8b
shiyu1314 <shiyu1052@gmail.com>
2024-08-16 17:53:43 +0800

    produces Amlogic Burn Image Dependent on img2simg

--- target/linux/amlogic/image/gen_aml_emmc_img.sh
+++ target/linux/amlogic/image/gen_aml_emmc_img.sh
@@ -1,29 +1,35 @@
-#!/bin/sh
+#!/bin/bash
 
-set -x
-[ $# -eq 5 ] || {
-    echo "SYNTAX: $0 <file> <bootfs image> <rootfs image> <bootfs size> <rootfs size>"
+# 检查参数数量
+if [ "$#" -ne 5 ]; then
+    echo "Usage: $0 <output_image> <boot_partition_image> <rootfs_image>"
     exit 1
-}
+fi
 
-OUTPUT="$1"
-BOOTFS="$2"
-ROOTFS="$3"
-BOOTFSSIZE="$4"
-ROOTFSSIZE="$5"
-
-head=4
-sect=63
-
-set $(ptgen -o $OUTPUT -h $head -s $sect -l 4096 -t c -p ${BOOTFSSIZE}M -t 83 -p ${ROOTFSSIZE}M)
-
-BOOTOFFSET="$(($1 / 512))"
-BOOTSIZE="$(($2 / 512))"
-ROOTFSOFFSET="$(($3 / 512))"
-ROOTFSSIZE="$(($4 / 512))"
-
-dd bs=512 if="$BOOTFS" of="$OUTPUT" seek="$BOOTOFFSET" conv=notrunc
-dd bs=512 if="$ROOTFS" of="$OUTPUT" seek="$ROOTFSOFFSET" conv=notrunc
+OUTPUT_IMAGE=$1
+BOOT_PARTITION_IMAGE=$2
+ROOTFS_IMAGE=$3
 
 
+# 下载并准备工具
+ver="v0.3.1"
+curl -L -o ./AmlImg https://github.com/hzyitc/AmlImg/releases/download/$ver/AmlImg_${ver}_linux_amd64
+chmod +x ./AmlImg
+curl -L -o ./uboot.img https://github.com/shiyu1314/u-boot-onecloud/releases/download/build-20230901-0443/eMMC.burn.img
+./AmlImg unpack ./uboot.img burn/
 
+# 转换镜像格式
+img2simg ${BOOT_PARTITION_IMAGE} burn/boot.simg
+img2simg ${ROOTFS_IMAGE} burn/rootfs.simg
+
+# 创建命令文件
+cat <<EOF >>burn/commands.txt
+PARTITION:boot:sparse:boot.simg
+PARTITION:rootfs:sparse:rootfs.simg
+EOF
+
+# 打包生成最终镜像
+./AmlImg pack ${OUTPUT_IMAGE} burn/
+
+# 清理临时文件夹
+rm -rf burn
--- target/linux/amlogic/image/meson8b.mk
+++ target/linux/amlogic/image/meson8b.mk
@@ -1,7 +1,7 @@
 
 define Device/Default
   FILESYSTEMS := ext4
-  IMAGES := emmc.img
+  IMAGES := emmc_burn.img
   KERNEL_DEPENDS = $$(wildcard $(DTS_DIR)/$$(DEVICE_DTS).dts)
   KERNEL_LOADADDR := 0x01080000
   KERNEL_NAME := Image
@@ -13,6 +13,6 @@ define Device/thunder-onecloud
   DEVICE_DTS := meson8b-onecloud
   DEVICE_TITLE := Thunder OneCloud
   KERNEL_LOADADDR := 0x00208000
-  IMAGE/emmc.img := boot-script onecloud | emmc-common $$(DEVICE_NAME)
+  IMAGE/emmc_burn.img := boot-script onecloud | emmc-common $$(DEVICE_NAME)
 endef
 TARGET_DEVICES += thunder-onecloud
