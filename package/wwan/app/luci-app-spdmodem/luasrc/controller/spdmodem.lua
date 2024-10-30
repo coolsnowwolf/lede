35cc3202be1b5419f0f12c73e8b5d070932e5a74
coolsnowwolf <coolsnowwolf@gmail.com>
2024-10-30 11:19:26 +0800

    luci-app-spdmodem: remove spdmodem package

--- package/wwan/app/luci-app-spdmodem/luasrc/controller/spdmodem.lua
+++
@@ -1,9 +0,0 @@
-module("luci.controller.spdmodem", package.seeall)
-
-function index()
-	if not nixio.fs.access("/etc/config/spdmodem") then
-		return
-	end
-
-	entry({"admin", "network", "spdmodem"}, cbi("spdmodem"), _("SPD Modem Server"), 80).dependent = false
-end
