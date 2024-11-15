f43ae1934396a9ff6bb5199021bf5966ab78f892
coolsnowwolf <coolsnowwolf@gmail.com>
2024-11-15 15:57:32 +0800

    qualcommax: ipq60xx: rename JDCloud Series model to friendly name

--- target/linux/qualcommax/image/ipq60xx.mk
+++ target/linux/qualcommax/image/ipq60xx.mk
@@ -62,33 +62,7 @@ define Device/glinet_gl-axt1800
 endef
 TARGET_DEVICES += glinet_gl-axt1800
 
-define Device/jdcloud_re-cs-02
-	$(call Device/FitImage)
-	$(call Device/EmmcImage)
-	DEVICE_VENDOR := JDCloud
-	DEVICE_MODEL := AX6600
-	SOC := ipq6010
-	BLOCKSIZE := 64k
-	KERNEL_SIZE := 6144k
-	DEVICE_DTS_CONFIG := config@cp03-c3
-	DEVICE_PACKAGES := ipq-wifi-jdcloud_ax6600 kmod-ath11k-pci ath11k-firmware-qcn9074
-endef
-TARGET_DEVICES += jdcloud_re-cs-02
-
-define Device/jdcloud_re-cs-07
-	$(call Device/FitImage)
-	$(call Device/EmmcImage)
-	DEVICE_VENDOR := JDCloud
-	DEVICE_MODEL := RE-CS-07
-	SOC := ipq6010
-	BLOCKSIZE := 64k
-	KERNEL_SIZE := 6144k
-	DEVICE_DTS_CONFIG := config@cp03-c4
-	DEVICE_PACKAGES := -kmod-ath11k-ahb -ath11k-firmware-ipq6018
-endef
-TARGET_DEVICES += jdcloud_re-cs-07
-
-define Device/jdcloud_re-ss-01
+define Device/jdcloud_ax1800-pro
 	$(call Device/FitImage)
 	$(call Device/EmmcImage)
 	DEVICE_VENDOR := JDCloud
@@ -97,9 +71,41 @@ define Device/jdcloud_re-ss-01
 	BLOCKSIZE := 64k
 	KERNEL_SIZE := 6144k
 	DEVICE_DTS_CONFIG := config@cp03-c2
+	DEVICE_DTS := ipq6000-re-ss-01
+	SUPPORTED_DEVICES += jdcloud,re-ss-01
 	DEVICE_PACKAGES := ipq-wifi-jdcloud_ax1800pro
 endef
-TARGET_DEVICES += jdcloud_re-ss-01
+TARGET_DEVICES += jdcloud_ax1800-pro
+
+define Device/jdcloud_ax6600
+	$(call Device/FitImage)
+	$(call Device/EmmcImage)
+	DEVICE_VENDOR := JDCloud
+	DEVICE_MODEL := AX6600
+	SOC := ipq6010
+	BLOCKSIZE := 64k
+	KERNEL_SIZE := 6144k
+	DEVICE_DTS_CONFIG := config@cp03-c3
+	DEVICE_DTS := ipq6010-re-cs-02
+	SUPPORTED_DEVICES += jdcloud,re-cs-02
+	DEVICE_PACKAGES := ipq-wifi-jdcloud_ax6600 kmod-ath11k-pci ath11k-firmware-qcn9074
+endef
+TARGET_DEVICES += jdcloud_ax6600
+
+define Device/jdcloud_er1
+	$(call Device/FitImage)
+	$(call Device/EmmcImage)
+	DEVICE_VENDOR := JDCloud
+	DEVICE_MODEL := ER1
+	SOC := ipq6010
+	BLOCKSIZE := 64k
+	KERNEL_SIZE := 6144k
+	DEVICE_DTS_CONFIG := config@cp03-c4
+	DEVICE_DTS := ipq6010-re-cs-07
+	SUPPORTED_DEVICES += jdcloud,re-cs-07
+	DEVICE_PACKAGES := -kmod-ath11k-ahb -ath11k-firmware-ipq6018
+endef
+TARGET_DEVICES += jdcloud_er1
 
 define Device/linksys_mr7350
 	$(call Device/FitImage)
