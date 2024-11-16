0f3741b7bc9f989ca314addc8c1942dd7cea7d20
coolsnowwolf <coolsnowwolf@gmail.com>
2024-11-16 15:06:01 +0800

    target.mk: reduce default image size

--- include/target.mk
+++ include/target.mk
@@ -56,9 +56,9 @@ DEFAULT_PACKAGES.router:=\
 	dnsmasq-full firewall iptables ppp ppp-mod-pppoe odhcp6c odhcpd-ipv6only \
 	block-mount coremark kmod-nf-nathelper kmod-nf-nathelper-extra kmod-ipt-raw kmod-tun \
 	iptables-mod-tproxy iptables-mod-extra ipset ip-full default-settings luci luci-proto-ipv6 \
-	ddns-scripts_aliyun ddns-scripts_dnspod luci-app-ddns luci-app-upnp luci-app-autoreboot \
+	luci-app-ddns luci-app-upnp luci-app-autoreboot \
 	luci-app-arpbind luci-app-filetransfer luci-app-vsftpd luci-app-ssr-plus luci-app-vlmcsd \
-	luci-app-accesscontrol luci-app-nlbwmon luci-app-turboacc luci-app-wol curl ca-certificates
+	luci-app-accesscontrol luci-app-nlbwmon luci-app-turboacc luci-app-wol
 
 ifneq ($(DUMP),)
   all: dumpinfo
