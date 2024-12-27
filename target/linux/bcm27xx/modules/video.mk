975ce336109d9e400c9290a1aaaad72769edb090
=?UTF-8?q?=C3=81lvaro=20Fern=C3=A1ndez=20Rojas?= <noltari@gmail.com>
2024-12-27 17:14:40 +0100

    bcm27xx: pull 6.6 patches from RPi
    Adds latest 6.6 patches from the Raspberry Pi repository.
    
    These patches were generated from:
    https://github.com/raspberrypi/linux/commits/rpi-6.6.y/
    
    Signed-off-by: Álvaro Fernández Rojas <noltari@gmail.com>

--- target/linux/bcm27xx/modules/video.mk
+++ target/linux/bcm27xx/modules/video.mk
@@ -20,6 +20,25 @@ endef
 $(eval $(call KernelPackage,camera-bcm2835))
 
 
+define KernelPackage/rp1-cfe
+  TITLE:=RP1 Camera Front-End
+  SUBMENU:=$(VIDEO_MENU)
+  KCONFIG:= \
+     CONFIG_VIDEO_RP1_CFE \
+     CONFIG_VIDEO_BCM2835
+  FILES:=$(LINUX_DIR)/drivers/media/platform/raspberrypi/rp1_cfe/rp1-cfe.ko
+  AUTOLOAD:=$(call AutoLoad,67,rp1-cfe)
+  DEPENDS:=@TARGET_bcm27xx_bcm2712 +kmod-video-core +kmod-video-fwnode +kmod-video-dma-contig +kmod-video-async
+endef
+
+define KernelPackage/rp1-cfe/description
+  Driver for the Camera Serial Interface (CSI) to capture video
+  streams from connected cameras.
+endef
+
+$(eval $(call KernelPackage,rp1-cfe))
+
+
 define KernelPackage/codec-bcm2835
   TITLE:=BCM2835 Video Codec
   KCONFIG:= \
@@ -115,3 +134,81 @@ define KernelPackage/vchiq-mmal-bcm2835/description
 endef
 
 $(eval $(call KernelPackage,vchiq-mmal-bcm2835))
+
+
+define KernelPackage/drm-rp1-dsi
+  SUBMENU:=$(VIDEO_MENU)
+  TITLE:=RP1 Display Serial Interface for Video
+  KCONFIG:= \
+    CONFIG_DRM_RP1_DSI \
+    CONFIG_DRM_GEM_DMA_HELPER \
+    CONFIG_DRM_KMS_HELPER \
+    CONFIG_DRM_MIPI_DSI=y \
+    CONFIG_DRM_VRAM_HELPER=n \
+    CONFIG_DRM_TTM=n \
+    CONFIG_DRM_TTM_HELPER=n \
+    CONFIG_GENERIC_PHY_MIPI_DPHY=n \
+    CONFIG_DRM_WERROR=n
+  FILES:=$(LINUX_DIR)/drivers/gpu/drm/rp1/rp1-dsi/drm-rp1-dsi.ko
+  AUTOLOAD:=$(call AutoLoad,67,drm-rp1-dsi)
+  DEPENDS:=@TARGET_bcm27xx_bcm2712 +kmod-drm-vc4
+endef
+
+define KernelPackage/drm-rp1-dsi/description
+  This module manages the DSI for driving high-resolution LCD panels
+  such as the official Raspberry Pi displays or other screens that
+  use the DSI interface.
+endef
+
+$(eval $(call KernelPackage,drm-rp1-dsi))
+
+
+define KernelPackage/drm-rp1-dpi
+  SUBMENU:=$(VIDEO_MENU)
+  TITLE:=RP1 Display Parallel Interface for Video
+  KCONFIG:= \
+    CONFIG_DRM_RP1_DPI \
+    CONFIG_DRM_GEM_DMA_HELPER \
+    CONFIG_DRM_KMS_HELPER \
+    CONFIG_DRM_VRAM_HELPER=n \
+    CONFIG_DRM_TTM=n \
+    CONFIG_DRM_TTM_HELPER=n
+  FILES:=$(LINUX_DIR)/drivers/gpu/drm/rp1/rp1-dpi/drm-rp1-dpi.ko
+  AUTOLOAD:=$(call AutoLoad,67,drm-rp1-dpi)
+  DEPENDS:=@TARGET_bcm27xx_bcm2712 +kmod-drm-vc4 +kmod-rp1-pio
+endef
+
+define KernelPackage/drm-rp1-dpi/description
+  This module is or driving displays using the DPI standard.
+  Useful for interfacing with custom or low-level LCD panels
+  that require parallel RGB signals.  Provides direct control
+  over the timing and signal driving of raw LCD panels.
+  Typically used in maker projects or with non-HDMI displays.
+endef
+
+$(eval $(call KernelPackage,drm-rp1-dpi))
+
+
+define KernelPackage/drm-rp1-vec
+  SUBMENU:=$(VIDEO_MENU)
+  TITLE:=RP1 Display Composite Video
+  KCONFIG:= \
+    CONFIG_DRM_RP1_VEC \
+    CONFIG_DRM_GEM_DMA_HELPER \
+    CONFIG_DRM_KMS_HELPER \
+    CONFIG_DRM_VRAM_HELPER=n \
+    CONFIG_DRM_TTM=n \
+    CONFIG_DRM_TTM_HELPER=n
+  FILES:=$(LINUX_DIR)/drivers/gpu/drm/rp1/rp1-vec/drm-rp1-vec.ko
+  AUTOLOAD:=$(call AutoLoad,67,drm-rp1-vec)
+  DEPENDS:=@TARGET_bcm27xx_bcm2712 +kmod-drm-vc4
+endef
+
+define KernelPackage/drm-rp1-vec/description
+  This module is used for composite video output, which is typically
+  transmitted through the RCA jack.  Primary use is onnecting older
+  TVs or monitors that rely on analog signals via a composite interface.
+  Handles standard-definition analog signals in NTSC and PAL.
+endef
+
+$(eval $(call KernelPackage,drm-rp1-vec))
