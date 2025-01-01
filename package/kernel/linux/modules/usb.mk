96f3d3781ba8ed44ace960a7e7c38c8afdbc9ff9
coolsnowwolf <coolsnowwolf@gmail.com>
2025-01-01 20:05:36 +0800

    r8152: fix missing dependency for 6.12

--- package/kernel/linux/modules/usb.mk
+++ package/kernel/linux/modules/usb.mk
@@ -1439,11 +1439,11 @@ $(eval $(call KernelPackage,usb-net-rtl8150))
 
 define KernelPackage/usb-net-rtl8152
   TITLE:=Kernel module for USB-to-Ethernet Realtek convertors
-  DEPENDS:=+r8152-firmware +kmod-crypto-sha256 +kmod-mii
+  DEPENDS:=+r8152-firmware +kmod-crypto-sha256 +kmod-mii +LINUX_6_12:kmod-libphy
   KCONFIG:=CONFIG_USB_RTL8152
   FILES:=$(LINUX_DIR)/drivers/$(USBNET_DIR)/r8152.ko
   AUTOLOAD:=$(call AutoProbe,r8152)
-  $(call AddDepends/usb, +LINUX_5_10:kmod-crypto-hash)
+  $(call AddDepends/usb-net)
 endef
 
 define KernelPackage/usb-net-rtl8152/description
