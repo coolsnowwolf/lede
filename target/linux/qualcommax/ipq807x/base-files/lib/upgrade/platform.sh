b9e8fb3049832be23ceb78598c357f1192d318d4
aiamadeus <42570690+aiamadeus@users.noreply.github.com>
2025-01-22 23:18:15 +0800

    qualcommax: fixes Aliyun AP8220 support

--- target/linux/qualcommax/ipq807x/base-files/lib/upgrade/platform.sh
+++ target/linux/qualcommax/ipq807x/base-files/lib/upgrade/platform.sh
@@ -10,10 +10,7 @@ platform_check_image() {
 
 platform_do_upgrade() {
 	case "$(board_name)" in
-	aliyun,ap8220)
-		CI_UBIPART="rootfs"
-		nand_do_upgrade "$1"
-		;;
+	aliyun,ap8220|\
 	arcadyan,aw1000|\
 	cmcc,rm2-6|\
 	compex,wpq873|\
