7b1175e88276a58e7164174724489d128dac8b26
Joe Zheng <joe.zheng@intel.com>
2025-02-08 01:18:03 +0000

    kernel: add package for Intel VPU/NPU driver
    Intel NPU device is an AI inference accelerator integrated with Intel
    client CPUs, starting from Intel Core Ultra generation of CPUs
    (formerly known as Meteor Lake). It enables energy-efficient execution
    of artificial neural network tasks.
    
    The full device name is Neural Processing Unit, but the Linux kernel
    driver uses the older name - Versatile Processing Unit (VPU).
    
    This package is for NPU/VPU firmware.
    
    Details in https://github.com/intel/linux-npu-driver
    
    Signed-off-by: Joe Zheng <joe.zheng@intel.com>

--- package/kernel/linux/modules/video.mk
+++ package/kernel/linux/modules/video.mk
@@ -494,6 +494,23 @@ endef
 
 $(eval $(call KernelPackage,drm-i915))
 
+define KernelPackage/drm-ivpu
+  SUBMENU:=$(VIDEO_MENU)
+  TITLE:=Intel VPU DRM support
+  DEPENDS:=@TARGET_x86 +ivpu-firmware
+  KCONFIG:=CONFIG_DRM_ACCEL_IVPU \
+       CONFIG_DRM_ACCEL_HABANALABS=n \
+       CONFIG_DRM_ACCEL_QAIC=n \
+       CONFIG_DRM_ACCEL=y
+  FILES:=$(LINUX_DIR)/drivers/accel/ivpu/intel_vpu.ko
+  AUTOLOAD:=$(call AutoProbe,intel_vpu)
+endef
+
+define KernelPackage/drm-ivpu/description
+  Direct Rendering Manager (DRM) support for Intel VPU
+endef
+
+$(eval $(call KernelPackage,drm-ivpu))
 
 define KernelPackage/drm-imx
   SUBMENU:=$(VIDEO_MENU)
