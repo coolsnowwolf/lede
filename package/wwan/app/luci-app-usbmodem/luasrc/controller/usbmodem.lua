04b8d9bdd690a84f2a6e4aac3c24dafef8d75677
coolsnowwolf <coolsnowwolf@gmail.com>
2025-02-23 21:09:15 +0800

    package/wwan: remove outdated packages

--- package/wwan/app/luci-app-usbmodem/luasrc/controller/usbmodem.lua
+++
@@ -1,9 +0,0 @@
-module("luci.controller.usbmodem", package.seeall)
-
-function index()
-	if not nixio.fs.access("/etc/config/usbmodem") then
-		return
-	end
-
-	entry({"admin", "network", "usbmodem"}, cbi("usbmodem"), _("USB Modem Server"), 80).dependent = false
-end
