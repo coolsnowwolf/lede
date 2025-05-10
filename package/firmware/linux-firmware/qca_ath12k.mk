f387ee9b9a698d53ec4357544f2ab7871ed03c38
John Audia <therealgraysky@proton.me>
2025-05-10 13:58:00 -0400

    linux-firmware: update to 20250509
    Signed-off-by: John Audia <therealgraysky@proton.me>

--- package/firmware/linux-firmware/qca_ath12k.mk
+++ package/firmware/linux-firmware/qca_ath12k.mk
@@ -5,3 +5,11 @@ define Package/ath12k-firmware-wcn7850/install
 		$(PKG_BUILD_DIR)/ath12k/WCN7850/hw2.0/* $(1)/lib/firmware/ath12k/WCN7850/hw2.0/
 endef
 $(eval $(call BuildPackage,ath12k-firmware-wcn7850))
+
+Package/ath12k-firmware-qcn9274 = $(call Package/firmware-default,QCN9274 ath12k firmware)
+define Package/ath12k-firmware-qcn9274/install
+	$(INSTALL_DIR) $(1)/lib/firmware/ath12k/QCN9274/hw2.0
+	$(INSTALL_DATA) \
+		$(PKG_BUILD_DIR)/ath12k/QCN9274/hw2.0/* $(1)/lib/firmware/ath12k/QCN9274/hw2.0/
+endef
+$(eval $(call BuildPackage,ath12k-firmware-qcn9274))
