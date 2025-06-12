264661a7044dd676f2ebec77b57dc0f1da49a6c6
Mathew McBride <matt@traverse.com.au>
2025-06-12 12:17:47 +1000

    kernel: enable memory mapped TPM interface on armsr
    The memory mapped TPM interface (TIS) can be used for
    emulated or virtualized TPM instances with QEMU and
    a simulation package like swtpm.
    
    On QEMU the device will appear in the device tree
    with a "tcg,tpm-tis-mmio" compatible.
    
    For example:
    qemu-system-aarch64 -machine virt -cpu max \
        -bios u-boot.bin \
        -nographic \
        .... \
        -hda openwrt-armsr-armv8-combined-efi.img \
        -chardev socket,id=chrtpm,path=/tmp/mytpm/swtpm-sock \
        -tpmdev emulator,id=tpm0,chardev=chrtpm \
        -device tpm-tis-device,tpmdev=tpm0
    
    Signed-off-by: Mathew McBride <matt@traverse.com.au>
    Link: https://github.com/openwrt/openwrt/pull/19188
    Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

--- package/kernel/linux/modules/other.mk
+++ package/kernel/linux/modules/other.mk
@@ -1272,7 +1272,7 @@ $(eval $(call KernelPackage,tpm))
 define KernelPackage/tpm-tis
   SUBMENU:=$(OTHER_MENU)
   TITLE:=TPM TIS 1.2 Interface / TPM 2.0 FIFO Interface
-	DEPENDS:= @TARGET_x86 +kmod-tpm
+	DEPENDS:= @(TARGET_x86||TARGET_armsr) +kmod-tpm
   KCONFIG:= CONFIG_TCG_TIS
   FILES:= \
 	$(LINUX_DIR)/drivers/char/tpm/tpm_tis.ko \
