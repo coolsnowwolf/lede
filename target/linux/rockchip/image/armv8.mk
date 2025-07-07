8dd26e1de56afc73fd7668e498bb4d1b7b7077ce
zzzz0317 <zhangzhe0177@gmail.com>
2025-07-07 06:37:50 +0000

    rockchip: add support for NLnet XiGuaPi Board V3

--- target/linux/rockchip/image/armv8.mk
+++ target/linux/rockchip/image/armv8.mk
@@ -364,6 +364,17 @@ define Device/mmbox_anas3035
 endef
 TARGET_DEVICES += mmbox_anas3035
 
+define Device/nlnet_xgp
+  DEVICE_VENDOR := NLnet
+  DEVICE_MODEL := XiGuaPi
+  SOC := rk3568
+  UBOOT_DEVICE_NAME := nlnet-xgp-rk3568
+  IMAGE/sysupgrade.img.gz := boot-common | boot-script | pine64-img | gzip | append-metadata
+  DEVICE_PACKAGES := kmod-hwmon-pwmfan kmod-mt7921e wpad-openssl
+  DEVICE_DTS = rockchip/rk3568-nlnet-xgp-v3
+endef
+TARGET_DEVICES += nlnet_xgp
+
 define Device/panther_x2
   DEVICE_VENDOR := Panther
   DEVICE_MODEL := X2
