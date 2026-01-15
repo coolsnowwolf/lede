100934adc19013603472b170e7fb51e112a8b22f
Andrew LaMarche <andrewjlamarche@gmail.com>
2026-01-15 17:46:15 -0500

    qualcommbe: add support for Askey SBE1V1K
    This commit adds support for Askey SBE1V1K.
    
    Device specification
    --------------------
    SoC:		Qualcomm IPQ9570
    RAM:		Micron (2GB)
    Flash:		8GB eMMC
    Ethernet:	1x 10G RTL8261BE
    		1x 2.5G QCA8081
    		2x 1G QCA8075
    Wi-Fi:		QCN6214 2.4GHz 4x4
    		QCN6274 5GHz 4x4
    		QCN6274 6GHz 4x4
    LEDs:		1 LED, power/status
    Buttons:	1 reset
    USB ports:	none
    
    Co-authored-by: Kenneth Kasilag <kenneth@kasilag.me>
    Signed-off-by: Andrew LaMarche <andrewjlamarche@gmail.com>
    Signed-off-by: Robert Marko <robimarko@gmail.com>

--- target/linux/qualcommbe/image/ipq95xx.mk
+++ target/linux/qualcommbe/image/ipq95xx.mk
@@ -1,5 +1,36 @@
 DTS_DIR := $(DTS_DIR)/qcom
 
+define Device/8devices_kiwi-dvk
+	$(call Device/FitImage)
+	$(call Device/EmmcImage)
+	DEVICE_VENDOR := 8devices
+	DEVICE_MODEL := Kiwi-DVK
+	DEVICE_DTS_CONFIG := config@8dev-kiwi
+	SOC := ipq9570
+	DEVICE_PACKAGES := kmod-ath12k ath12k-firmware-qcn9274 \
+		ipq-wifi-8devices_kiwi f2fsck mkf2fs kmod-sfp \
+		kmod-phy-maxlinear kmod-phy-realtek rtl826x-firmware
+	IMAGE/factory.bin := qsdk-ipq-factory-nor
+endef
+TARGET_DEVICES += 8devices_kiwi-dvk
+
+define Device/askey_sbe1v1k
+	$(call Device/FitImage)
+	$(call Device/EmmcImage)
+	DEVICE_VENDOR := Askey
+	DEVICE_MODEL := SBE1V1K
+	DEVICE_ALT0_VENDOR := Askey
+	DEVICE_ALT0_MODEL := RTQ7300T
+	DEVICE_ALT1_VENDOR := Spectrum
+	DEVICE_ALT1_MODEL := SBE1V1K
+	DEVICE_DTS_CONFIG := config@rtq7300t-rev0
+	KERNEL_LOADADDR := 0x42080000
+	SOC := ipq9570
+	DEVICE_PACKAGES := ath12k-firmware-qcn9274 f2fsck ipq-wifi-askey_sbe1v1k kmod-ath12k \
+		kmod-hwmon-pwmfan kmod-phy-realtek mkf2fs rtl826x-firmware
+endef
+TARGET_DEVICES += askey_sbe1v1k
+
 define Device/qcom_rdp433
 	$(call Device/FitImageLzma)
 	DEVICE_VENDOR := Qualcomm Technologies, Inc.
--- target/linux/qualcommbe/ipq95xx/base-files/lib/upgrade/platform.sh
+++ target/linux/qualcommbe/ipq95xx/base-files/lib/upgrade/platform.sh
@@ -1,4 +1,5 @@
 PART_NAME=firmware
+REQUIRE_IMAGE_METADATA=1
 
 RAMFS_COPY_BIN='fw_printenv fw_setenv head'
 RAMFS_COPY_DATA='/etc/fw_env.config /var/lock/fw_printenv.lock'
@@ -9,8 +10,28 @@ platform_check_image() {
 
 platform_do_upgrade() {
 	case "$(board_name)" in
+	8devices,kiwi-dvk)
+		CI_KERNPART="0:HLOS"
+		CI_ROOTPART="rootfs"
+		emmc_do_upgrade "$1"
+		;;
+	askey,sbe1v1k)
+		CI_KERNPART="0:HLOS"
+		CI_ROOTPART="rootfs"
+		CI_DATAPART="rootfs_data"
+		emmc_do_upgrade "$1"
+		;;
 	*)
 		default_do_upgrade "$1"
+		;;
+	esac
+}
+
+platform_copy_config() {
+	case "$(board_name)" in
+	8devices,kiwi-dvk|\
+	askey,sbe1v1k)
+		emmc_copy_config
 		;;
 	esac
 }
