1bc56a7ba1f70bd0963b5b8bf2b3128e6027954e
Christian Marangi <ansuelsmth@gmail.com>
2026-03-04 13:31:00 +0100

    firmware: Add support for Airoha NPU variant firmware
    Add support for Airoha EN7581/AN7583 NPU variant firmware present in
    linux-firmware. The Airoha EN7581 NPU variant is to support devices
    equipped with the MT7996 WiFi chip.
    
    While at it also add an extra new line to follow pattern of double new line to
    separate each firmware package.
    
    Signed-off-by: Christian Marangi <ansuelsmth@gmail.com>

--- package/firmware/linux-firmware/airoha.mk
+++ package/firmware/linux-firmware/airoha.mk
@@ -9,6 +9,7 @@ endef
 
 $(eval $(call BuildPackage,airoha-en8811h-firmware))
 
+
 Package/airoha-en7581-npu-firmware = $(call Package/firmware-default,Airoha EN7581 NPU firmware)
 define Package/airoha-en7581-npu-firmware/install
 	$(INSTALL_DIR) $(1)/lib/firmware/airoha
@@ -19,3 +20,27 @@ define Package/airoha-en7581-npu-firmware/install
 endef
 
 $(eval $(call BuildPackage,airoha-en7581-npu-firmware))
+
+
+Package/airoha-en7581-mt7996-npu-firmware = $(call Package/firmware-default,Airoha EN7581+MT7996 NPU firmware)
+define Package/airoha-en7581-mt7996-npu-firmware/install
+	$(INSTALL_DIR) $(1)/lib/firmware/airoha
+	$(CP) \
+		$(PKG_BUILD_DIR)/airoha/en7581_MT7996_npu_data.bin \
+		$(PKG_BUILD_DIR)/airoha/en7581_MT7996_npu_rv32.bin \
+		$(1)/lib/firmware/airoha
+endef
+
+$(eval $(call BuildPackage,airoha-en7581-mt7996-npu-firmware))
+
+
+Package/airoha-an7583-npu-firmware = $(call Package/firmware-default,Airoha AN7583 NPU firmware)
+define Package/airoha-an7583-npu-firmware/install
+	$(INSTALL_DIR) $(1)/lib/firmware/airoha
+	$(CP) \
+		$(PKG_BUILD_DIR)/airoha/an7583_npu_data.bin \
+		$(PKG_BUILD_DIR)/airoha/an7583_npu_rv32.bin \
+		$(1)/lib/firmware/airoha
+endef
+
+$(eval $(call BuildPackage,airoha-an7583-npu-firmware))
