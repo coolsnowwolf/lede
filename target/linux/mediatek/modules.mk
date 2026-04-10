5fcacc977bf2da1090b5beb7081d3f2e7a61d8cf
Daniel Golle <daniel@makrotopia.org>
2026-04-10 21:00:29 +0100

    mediatek: build driver for built-in 2.5GE PHY
    The by-now-upstream driver for the built-in 2.5GE PHY of the MediaTek
    MT7988 and MT7987 SoC loads firmware at probe time.
    
    Build the driver as a module in order to make sure the driver only
    attempts to load the firmware by the time the rootfs with the firmware
    file has become available.
    
    Signed-off-by: Daniel Golle <daniel@makrotopia.org>

--- target/linux/mediatek/modules.mk
+++ target/linux/mediatek/modules.mk
@@ -37,3 +37,21 @@ define KernelPackage/iio-mt6577-auxadc
   $(call AddDepends/iio)
 endef
 $(eval $(call KernelPackage,iio-mt6577-auxadc))
+
+
+define KernelPackage/phy-mediatek-2p5g
+  SUBMENU:=$(NETWORK_DEVICES_MENU)
+  TITLE:=MediaTek 2.5G Ethernet PHY
+  DEPENDS:=@TARGET_mediatek_filogic @LINUX_6_18 +kmod-libphy
+  KCONFIG:=CONFIG_MEDIATEK_2P5GE_PHY
+  FILES:= \
+   $(LINUX_DIR)/drivers/net/phy/mediatek/mtk-2p5ge.ko
+  AUTOLOAD:=$(call AutoLoad,18,mtk-2p5ge,1)
+endef
+
+define KernelPackage/phy-mediatek-2p5g/description
+  Kernel modules for 2.5G Ethernet PHY built-into the
+  MediaTek MT7988 and MT7987 SoCs.
+endef
+
+$(eval $(call KernelPackage,phy-mediatek-2p5g))
