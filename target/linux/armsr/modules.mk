e0dc811f1df48f70d870348dea63d29009d458d8
coolsnowwolf <coolsnowwolf@gmail.com>
2026-08-31 05:05:27 +0800

    armsr: add Linux 6.18 testing support
    Signed-off-by: coolsnowwolf <coolsnowwolf@gmail.com>

--- target/linux/armsr/modules.mk
+++ target/linux/armsr/modules.mk
@@ -15,7 +15,7 @@ $(eval $(call KernelPackage,acpi-mdio))
 
 define KernelPackage/bcmgenet
   SUBMENU=$(NETWORK_DEVICES_MENU)
-  DEPENDS:=@TARGET_armsr +kmod-mdio-bcm-unimac
+  DEPENDS:=@TARGET_armsr +kmod-mdio-bcm-unimac +kmod-phy-bcm7xxx
   TITLE:=Broadcom GENET internal MAC (Raspberry Pi 4)
   KCONFIG:=CONFIG_BCMGENET
   FILES=$(LINUX_DIR)/drivers/net/ethernet/broadcom/genet/genet.ko
@@ -93,6 +93,7 @@ define KernelPackage/fsl-enetc-net
   FILES:= \
     $(LINUX_DIR)/drivers/net/ethernet/freescale/enetc/fsl-enetc.ko \
     $(LINUX_DIR)/drivers/net/ethernet/freescale/enetc/fsl-enetc-core.ko \
+    $(LINUX_DIR)/drivers/net/ethernet/freescale/enetc/nxp-enetc-pf-common.ko@ge6.18 \
     $(LINUX_DIR)/drivers/net/ethernet/freescale/enetc/fsl-enetc-vf.ko \
     $(LINUX_DIR)/drivers/net/ethernet/freescale/enetc/fsl-enetc-mdio.ko \
     $(LINUX_DIR)/drivers/net/ethernet/freescale/enetc/fsl-enetc-ierb.ko
@@ -190,6 +191,7 @@ $(eval $(call KernelPackage,mvpp2))
 define KernelPackage/imx2-wdt
   SUBMENU:=$(OTHER_MENU)
   TITLE:=NXP (Freescale) i.MX2+ and Layerscape watchdog driver
+  DEPENDS:=@TARGET_armsr
   KCONFIG:=CONFIG_IMX2_WDT
   FILES=$(LINUX_DIR)/drivers/watchdog/imx2_wdt.ko
   AUTOLOAD=$(call AutoLoad,60,imx2_wdt)
@@ -200,6 +202,7 @@ $(eval $(call KernelPackage,imx2-wdt))
 define KernelPackage/imx7-ulp-wdt
   SUBMENU:=$(OTHER_MENU)
   TITLE:=NXP (Freescale) i.MX7ULP and later watchdog
+  DEPENDS:=@TARGET_armsr
   KCONFIG:=CONFIG_IMX7ULP_WDT
   FILES=$(LINUX_DIR)/drivers/watchdog/imx7ulp_wdt.ko
   AUTOLOAD=$(call AutoLoad,60,imx7ulp_wdt)
@@ -217,6 +220,17 @@ define KernelPackage/dwmac-imx
 endef
 
 $(eval $(call KernelPackage,dwmac-imx))
+
+define KernelPackage/dwmac-sun55i
+  SUBMENU=$(NETWORK_DEVICES_MENU)
+  TITLE:=Allwinner A523 (sun55i) Ethernet
+  DEPENDS:=@LINUX_6_18 +kmod-stmmac-core +kmod-of-mdio +kmod-mdio-bus-mux
+  KCONFIG:=CONFIG_DWMAC_SUN55I
+  FILES=$(LINUX_DIR)/drivers/net/ethernet/stmicro/stmmac/dwmac-sun55i.ko
+  AUTOLOAD=$(call AutoLoad,45,dwmac-sun55i)
+endef
+
+$(eval $(call KernelPackage,dwmac-sun55i))
 
 define KernelPackage/dwmac-sun8i
   SUBMENU=$(NETWORK_DEVICES_MENU)
@@ -239,6 +253,17 @@ define KernelPackage/dwmac-rockchip
 endef
 
 $(eval $(call KernelPackage,dwmac-rockchip))
+
+define KernelPackage/dwmac-renesas-gbeth
+  SUBMENU=$(NETWORK_DEVICES_MENU)
+  TITLE:=Renesas GBETH Ethernet
+  DEPENDS:=@LINUX_6_18 +kmod-stmmac-core +kmod-of-mdio +LINUX_6_18:kmod-renesas-rzn-pcs
+  KCONFIG:=CONFIG_DWMAC_RENESAS_GBETH
+  FILES=$(LINUX_DIR)/drivers/net/ethernet/stmicro/stmmac/dwmac-renesas-gbeth.ko
+  AUTOLOAD=$(call AutoLoad,45,dwmac-renesas-gbeth)
+endef
+
+$(eval $(call KernelPackage,dwmac-renesas-gbeth))
 
 define KernelPackage/mdio-thunder
   SUBMENU:=$(NETWORK_DEVICES_MENU)
@@ -304,9 +329,21 @@ endef
  
 $(eval $(call KernelPackage,renesas-net-avb))
 
+define KernelPackage/renesas-rzn-pcs
+  SUBMENU:=$(NETWORK_DEVICES_MENU)
+  TITLE:=Renesas RZ/N and RZ/T2H MII controller
+  DEPENDS:=@LINUX_6_18
+  KCONFIG:=CONFIG_PCS_RZN1_MIIC
+  FILES=$(LINUX_DIR)/drivers/net/pcs/pcs-rzn1-miic.ko
+  AUTOLOAD:=$(call AutoProbe,pcs-rzn1-miic)
+endef
+
+$(eval $(call KernelPackage,renesas-rzn-pcs))
+
 define KernelPackage/wdt-sp805
   SUBMENU:=$(OTHER_MENU)
   TITLE:=ARM SP805 Watchdog
+  DEPENDS:=@TARGET_armsr
   KCONFIG:=CONFIG_ARM_SP805_WATCHDOG
   FILES=$(LINUX_DIR)/drivers/watchdog/sp805_wdt.ko
   AUTOLOAD=$(call AutoLoad,50,sp805_wdt)
@@ -319,4 +356,3 @@ define KernelPackage/wdt-sp805/description
 endef
 
 $(eval $(call KernelPackage,wdt-sp805))
-
