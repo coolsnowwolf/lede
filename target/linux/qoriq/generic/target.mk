de79f445167541463fcba23cf8842548aba7ac33
coolsnowwolf <coolsnowwolf@gmail.com>
2026-08-31 17:30:36 +0800

    qoriq: add Linux 6.18 target support

---
+++ target/linux/qoriq/generic/target.mk
@@ -0,0 +1,5 @@
+define Target/Description
+        Build firmware images for NXP QorIQ boards in the
+        T-Series(T1-T5) aka AMP Series platforms.
+        Features the e5500 and e6500 64-bit Power Architecture cores.
+endef
