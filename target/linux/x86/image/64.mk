6d063c6f609a37b4b6b198180ebf61483f97d8bf
coolsnowwolf <coolsnowwolf@gmail.com>
2026-09-06 00:57:44 +0800

    x86: include ixgbevf in generic images
    Signed-off-by: coolsnowwolf <coolsnowwolf@gmail.com>

--- target/linux/x86/image/64.mk
+++ target/linux/x86/image/64.mk
@@ -3,7 +3,8 @@ define Device/generic
   DEVICE_MODEL := x86/64
   DEVICE_PACKAGES += kmod-amazon-ena kmod-amd-xgbe kmod-bnx2 \
 	kmod-e1000e kmod-e1000 kmod-forcedeth kmod-fs-vfat \
-	kmod-igb kmod-igc kmod-ixgbe kmod-r8168 kmod-tg3
+	kmod-igb kmod-igc kmod-ixgbe kmod-r8168 kmod-tg3 \
+	kmod-ixgbevf
   GRUB2_VARIANT := generic
 endef
 TARGET_DEVICES += generic
