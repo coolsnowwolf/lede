1397f1c98ff7657c2b99ab96e6decf75dee43d18
coolsnowwolf <coolsnowwolf@gmail.com>
2026-09-05 20:29:54 +0800

    mediatek: add CMCC RAX3000Me support
    Signed-off-by: coolsnowwolf <coolsnowwolf@gmail.com>

--- target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
+++ target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
@@ -46,6 +46,7 @@ platform_do_upgrade() {
 		esac
 		;;
 	cmcc,rax3000m-emmc|\
+	cmcc,rax3000me-emmc|\
 	cmcc,xr30-emmc|\
 	glinet,gl-mt2500|\
 	glinet,gl-mt6000|\
@@ -108,6 +109,7 @@ platform_copy_config() {
 		esac
 		;;
 	cmcc,rax3000m-emmc|\
+	cmcc,rax3000me-emmc|\
 	cmcc,xr30-emmc|\
 	glinet,gl-mt2500|\
 	glinet,gl-mt6000|\
--- target/linux/mediatek/image/filogic.mk
+++ target/linux/mediatek/image/filogic.mk
@@ -433,6 +433,42 @@ define Device/cmcc_rax3000m-nand
 endef
 TARGET_DEVICES += cmcc_rax3000m-nand
 
+define Device/cmcc_rax3000me-emmc
+  DEVICE_VENDOR := CMCC
+  DEVICE_MODEL := RAX3000Me (eMMC version)
+  DEVICE_DTS := mt7981b-cmcc-rax3000me
+  DEVICE_DTS_DIR := ../dts
+  DEVICE_PACKAGES := kmod-mt7981-firmware mt7981-wo-firmware kmod-usb3 f2fsck mkf2fs
+  KERNEL := kernel-bin | lzma | fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb
+  KERNEL_INITRAMFS := kernel-bin | lzma | fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb with-initrd | pad-to 64k
+  ARTIFACTS := emmc-preloader.bin emmc-bl31-uboot.fip
+  ARTIFACT/emmc-preloader.bin := mt7981-bl2 emmc-ddr3-1866
+  ARTIFACT/emmc-bl31-uboot.fip := mt7981-bl31-uboot cmcc_rax3000me-emmc
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+endef
+TARGET_DEVICES += cmcc_rax3000me-emmc
+
+define Device/cmcc_rax3000me-nand
+  DEVICE_VENDOR := CMCC
+  DEVICE_MODEL := RAX3000Me (NAND version)
+  DEVICE_DTS := mt7981b-cmcc-rax3000me
+  DEVICE_DTS_DIR := ../dts
+  DEVICE_DTS_OVERLAY := mt7981b-cmcc-rax3000me-nousb
+  DEVICE_PACKAGES := kmod-mt7981-firmware mt7981-wo-firmware kmod-usb3
+  UBINIZE_OPTS := -E 5
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  IMAGE_SIZE := 116736k
+  KERNEL_IN_UBI := 1
+  ARTIFACTS := nand-ddr3-preloader.bin nand-ddr3-bl31-uboot.fip nand-ddr4-preloader.bin nand-ddr4-bl31-uboot.fip
+  ARTIFACT/nand-ddr3-preloader.bin := mt7981-bl2 spim-nand-ddr3-1866
+  ARTIFACT/nand-ddr3-bl31-uboot.fip := mt7981-bl31-uboot cmcc_rax3000me-nand-ddr3
+  ARTIFACT/nand-ddr4-preloader.bin := mt7981-bl2 spim-nand-ddr4
+  ARTIFACT/nand-ddr4-bl31-uboot.fip := mt7981-bl31-uboot cmcc_rax3000me-nand-ddr4
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+endef
+TARGET_DEVICES += cmcc_rax3000me-nand
+
 define Device/cmcc_xr30-emmc
   DEVICE_VENDOR := CMCC
   DEVICE_MODEL := XR30 (eMMC version)
